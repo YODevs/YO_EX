b07a12c50cce41844aaa73f633e1caba8792569a
Amin Badin <amin@yo-lang.org>
2020-07-15 08:49:41 -0700

    Added Void Method :: parse_args
    void parse_args(int argc_i, char *argval[]);

--- compiler/main.c
+++ compiler/main.c
@@ -1,9 +1,12 @@
 #include <stdio.h>
 #include "cli/cli.h"
+#include "cli/parseargs.h"
+
 int main(int argc_i, char *argval[]) {
     if(argc_i > 1)
     {
         //argument available.
+        parse_args(argc_i,argval);
     }else{
      //start init cli -- show
     init_main_cli();
