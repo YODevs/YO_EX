2b882d3068aee7579f5bdcc7860c6a1e04e16444
Amin Badin <amin@yo-lang.org>
2020-07-17 06:44:37 -0700

    Invoke / compile source [manually]

--- compiler/main.c
+++ compiler/main.c
@@ -3,13 +3,20 @@
 #include "cli/cli.h"
 #include "cli/parseargs.h"
 #include "xtest/customargs.h"
+#include "xtest/invsfile.h"
 
-#define STARTTEST true
+//Debug / Test constants.
+#define INJECTARG false
+#define INVOKESOURCEFILE true
+//..........................
 
 int main(int argc_i, char *argval[]) {
-    if (STARTTEST == true)
+    if (INJECTARG == true)
     {
         set_custom_argument("yoc.exe build ./main.yo");
+        return 0;
+    }else if(INVOKESOURCEFILE == true) {
+        invoke_build_command("E:\\IX\\main.yo");
         return 0;
     }
     if(argc_i > 1)
@@ -17,8 +24,8 @@ int main(int argc_i, char *argval[]) {
         //argument available.
         parse_args(argc_i,argval);
     }else{
-     //start init cli -- show
-    init_main_cli();
+       //start init cli -- show
+        init_main_cli();
     }
     return 0;
 }
