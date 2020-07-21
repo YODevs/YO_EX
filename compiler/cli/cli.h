bba90529449ee7d9fcbed4979721e0c340bd4b9d
Amin Badin <amin@yo-lang.org>
2020-07-20 23:29:02 -0700

    Reset C Files .

--- compiler/cli/cli.h
+++
@@ -1,6 +0,0 @@
-//
-// Created by AminCoder on 7/15/2020.
-//
-
-extern void init_main_cli();
-
--- compiler/lexer/tokenid.c
+++
@@ -1,9 +0,0 @@
-//
-// Created by Amin Badin [@AminCoder] on 7/16/2020.
-// Cv : www.AminCoder.ir
-// Mail : dev@yo-lang.org
-// Repository : https://github.com/YODevs/YO
-// Official Website : www.yo-lang.org
-//
-
-#include "tokenid.h"
