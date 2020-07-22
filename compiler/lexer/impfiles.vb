761d42f41cf468e473901a58f52d2eacf0c54a6e
Amin Badin <amin@yo-lang.org>
2020-07-22 10:39:48 -0700

    New Class :: Import directory & Exception method .

---
+++ compiler/lexer/impfiles.vb
@@ -0,0 +1,11 @@
+﻿Imports System.IO
+
+Public Class impfiles
+    Public Shared Sub import_directory(dir As String)
+        If Directory.Exists(dir) Then
+
+        Else
+            dserr.new_error(conserr.errortype.DIRNOTFOUND, dir & " => dir not found.")
+        End If
+    End Sub
+End Class
