5e4c781cf10fc99c654dab1fecfe6e6e3df646e6
Amin Badin <amin@yo-lang.org>
2020-07-22 10:38:24 -0700

    BYPASSFLOW & cflowtester added .

--- compiler/maincr.vb
+++ compiler/maincr.vb
@@ -1,6 +1,13 @@
 ﻿Module maincr
-
+    Const BYPASSFLOW As Boolean = True
     Sub main(ByVal argsval() As String)
+        conserr.init_error_struct()
+        If BYPASSFLOW Then
+            cli.init_cli(False)
+            cflowtester.lex_dir(conrex.APPDIR & "\myapp\")
+            Console.ReadLine()
+            Return
+        End If
         If argsval.Length = 0 Then
             cli.init_cli(True)
             Console.ReadLine()
---
+++ compiler/xtest/cflowtester.vb
@@ -0,0 +1,8 @@
+﻿
+Public Class cflowtester
+
+    Friend Shared Sub lex_dir(location As String)
+        impfiles.import_directory(location)
+    End Sub
+
+End Class
