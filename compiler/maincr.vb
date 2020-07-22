54091b1ab8d9f93eb1467aeb9a44a0c2b733e8b7
Amin Badin <amin@yo-lang.org>
2020-07-22 00:50:42 -0700

    Change defualt font to Consolas font.

--- compiler/maincr.vb
+++ compiler/maincr.vb
@@ -2,7 +2,7 @@
 
     Sub main(ByVal argsval() As String)
         If argsval.Length = 0 Then
-            cli.init_cli()
+            cli.init_cli(True)
             Console.ReadLine()
             Return
         End If
