19749db67eebc6550b3fcaaf37bdcc62fbbb302b
Amin Badin <amin@yo-lang.org>
2020-07-22 10:42:22 -0700

    New Class :: data of errors

---
+++ compiler/err/conserr.vb
@@ -0,0 +1,33 @@
+﻿Public Class conserr
+    Enum errortype
+        DIRNOTFOUND
+    End Enum
+
+    Enum errorpriority
+        IGNORE
+        [STOP]
+    End Enum
+    Structure errorstruct
+        Dim title As String
+        Dim text As String
+        Dim err As errortype
+        Dim priority As errorpriority
+    End Structure
+
+    Public Shared err() As errorstruct
+    Public Shared Sub set_new_error(errtype As errortype, errpriority As errorpriority, title As String, text As String)
+        Static Dim indexarray As Int16 = 0
+        Array.Resize(err, indexarray + 1)
+        err(indexarray) = New errorstruct
+        err(indexarray).err = errtype
+        err(indexarray).priority = errpriority
+        err(indexarray).title = title.ToUpper
+        err(indexarray).text = text
+        indexarray += 1
+        Return
+    End Sub
+
+    Public Shared Sub init_error_struct()
+        set_new_error(errortype.DIRNOTFOUND, errorpriority.STOP, "Directory not found", "Folder containing '.yo' files not found.")
+    End Sub
+End Class
