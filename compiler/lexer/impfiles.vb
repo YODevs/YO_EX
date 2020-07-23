247fbf6ae7399abfb99c9ec28d817449a69f715e
Amin Badin <amin@yo-lang.org>
2020-07-22 22:00:41 -0700

    New function :: get_yo_files()

--- compiler/lexer/impfiles.vb
+++ compiler/lexer/impfiles.vb
@@ -3,9 +3,38 @@
 Public Class impfiles
     Public Shared Sub import_directory(dir As String)
         If Directory.Exists(dir) Then
+            Dim files As New ArrayList
+            If get_yo_files(dir, files) Then
 
+            Else
+                Return
+            End If
         Else
             dserr.new_error(conserr.errortype.DIRNOTFOUND, dir & " => dir not found.")
         End If
     End Sub
+
+    Friend Shared Function get_yo_files(dir As String, ByRef files As ArrayList) As Boolean
+
+        If Directory.GetFiles(dir).Count = 0 Then
+            dserr.new_error(conserr.errortype.YOFILENOTFOUND, "path => " & dir)
+            Return False
+        End If
+
+        Dim file As String = Nothing
+        For index = 0 To Directory.GetFiles(dir).Count - 1
+            file = Directory.GetFiles(dir)(index)
+            If file.ToLower.EndsWith(conrex.YOFORMAT) Then
+                files.Add(file)
+            End If
+        Next
+
+        'TODO : Check files in directory .
+
+        If files.Count = 0 Then
+            dserr.new_error(conserr.errortype.YOFILENOTFOUND, "path => " & dir)
+            Return False
+        End If
+        Return True
+    End Function
 End Class
