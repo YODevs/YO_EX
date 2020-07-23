b7c6ef25016db526569e148ad62f8fdd18b5fe5c
Amin Badin <amin@yo-lang.org>
2020-07-22 21:26:52 -0700

    New Enum :: exitcode status { SUCCESS = 0 , ERROR = 1}

--- compiler/const/conrex.vb
+++ compiler/const/conrex.vb
@@ -2,4 +2,9 @@
     Public Const TITLE As String = "[ YO Lang ]"
     Public Shared VER As String = My.Application.Info.Version.ToString
     Public Shared APPDIR As String = My.Application.Info.DirectoryPath
+
+    Enum exitcode
+        SUCCESS
+        [ERROR]
+    End Enum
 End Class
