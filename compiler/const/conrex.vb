42f741fce04befc4c0cc99a35adf3a75fce7aa31
Amin Badin <amin@yo-lang.org>
2020-07-22 21:59:20 -0700

    New Field :: YOFORMAT

--- compiler/const/conrex.vb
+++ compiler/const/conrex.vb
@@ -2,7 +2,7 @@
     Public Const TITLE As String = "[ YO Lang ]"
     Public Shared VER As String = My.Application.Info.Version.ToString
     Public Shared APPDIR As String = My.Application.Info.DirectoryPath
-
+    Public Const YOFORMAT As String = ".yo"
     Enum exitcode
         SUCCESS
         [ERROR]
