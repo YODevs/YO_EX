580a3744b9cbf6b072e7fd3de7a697046379430a
Amin Badin <amin@yo-lang.org>
2020-07-23 12:04:25 -0700

    New Class :: lexer , import source

---
+++ compiler/lexer/lexer.vb
@@ -0,0 +1,31 @@
+﻿Imports System.IO
+
+Public Class lexer
+    Public Sub New(file As String)
+        fsource = import_source(file)
+        sfile = file
+    End Sub
+
+
+    Private ReadOnly fsource As String
+    Public ReadOnly Property source() As String
+        Get
+            Return fsource
+        End Get
+    End Property
+    Private ReadOnly sfile As String
+    Public ReadOnly Property fileloc() As String
+        Get
+            Return sfile
+        End Get
+    End Property
+
+    Private Function import_source(path As String) As String
+        If File.Exists(path) Then
+            Return File.ReadAllText(path)
+        Else
+            dserr.new_error(conserr.errortype.YOFILENOTFOUND, "path => " & Dir())
+        End If
+        Return conrex.NULL
+    End Function
+End Class
