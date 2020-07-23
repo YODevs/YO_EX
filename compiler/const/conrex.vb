b743fca45e41528e3c96a58f208b14f42d29bf51
Amin Badin <amin@yo-lang.org>
2020-07-23 12:08:57 -0700

    New Field :: NULL

--- compiler/const/conrex.vb
+++ compiler/const/conrex.vb
@@ -3,6 +3,7 @@
     Public Shared VER As String = My.Application.Info.Version.ToString
     Public Shared APPDIR As String = My.Application.Info.DirectoryPath
     Public Const YOFORMAT As String = ".yo"
+    Public Const NULL As String = Nothing
     Enum exitcode
         SUCCESS
         [ERROR]
