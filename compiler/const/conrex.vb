1081086366098f4c12b73845b9209ae415555721
Amin Badin <amin@yo-lang.org>
2020-07-24 21:52:49 -0700

    New Const :: SPACE

--- compiler/const/conrex.vb
+++ compiler/const/conrex.vb
@@ -4,6 +4,7 @@
     Public Shared APPDIR As String = My.Application.Info.DirectoryPath
     Public Const YOFORMAT As String = ".yo"
     Public Const NULL As String = Nothing
+    Public Const SPACE As String = " "
     Enum exitcode
         SUCCESS
         [ERROR]
