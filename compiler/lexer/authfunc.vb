0b1d0aa7b30c236e6c0c7ea03cccef4eb3a6ddce
Amin Badin <amin@yo-lang.org>
2020-07-24 23:17:14 -0700

    New Func :: get_line_error(...)

--- compiler/lexer/authfunc.vb
+++ compiler/lexer/authfunc.vb
@@ -16,4 +16,14 @@ Public Class authfunc
         End If
         Return conrex.NULL
     End Function
+
+    Public Shared Function get_line_error(path As String, linecinf As lexer.targetinf, errtoken As String)
+        Dim code As String = get_line_code(path, linecinf.line + 1) & vbCrLf
+        Dim startindexof As Integer = code.IndexOf(errtoken)
+        Dim excreviewer As String = Space(startindexof)
+        For index = 0 To errtoken.Length - 1
+            excreviewer &= conrex.CURSORERR
+        Next
+        Return code & vbCr & excreviewer
+    End Function
 End Class
