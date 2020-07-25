c74e79633616c7af1ed5eb137d89636ce2ec3259
Amin Badin <amin@yo-lang.org>
2020-07-24 23:28:09 -0700

    Throw RUNTIMEERROR

--- compiler/maincr.vb
+++ compiler/maincr.vb
@@ -1,18 +1,22 @@
 ﻿Module maincr
     Const BYPASSFLOW As Boolean = True
     Sub main(ByVal argsval() As String)
-        init_class_data()
-        If BYPASSFLOW Then
-            cli.init_cli(False)
-            cflowtester.lex_dir(conrex.APPDIR & "\myapp\")
-            Console.ReadLine()
-            Return
-        End If
-        If argsval.Length = 0 Then
-            cli.init_cli(True)
-            Console.ReadLine()
-            Return
-        End If
+        Try
+            init_class_data()
+            If BYPASSFLOW Then
+                cli.init_cli(False)
+                cflowtester.lex_dir(conrex.APPDIR & "\myapp\")
+                Console.ReadLine()
+                Return
+            End If
+            If argsval.Length = 0 Then
+                cli.init_cli(True)
+                Console.ReadLine()
+                Return
+            End If
+        Catch ex As Exception
+            dserr.new_error(conserr.errortype.RUNTIMEERROR, ex.Message & vbCrLf & ex.StackTrace)
+        End Try
     End Sub
 
     Sub init_class_data()
