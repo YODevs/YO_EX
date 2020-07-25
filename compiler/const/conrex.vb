c5fb2dd5fb2f1a72758b960f9367eb21386757c4
Amin Badin <amin@yo-lang.org>
2020-07-24 23:16:12 -0700

    Change data-type & New Constant :: CURSORERR

--- compiler/const/conrex.vb
+++ compiler/const/conrex.vb
@@ -4,7 +4,9 @@
     Public Shared APPDIR As String = My.Application.Info.DirectoryPath
     Public Const YOFORMAT As String = ".yo"
     Public Const NULL As String = Nothing
-    Public Const SPACE As String = " "
+    Public Const SPACE As Char = " "
+    Public Const DOT As Char = "."
+    Public Const CURSORERR As Char = "^"
     Enum exitcode
         SUCCESS
         [ERROR]
