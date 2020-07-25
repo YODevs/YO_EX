88b982258e3fd1d3da2b86ecdce20ace3a99277e
Amin Badin <amin@yo-lang.org>
2020-07-24 22:38:50 -0700

    New Func :: get_line_code(...)

---
+++ compiler/lexer/authfunc.vb
@@ -0,0 +1,19 @@
+﻿Imports System.IO
+
+Public Class authfunc
+    Public Shared Function get_line_code(path As String, line As Integer) As String
+        If File.Exists(path) Then
+            Dim fsource As String = File.ReadAllText(path)
+            Dim splsource() As String = fsource.Split(Chr(10)).ToArray
+            If line <= splsource.Length - 1 Then
+                Return splsource(line)
+            Else
+                'Error...
+            End If
+        Else
+            dserr.new_error(conserr.errortype.YOFILENOTFOUND, "path => " & path)
+            Return conrex.NULL
+        End If
+        Return conrex.NULL
+    End Function
+End Class
