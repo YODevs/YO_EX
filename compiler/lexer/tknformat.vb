12a68fece73c1e987b79a6741579e4d391205fd6
Amin Badin <amin@yo-lang.org>
2020-07-25 23:24:40 -0700

    Create tknformat.vb

---
+++ compiler/lexer/tknformat.vb
@@ -0,0 +1,29 @@
+﻿Public Class tknformat
+    Structure _class
+        Dim name As String
+        Dim methods() As _method
+    End Structure
+
+    Structure _method
+        Dim name As String
+        Dim returntype As String
+        Dim parameters() As _parameter
+    End Structure
+
+    Structure _parameter
+        Dim name As String
+        Dim ptype As String
+        Dim byreference As Boolean
+        Dim defvalue As String
+    End Structure
+    Structure _pubfield
+
+    End Structure
+
+    Structure _inlcode
+
+    End Structure
+    Public Sub New()
+
+    End Sub
+End Class
