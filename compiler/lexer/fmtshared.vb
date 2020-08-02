50c9e1880efaa21a8acd8a771b7e3753ece4098e
Amin Badin <amin@yo-lang.org>
2020-08-01 22:28:08 -0700

    New method :: imp_token(...) , add tokens

---
+++ compiler/lexer/fmtshared.vb
@@ -0,0 +1,13 @@
+﻿Public Class fmtshared
+    Public tknfmt As tknformat
+    Private sourceloc As String
+    Public Sub New(file As String)
+        tknfmt = New tknformat
+        sourceloc = file
+    End Sub
+
+    Public Sub imp_token(value As String, rd_token As tokenhared.token, linecinf As lexer.targetinf)
+        MsgBox(value)
+    End Sub
+
+End Class
