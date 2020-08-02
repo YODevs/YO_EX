97247eb04c32c354ecb15c552bb098a95fe721fb
Amin Badin <amin@yo-lang.org>
2020-08-01 23:18:42 -0700

    Get function names

--- compiler/lexer/fmtshared.vb
+++ compiler/lexer/fmtshared.vb
@@ -2,11 +2,13 @@
     Public tknfmt As tknformat
     Private sourceloc As String
     Dim state As statecursor
-    Dim xmethods(1) As tknformat._method
+    Dim xmethods(0) As tknformat._method
+    Dim funcstate As funcstatecursor
     Public Sub New(file As String)
         tknfmt = New tknformat
         sourceloc = file
         state = statecursor.OUT
+        funcstate = funcstatecursor.OUT
         xmethods(0) = New tknformat._method
     End Sub
 
@@ -17,6 +19,7 @@
     End Enum
 
     Enum funcstatecursor
+        OUT
         ACCESSRESTR
         FUNCINTRO
         FUNCNAME
@@ -31,12 +34,33 @@
                 Case tokenhared.token.FUNC
                     _rev_func(value, rd_token, linecinf)
             End Select
+
+        ElseIf state = statecursor.INFUNC Then
+            _rev_func(value, rd_token, linecinf)
         End If
     End Sub
-
-
-
     Private Sub _rev_func(value As String, rd_token As tokenhared.token, linecinf As lexer.targetinf)
+        Dim i As Integer = xmethods.Length - 1
+        Select Case funcstate
+            Case funcstatecursor.OUT
+                If rd_token = tokenhared.token.FUNC Then
+                    If xmethods.Length <> 1 Then
+                        Array.Resize(xmethods, xmethods.Length)
+                    End If
+                    funcstate = funcstatecursor.FUNCNAME
+                    state = statecursor.INFUNC
+                    Return
+                End If
+            Case funcstatecursor.FUNCNAME
+                If rd_token = tokenhared.token.IDENTIFIER Then
+                    xmethods(i).name = value
+                    funcstate = funcstatecursor.FUNCPARA
+                Else
+                    dserr.new_error(conserr.errortype.IDENTIFIEREXPECTED, authfunc.get_line_error(sourceloc, linecinf, value), "func main()
+{...}")
+                End If
+        End Select
 
     End Sub
+
 End Class
