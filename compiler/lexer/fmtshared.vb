b7da7309064a5ca23a63d952ac197f2731832034
Amin Badin <amin@yo-lang.org>
2020-08-03 00:07:00 -0700

    Analyze the parameters of a function and manage its exceptions

--- compiler/lexer/fmtshared.vb
+++ compiler/lexer/fmtshared.vb
@@ -4,11 +4,13 @@
     Dim state As statecursor
     Dim xmethods(0) As tknformat._method
     Dim funcstate As funcstatecursor
+    Dim parastate As funcparastate
     Public Sub New(file As String)
         tknfmt = New tknformat
         sourceloc = file
         state = statecursor.OUT
         funcstate = funcstatecursor.OUT
+        parastate = funcparastate.WAITFORSTARTBRACKET
         xmethods(0) = New tknformat._method
     End Sub
 
@@ -27,6 +29,14 @@
         FUNCSTBLOCK
         FUNCBODY
         FUNCENBLOCK
+    End Enum
+
+    Enum funcparastate
+        WAITFORSTARTBRACKET
+        WAITFORIDENTIFIER
+        WAITFORASSIGNMENTOPERATOR
+        WAITFORPARAMETERTYPE
+        WAITFORNEWPARAMETER
     End Enum
     Public Sub imp_token(value As String, rd_token As tokenhared.token, linecinf As lexer.targetinf)
         If state = statecursor.OUT Then
@@ -59,6 +69,29 @@
                     dserr.new_error(conserr.errortype.IDENTIFIEREXPECTED, linecinf.line, sourceloc, authfunc.get_line_error(sourceloc, linecinf, value), "func main()
 {...}")
                 End If
+            Case funcstatecursor.FUNCPARA
+                Select Case parastate
+                    Case funcparastate.WAITFORSTARTBRACKET
+                        If rd_token = tokenhared.token.PRSTART Then
+                            xmethods(i).nopara = True
+                            parastate = funcparastate.WAITFORNEWPARAMETER
+                            Return
+                        Else
+                            dserr.new_error(conserr.errortype.FUNCOPBRACKETEXPECTED, linecinf.line, sourceloc, authfunc.get_line_error(sourceloc, linecinf, value), "func main()
+{...}")
+                        End If
+                    Case funcparastate.WAITFORNEWPARAMETER
+                        If rd_token = tokenhared.token.PREND Then
+                            funcstate = funcstatecursor.FUNCSTBLOCK 'TODO : Return Type
+                            Return
+                        ElseIf rd_token = tokenhared.token.IDENTIFIER Then
+                            xmethods(i).nopara = False
+                            'TODO
+                        Else
+                            dserr.new_error(conserr.errortype.IDENTIFIEREXPECTED, linecinf.line, sourceloc, authfunc.get_line_error(sourceloc, linecinf, value), "func get_data(id : i16)
+{...}")
+                        End If
+                End Select
         End Select
 
     End Sub
