13a39f80effb3901f70d1230f3bfdb71e2bcac3d
Amin Badin <amin@yo-lang.org>
2020-08-04 04:55:29 -0700

    New Class :: write/output data

---
+++ compiler/xtest/coutputdata.vb
@@ -0,0 +1,8 @@
+﻿Imports System.IO
+
+Public Class coutputdata
+
+    Public Shared Sub write_data(data As String)
+        File.WriteAllText(My.Application.Info.DirectoryPath & "\coutput.txt", data)
+    End Sub
+End Class
