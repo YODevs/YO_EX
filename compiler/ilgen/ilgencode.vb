bce13dfac383947ecc29184c0db526246d89dd46
Amin Badin <amin@yo-lang.org>
2020-08-08 06:17:50 -0700

    Create ilgencode.vb

---
+++ compiler/ilgen/ilgencode.vb
@@ -0,0 +1,13 @@
+﻿Public Class ilgencode
+
+    Dim classdt() As tknformat._class
+    Public Sub New(tknfmtclass() As tknformat._class)
+        classdt = tknfmtclass
+    End Sub
+
+    Public Sub codegenerator()
+        For index = 0 To classdt.Length - 1
+
+        Next
+    End Sub
+End Class
