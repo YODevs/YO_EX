b65e20537bd3952f2af9277f5db7964d2340ed4e
Amin Badin <amin@yo-lang.org>
2020-08-09 02:34:28 -0700

    New Struct :: _ilassemblyextern structure for store assemblies extern.

--- compiler/ilgen/ilformat.vb
+++ compiler/ilgen/ilformat.vb
@@ -6,6 +6,8 @@
     End Structure
     Public Structure ildata
         Dim ilmethod() As _ilmethodcollection
+        Dim assemblyextern() As _ilassemblyextern
+        Dim modulename As String
         'Assembly
         'Global Fields
     End Structure
@@ -21,6 +23,10 @@
         Dim line As ArrayList
     End Structure
 
+    Public Structure _ilassemblyextern
+        Dim name As String
+        Dim assemblyproperty As String
+    End Structure
     Public Sub New()
 
     End Sub
