d097b3b5307391cf68a11145aaddb43049474338
Amin Badin <amin@yo-lang.org>
2020-08-08 20:35:29 -0700

    Create ilasmgen.vb

---
+++ compiler/ilgen/ilasmgen.vb
@@ -0,0 +1,10 @@
+﻿Public Class ilasmgen
+    Private ilcollection As ilformat.ildata
+    Public Sub New(ilcol As ilformat.ildata)
+        ilcollection = ilcol
+    End Sub
+
+    Public Function gen() As ilformat.resultildata
+
+    End Function
+End Class
