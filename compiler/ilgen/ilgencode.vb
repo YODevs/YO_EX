799c00b3dec17d81b3d204c43edcf5b5dc1b7738
Amin Badin <amin@yo-lang.org>
2020-08-08 20:36:57 -0700

    Init ilformat & ilasmgen

--- compiler/ilgen/ilgencode.vb
+++ compiler/ilgen/ilgencode.vb
@@ -1,12 +1,17 @@
 ﻿Public Class ilgencode
 
     Dim classdt() As tknformat._class
+    Dim ilcollection As ilformat.ildata
+    Dim ilasm As ilasmgen
     Public Sub New(tknfmtclass() As tknformat._class)
         classdt = tknfmtclass
+        ilcollection = New ilformat.ildata
     End Sub
 
     Public Sub codegenerator()
         For index = 0 To classdt.Length - 1
+            'Class Generate ...
+            ilasm = New ilasmgen(ilcollection)
 
         Next
     End Sub
