7da832ac86aa4506a3394f7948fc97aa9f8dc11b
Amin Badin <amin@yo-lang.org>
2020-08-09 10:15:33 -0700

    Invoke _ilfunc.gen() for build methods.

--- compiler/ilgen/ilasmgen.vb
+++ compiler/ilgen/ilasmgen.vb
@@ -1,6 +1,7 @@
 ﻿Public Class ilasmgen
 
     Private _ilhead As ilhead
+    Private _ilfunc As ilfuncgen
     Private ilcollection As ilformat.ildata
     Public Sub New(ilcol As ilformat.ildata)
         ilcollection = ilcol
@@ -8,5 +9,8 @@
 
     Public Function gen(yoclassdt As tknformat._class) As ilformat.resultildata
         _ilhead = New ilhead(ilcollection, yoclassdt.name)
+        'Public fields ...
+        _ilfunc = New ilfuncgen(ilcollection, yoclassdt)
+        _ilfunc.gen()
     End Function
 End Class
