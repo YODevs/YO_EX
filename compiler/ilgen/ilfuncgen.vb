1832a6a96c7891366f40a2d51a573a013252352c
Amin Badin <amin@yo-lang.org>
2020-08-09 10:15:59 -0700

    New Action :: Build ILmethods

--- compiler/ilgen/ilfuncgen.vb
+++ compiler/ilgen/ilfuncgen.vb
@@ -1,6 +1,23 @@
 ﻿Public Class ilfuncgen
     Private ilcollection As ilformat.ildata
-    Public Sub New(ilcol As ilformat.ildata)
+    Private yoclassdt As tknformat._class
+    Private _ilmethods(0) As ilformat._ilmethodcollection
+
+    Public Sub New(ilcol As ilformat.ildata, yoclass As tknformat._class)
         ilcollection = ilcol
+        yoclassdt = yoclass
+        _ilmethods(0) = New ilformat._ilmethodcollection
     End Sub
+
+    Public Function gen() As ilformat._ilmethodcollection()
+        For index = 0 To yoclassdt.methods.Length - 1
+            Dim _ilmethodslen As Integer = _ilmethods.Length
+            Dim imethod As Int16 = _ilmethodslen - 1
+            If _ilmethodslen <> 0 Then
+                Array.Resize(_ilmethods, _ilmethodslen + 1)
+            End If
+
+
+        Next
+    End Function
 End Class
