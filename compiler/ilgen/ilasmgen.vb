da64008dd2feab1c6a8e762cd3aeff9eca137f70
Amin Badin <amin@yo-lang.org>
2020-08-09 02:34:49 -0700

    Init _ilhead class

--- compiler/ilgen/ilasmgen.vb
+++ compiler/ilgen/ilasmgen.vb
@@ -1,10 +1,12 @@
 ﻿Public Class ilasmgen
+
+    Private _ilhead As ilhead
     Private ilcollection As ilformat.ildata
     Public Sub New(ilcol As ilformat.ildata)
         ilcollection = ilcol
     End Sub
 
-    Public Function gen() As ilformat.resultildata
-
+    Public Function gen(yoclassdt As tknformat._class) As ilformat.resultildata
+        _ilhead = New ilhead(ilcollection, yoclassdt.name)
     End Function
 End Class
