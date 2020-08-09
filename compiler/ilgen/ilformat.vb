9f9e033b0c04cac0d47f2504534ccf4e3baefcd7
Amin Badin <amin@yo-lang.org>
2020-08-08 20:34:57 -0700

    New Class :: ilformat , public structure for ilcodes

---
+++ compiler/ilgen/ilformat.vb
@@ -0,0 +1,27 @@
+﻿Public Class ilformat
+
+    Public Structure resultildata
+        Dim result As Boolean
+        Dim ilfmtdata As ildata
+    End Structure
+    Public Structure ildata
+        Dim ilmethod() As _ilmethodcollection
+        'Assembly
+        'Global Fields
+    End Structure
+
+    Public Structure _ilmethodcollection
+        Dim name As String
+        'Property
+        'ReturnType
+        'Parameters
+        Dim entrypoint As Boolean
+        Dim maxstack As Int16
+        Dim codes As ArrayList
+        Dim line As ArrayList
+    End Structure
+
+    Public Sub New()
+
+    End Sub
+End Class
