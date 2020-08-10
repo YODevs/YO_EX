387c45e7101ae1dd55ed22010bfdafde47769cb9
Amin Badin <amin@yo-lang.org>
2020-08-10 03:00:36 -0700

    New Method :: Create IL .Methods

--- compiler/ilgen/ilfuncgen.vb
+++ compiler/ilgen/ilfuncgen.vb
@@ -1,4 +1,6 @@
 ﻿Public Class ilfuncgen
+
+    Private grabentrypoint As Boolean = False
     Private ilcollection As ilformat.ildata
     Private yoclassdt As tknformat._class
     Private _ilmethods(0) As ilformat._ilmethodcollection
@@ -16,8 +18,29 @@
             If _ilmethodslen <> 0 Then
                 Array.Resize(_ilmethods, _ilmethodslen + 1)
             End If
-
-
+            func_ilforamtter(yoclassdt.methods(index), _ilmethodslen)
         Next
+        Return _ilmethods
     End Function
+
+
+    Public Sub func_ilforamtter(yomethod As tknformat._method, _ilmethodslen As Integer)
+        'Check name rules ...
+        'Check uniq names
+        _ilmethods(_ilmethodslen).name = yomethod.name
+        If yomethod.name.ToLower = "main" Then
+            If grabentrypoint = False Then
+                _ilmethods(_ilmethodslen).entrypoint = True
+                grabentrypoint = True
+            Else
+                'When two entry points 
+                'set error
+            End If
+        End If
+
+        _ilmethods(_ilmethodslen).codes.Add("ldstr ""Hello From YOLang""")
+        _ilmethods(_ilmethodslen).codes.Add("Call Void [mscorlib]System.Console:WriteLine(String)")
+        _ilmethods(_ilmethodslen).codes.Add("ret")
+
+    End Sub
 End Class
