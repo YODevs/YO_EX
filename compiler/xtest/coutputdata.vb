5034c98dfd122bffddba7be83fe9971e4a23e5c0
Amin Badin <amin@yo-lang.org>
2020-08-12 11:25:20 -0700

    New Method :: Make file data , with filename

--- compiler/xtest/coutputdata.vb
+++ compiler/xtest/coutputdata.vb
@@ -5,4 +5,7 @@ Public Class coutputdata
     Public Shared Sub write_data(data As String)
         File.WriteAllText(My.Application.Info.DirectoryPath & "\coutput.txt", data)
     End Sub
+    Public Shared Sub write_file_data(filename As String, data As String)
+        File.WriteAllText(My.Application.Info.DirectoryPath & "\" & filename, data)
+    End Sub
 End Class
