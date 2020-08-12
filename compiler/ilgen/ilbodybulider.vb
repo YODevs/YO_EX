b6d9db3db6383a208d6dc6bc6dde2b7b42d9db3e
Amin Badin <amin@yo-lang.org>
2020-08-12 13:28:33 -0700

    New Method :: add_assembly(...) to header il file.

--- compiler/ilgen/ilbodybulider.vb
+++ compiler/ilgen/ilbodybulider.vb
@@ -2,7 +2,7 @@
 
     Public ildt As ilformat.ildata
 
-    Private ReadOnly msilsource As String
+    Private msilsource As String
     Public ReadOnly Property source() As String
         Get
             Return msilsource
@@ -13,6 +13,23 @@
     End Sub
 
     Public Function conv_to_msil() As String
+        For index = 0 To ildt.assemblyextern.Length - 1
+            add_assembly(ildt.assemblyextern(index))
+        Next
 
+        Return source
     End Function
+
+    Public Sub add_assembly(assemblydt As ilformat._ilassemblyextern)
+        'check uniq assembly
+        'check names
+        If assemblydt.isextern Then
+            add_il_code(".assembly extern " & assemblydt.name & assemblydt.assemblyproperty)
+        Else
+            add_il_code(".assembly " & assemblydt.name & assemblydt.assemblyproperty)
+        End If
+    End Sub
+    Public Sub add_il_code(code As String)
+        msilsource &= vbCrLf & code
+    End Sub
 End Class
