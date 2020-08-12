d84ef4399e3de1ef8f13141708fc627564940bbd
Amin Badin <amin@yo-lang.org>
2020-08-12 11:26:41 -0700

    Create ilbodybuilder class for generate il codes.

---
+++ compiler/ilgen/ilbodybulider.vb
@@ -0,0 +1,18 @@
+﻿Public Class ilbodybulider
+
+    Public ildt As ilformat.ildata
+
+    Private ReadOnly msilsource As String
+    Public ReadOnly Property source() As String
+        Get
+            Return msilsource
+        End Get
+    End Property
+    Public Sub New(ilclassdata As ilformat.ildata)
+        ildt = ilclassdata
+    End Sub
+
+    Public Function conv_to_msil() As String
+
+    End Function
+End Class
