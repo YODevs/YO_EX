b44460fb854d46f80ba24c2638123583a41424e8
Amin Badin <amin@yo-lang.org>
2020-08-12 11:25:58 -0700

    Invoke ilbodybuilder class & conv_to_msil() func.

--- compiler/ilgen/ilgencode.vb
+++ compiler/ilgen/ilgencode.vb
@@ -8,11 +8,21 @@
         ilcollection = New ilformat.ildata
     End Sub
 
+
+    'TODO : compile multi code generator.
     Public Sub codegenerator()
         For index = 0 To classdt.Length - 1
             'Class Generate ...
+            'Single File ;
             ilasm = New ilasmgen(ilcollection)
-            ilasm.gen(classdt(index))
+            Dim resultdata As ilformat.resultildata = ilasm.gen(classdt(index))
+            If resultdata.result Then
+                Dim bodybuilder As New ilbodybulider(resultdata.ilfmtdata)
+                bodybuilder.conv_to_msil()
+                coutputdata.write_file_data("msil_source.il", bodybuilder.source)
+            Else
+                'Somethings error ...
+            End If
         Next
     End Sub
 End Class
