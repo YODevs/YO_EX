37b51e37a658960bfdb5e9814abd736d0443ceb9
Amin Badin <amin@yo-lang.org>
2020-08-14 10:08:11 -0700

    Compile & Print result in console .

---
+++ compiler/conv/ilasmconv.vb
@@ -0,0 +1,39 @@
+﻿Imports System.Text
+
+Public Class ilasmconv
+    Private ilasmoutputdata As StringBuilder
+    Private ilasmparameter As ilasmparam
+    Public Sub New(inputparam As ilasmparam)
+        ilasmparameter = inputparam
+    End Sub
+
+    Public Sub compile()
+        ilasmoutputdata = New StringBuilder()
+        Dim ilasmproc As New System.Diagnostics.Process()
+        With ilasmproc.StartInfo
+            .FileName = My.Application.Info.DirectoryPath & "\ilasm.exe"
+            .Arguments = ilasmparameter.get_param_list
+            .RedirectStandardOutput = True
+            .RedirectStandardError = True
+            .RedirectStandardInput = True
+            .UseShellExecute = False
+            .WindowStyle = ProcessWindowStyle.Normal
+            .CreateNoWindow = False
+        End With
+
+        AddHandler ilasmproc.OutputDataReceived, AddressOf ilasmoutputhandler
+        ilasmproc.Start()
+        ilasmproc.BeginOutputReadLine()
+        ilasmproc.WaitForExit()
+        Console.WriteLine(ilasmoutputdata.ToString())
+        coutputdata.write_file_data("conv_info.txt", ilasmoutputdata.ToString())
+    End Sub
+
+    Private Sub ilasmoutputhandler(sender As Object, e As DataReceivedEventArgs)
+        ilasmoutputdata.AppendLine(e.Data)
+    End Sub
+
+    Private Sub init()
+
+    End Sub
+End Class
