8c0f8aaf9670275636af835015e96d1fae072c47
Amin Badin <amin@yo-lang.org>
2020-08-14 10:07:47 -0700

    Create argument list for pass to ilasm.exe

---
+++ compiler/conv/ilasmparam.vb
@@ -0,0 +1,22 @@
+﻿Public Class ilasmparam
+
+    Private paramlist As String
+    Public Sub New()
+
+    End Sub
+
+    Public Sub add_param(argument As String)
+        paramlist &= argument & Space(1)
+    End Sub
+    Public Sub add_param(argument As String, value As String)
+        paramlist &= argument & ":" & value & Space(1)
+    End Sub
+
+    Public Sub add_file(path As String)
+        path = """" & path & """"
+        paramlist = path & Space(1) & paramlist
+    End Sub
+    Public Function get_param_list() As String
+        Return paramlist
+    End Function
+End Class
