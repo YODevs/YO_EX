493eb0c394a8a2ff1b472bc4f4b51061ace23de4
Amin Badin <amin@yo-lang.org>
2020-08-15 10:11:35 -0700

    transpile yo to msil code.

---
+++ compiler/iltrans/iltranscore.vb
@@ -0,0 +1,12 @@
+﻿Public Class iltranscore
+
+    Private methoddata As tknformat._method
+    Public Sub New(method As tknformat._method)
+        methoddata = method
+    End Sub
+
+    Public Sub gen_transpile_code(ByRef _ilmethod As ilformat._ilmethodcollection)
+        _ilmethod.codes = New ArrayList
+        _ilmethod.line = New ArrayList
+    End Sub
+End Class
