51dd63243bf523d975ace28a1ab155bef590552b
Amin Badin <amin@yo-lang.org>
2020-08-14 21:57:14 -0700

    Check exists essential files.

---
+++ compiler/initproc/initessentialfiles.vb
@@ -0,0 +1,14 @@
+﻿Imports System.IO
+
+Public Class initessentialfiles
+
+    Public Shared essfiles As New ArrayList
+    Public Shared Sub add_path(path As String)
+        essfiles.Add(path)
+        If File.Exists(path) Then
+            Return
+        Else
+            dserr.new_error(conserr.errortype.MISSINGESSENTIALFILES, -1, Nothing, "Path : " & path)
+        End If
+    End Sub
+End Class
