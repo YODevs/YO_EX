80a72280882962fb05862e33f668d2a787127d71
Amin Badin <amin@yo-lang.org>
2020-08-16 04:00:52 -0700

    init xmlunpkd class and invoke get_line_tokens

--- compiler/iltrans/iltranscore.vb
+++ compiler/iltrans/iltranscore.vb
@@ -8,5 +8,14 @@
     Public Sub gen_transpile_code(ByRef _ilmethod As ilformat._ilmethodcollection)
         _ilmethod.codes = New ArrayList
         _ilmethod.line = New ArrayList
+        Dim xmldata As New xmlunpkd(methoddata.bodyxmlfmt)
+
+        While xmldata.xmlreader.EOF = False
+            Dim clinecodestruc() As xmlunpkd.linecodestruc
+            clinecodestruc = xmldata.get_line_tokens
+            For index = 0 To clinecodestruc.Length - 1
+
+            Next
+        End While
     End Sub
 End Class
