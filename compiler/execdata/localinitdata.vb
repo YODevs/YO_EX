83d52af50c5ee5b439a8f47f57ec7b5bd77231ad
Amin Badin <amin@yo-lang.org>
2020-08-17 00:34:12 -0700

    New Class :: Add local init items.

---
+++ compiler/execdata/localinitdata.vb
@@ -0,0 +1,17 @@
+﻿Public Class localinitdata
+    Private datatypelocal As mapstoredata
+    Public Sub New()
+        datatypelocal = New mapstoredata
+    End Sub
+
+    Public Function check_local_init(name As String) As Boolean
+        name = name.ToLower
+        Return datatypelocal.find(name).issuccessful
+    End Function
+
+    Public Sub add_local_init(name As String, datatype As String)
+        name = name.ToLower
+        datatype = datatype.ToLower
+        datatypelocal.add(name, datatype)
+    End Sub
+End Class
