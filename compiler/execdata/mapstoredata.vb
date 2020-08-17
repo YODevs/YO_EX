50085db047a3134b5fddb473dfde5693167c153d
Amin Badin <amin@yo-lang.org>
2020-08-16 22:37:41 -0700

    New Class :: mapstoredata for store maps .

---
+++ compiler/execdata/mapstoredata.vb
@@ -0,0 +1,17 @@
+﻿Public Class mapstoredata
+    Private keys, values As ArrayList
+    Public Sub New()
+        keys = New ArrayList
+        values = New ArrayList
+    End Sub
+
+    Public Sub import_collection(keylist As ArrayList, valuelist As ArrayList)
+        keys = keylist
+        values = valuelist
+    End Sub
+
+    Public Sub add(key As String, value As String)
+        keys.Add(key)
+        values.Add(key)
+    End Sub
+End Class
