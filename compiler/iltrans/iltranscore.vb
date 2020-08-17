bddea99db40de072d8147eee4f70349a0396c620
Amin Badin <amin@yo-lang.org>
2020-08-17 04:11:12 -0700

    Define Let statements.

--- compiler/iltrans/iltranscore.vb
+++ compiler/iltrans/iltranscore.vb
@@ -1,5 +1,5 @@
 ﻿Public Class iltranscore
-    Private _illocallinit(0) As ilformat._illocalinit
+    Private _illocalinit(0) As ilformat._illocalinit
     Private methoddata As tknformat._method
     Private localinit As localinitdata
     Private path As String
@@ -19,7 +19,8 @@
             rev_cline_code(clinecodestruc, _ilmethod)
         End While
 
-        _ilmethod.locallinit = _illocallinit 'exc no local init
+        Array.Resize(_illocalinit, _illocalinit.Length - 1)
+        _ilmethod.locallinit = _illocalinit 'exc no local init
         xmldata.close()
     End Sub
 
@@ -33,22 +34,33 @@
     End Sub
 
     Private Sub nv_let(clinecodestruc() As xmlunpkd.linecodestruc)
-        Dim ilmethodlen As Integer = _illocallinit.Length
+        Dim ilmethodlen As Integer = _illocalinit.Length
         Dim index As Integer = ilmethodlen - 1
         If ilmethodlen = 0 Then index = 0
-        Array.Resize(_illocallinit, ilmethodlen + 1)
+        Array.Resize(_illocalinit, ilmethodlen + 1)
         If clinecodestruc(1).tokenid <> tokenhared.token.IDENTIFIER Then
             'IDENTIFIER EXPECTED
             dserr.new_error(conserr.errortype.IDENTIFIEREXPECTED, clinecodestruc(1).line, path, authfunc.get_line_error(path, get_target_info(clinecodestruc(1)), clinecodestruc(1).value), "let name : str = ""Amin""")
+
+            'TODO : Check Global Types.
         ElseIf localinit.check_local_init(clinecodestruc(1).value) Then
             'DECLARING ERROR
             dserr.new_error(conserr.errortype.DECLARINGERROR, clinecodestruc(1).line, path, authfunc.get_line_error(path, get_target_info(clinecodestruc(1)), clinecodestruc(1).value) & vbCrLf & "Choose another name.")
         End If
+        'Check ":" in let statements.
+        If clinecodestruc(2).tokenid <> tokenhared.token.ASSINQ Then
+            dserr.new_error(conserr.errortype.SYNTAXERROR, clinecodestruc(2).line, path, authfunc.get_line_error(path, get_target_info(clinecodestruc(2)), clinecodestruc(2).value), "let name : str = ""Amin""")
+        End If
 
-        _illocallinit(index).name = clinecodestruc(1).value
+        If clinecodestruc(3).tokenid = tokenhared.token.COMMONDATATYPE Then
+            _illocalinit(index).name = clinecodestruc(1).value.ToLower
+            _illocalinit(index).datatype = initcommondatatype.cdtype.find(clinecodestruc(3).value.ToLower).result
+        Else
+            'Check other type ...
+        End If
 
-        localinit.add_local_init(clinecodestruc(1).value, clinecodestruc(2).value)
-        MsgBox(_illocallinit(index).name)
+        'TODO : Check init value .
+        localinit.add_local_init(clinecodestruc(1).value, clinecodestruc(3).value)
     End Sub
 
     Public Function get_target_info(clinecodestruc As xmlunpkd.linecodestruc) As lexer.targetinf
