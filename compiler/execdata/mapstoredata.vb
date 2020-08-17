24fa11e7835def0704edcd3e753c6c8bed1bebb7
Amin Badin <amin@yo-lang.org>
2020-08-16 22:52:31 -0700

    New Methods :: find(..) & findkey(...)

--- compiler/execdata/mapstoredata.vb
+++ compiler/execdata/mapstoredata.vb
@@ -1,4 +1,9 @@
 ﻿Public Class mapstoredata
+
+    Public Structure dataresult
+        Dim result As String
+        Dim issuccessful As Boolean
+    End Structure
     Private keys, values As ArrayList
     Public Sub New()
         keys = New ArrayList
@@ -12,6 +17,34 @@
 
     Public Sub add(key As String, value As String)
         keys.Add(key)
-        values.Add(key)
+        values.Add(value)
     End Sub
+
+    Public Function find(key As String) As dataresult
+        Dim endpoint As Integer = keys.Count - 1
+        Dim result As New dataresult
+        result.issuccessful = False
+        For index = 0 To endpoint
+            If keys.Item(index) = key Then
+                result.result = values(index)
+                result.issuccessful = True
+                Return result
+            End If
+        Next
+        Return result
+    End Function
+
+    Public Function findkey(value As String) As dataresult
+        Dim endpoint As Integer = keys.Count - 1
+        Dim result As New dataresult
+        result.issuccessful = False
+        For index = 0 To endpoint
+            If values.Item(index) = value Then
+                result.result = keys(index)
+                result.issuccessful = True
+                Return result
+            End If
+        Next
+        Return result
+    End Function
 End Class
