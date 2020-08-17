b3909e8ac1d4dca4b01e372e27487f04f6496255
Amin Badin <amin@yo-lang.org>
2020-08-16 22:38:23 -0700

    init yo common data type

---
+++ compiler/initproc/initcommondatatype.vb
@@ -0,0 +1,9 @@
+﻿Public Class initcommondatatype
+
+    Friend Shared cdtype As New mapstoredata
+    Friend Shared Sub init_common_data_type()
+        For index = 0 To conrex.yocommondatatype.Length - 1
+            cdtype.add(conrex.yocommondatatype(index), conrex.msilcommondatatype(index))
+        Next
+    End Sub
+End Class
