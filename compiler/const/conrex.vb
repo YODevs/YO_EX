df8a5432e57bc082a07240bfd0fa3185010d6a36
Amin Badin <amin@yo-lang.org>
2020-08-18 11:19:06 -0700

    New CIL Type :: obj added.

--- compiler/const/conrex.vb
+++ compiler/const/conrex.vb
@@ -1,6 +1,6 @@
 ﻿Public Class conrex
-    Public Shared yocommondatatype() As String = {"bool", "i8", "i16", "i32", "i64", "i128", "ui8", "ui16", "ui32", "u64", "str", "chr", "f32", "f64"}
-    Public Shared msilcommondatatype() As String = {"bool", "int8", "int16", "int32", "int64", "valuetype [mscorlib]System.Decimal", "uint8", "uint16", "uint32", "uint64", "string", "char", "single", "double"}
+    Public Shared yocommondatatype() As String = {"bool", "i8", "i16", "i32", "i64", "i128", "ui8", "ui16", "ui32", "u64", "str", "chr", "f32", "f64", "obj"}
+    Public Shared msilcommondatatype() As String = {"bool", "int8", "int16", "int32", "int64", "valuetype [mscorlib]System.Decimal", "uint8", "uint16", "uint32", "uint64", "string", "char", "single", "double", "object"}
     Public Shared specificxmlchar() As Char = {">", "<", "'", """", "&"}
     Private Shared rand As New Random
     Public Shared specificrandomnumber As Integer = rand.Next(10000, 99999)
