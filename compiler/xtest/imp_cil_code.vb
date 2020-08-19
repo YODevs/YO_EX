9044772750ef6ce132017d0ab494ebd862abb937
Amin Badin <amin@yo-lang.org>
2020-08-18 23:01:32 -0700

    New Class :: Import test code in method.

---
+++ compiler/xtest/imp_cil_code.vb
@@ -0,0 +1,15 @@
+﻿Public Class imp_cil_code
+
+    Const codestr As String = "ldloc user
+call void [mscorlib]System.Console::WriteLine(string)
+ldloc fam
+call void [mscorlib]System.Console::WriteLine(string)
+ldloc val
+call void [mscorlib]System.Console::WriteLine(string)
+"
+    Friend Shared Sub import_test_code(ByRef funcdt As ilformat._ilmethodcollection)
+        For Each cline In codestr.Split(vbCr)
+            funcdt.codes.Add(cline.TrimStart)
+        Next
+    End Sub
+End Class
