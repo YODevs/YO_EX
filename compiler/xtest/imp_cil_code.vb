9e4ffb5c715036ecc6576ec2edbea2c7ecbc5e37
Amin Badin <amin@yo-lang.org>
2020-08-22 13:02:32 -0700

    Update imp_cil_code.vb

--- compiler/xtest/imp_cil_code.vb
+++ compiler/xtest/imp_cil_code.vb
@@ -1,14 +1,7 @@
 ﻿Public Class imp_cil_code
 
-    Const codestr As String = "ldloc user
-call void [mscorlib]System.Console::WriteLine(string)
-ldloc fam
-call void [mscorlib]System.Console::WriteLine(string)
-ldloc val
-call void [mscorlib]System.Console::WriteLine(string)
-ldloc pi
-call void [mscorlib]System.Console::WriteLine(string)
-"
+    Const codestr As String = "ldloc age
+call void [mscorlib]System.Console::WriteLine(int32)"
     Friend Shared Sub import_test_code(ByRef funcdt As ilformat._ilmethodcollection)
         For Each cline In codestr.Split(vbCr)
             funcdt.codes.Add(cline.TrimStart)
