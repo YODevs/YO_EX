226619b2fd1ca1b98959ec53d701297c5fd0f3de
Amin Badin <amin@yo-lang.org>
2020-08-22 22:53:50 -0700

    New Fields :: spdustrcommand , spdustrreact

--- compiler/const/conrex.vb
+++ compiler/const/conrex.vb
@@ -2,6 +2,10 @@
     Public Shared yocommondatatype() As String = {"bool", "i8", "i16", "i32", "i64", "i128", "ui8", "ui16", "ui32", "u64", "str", "char", "f32", "f64", "obj"}
     Public Shared msilcommondatatype() As String = {"bool", "int8", "int16", "int32", "int64", "valuetype [mscorlib]System.Decimal", "uint8", "uint16", "uint32", "uint64", "string", "char", "single", "double", "object"}
     Public Shared specificxmlchar() As Char = {">", "<", "'", """", "&"}
+    Public Shared spdustrcommand() As String = {"#nl", "#bs", "#tb", "#lf", "#cr", "#qu", "#co"}
+    Public Shared spdustrreact() As String = {"\r\n", Chr(8), Chr(9), "\n", "\r", "\""", "\'"}
+
+
     Private Shared rand As New Random
     Public Shared specificrandomnumber As Integer = rand.Next(10000, 99999)
     Public Const TITLE As String = "[ YO Lang ]"
