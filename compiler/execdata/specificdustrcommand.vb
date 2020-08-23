ab9de63b195fdaf7d5e84743fb3138f4b8e5049d
Amin Badin <amin@yo-lang.org>
2020-08-22 22:53:15 -0700

    New Class ::  specificdustrcommand

---
+++ compiler/execdata/specificdustrcommand.vb
@@ -0,0 +1,27 @@
+﻿Public Class specificdustrcommand
+    Public Shared spdustr As mapstoredata
+    Friend Shared Sub init()
+        spdustr = New mapstoredata
+        For index = 0 To conrex.spdustrcommand.Length - 1
+            spdustr.add(conrex.spdustrcommand(index), conrex.spdustrreact(index))
+        Next
+    End Sub
+
+    Friend Shared Sub get_specific_dustr_command(ByRef value As String)
+        If value.Contains("#") Then
+            For index = 0 To conrex.spdustrcommand.Length - 1
+                Dim resultspdustr As mapstoredata.dataresult = spdustr.find(conrex.spdustrcommand(index))
+                If value.Contains(conrex.spdustrcommand(index)) Then
+                    If resultspdustr.issuccessful Then
+                        value = value.Replace(conrex.spdustrcommand(index), resultspdustr.result)
+                    End If
+                End If
+
+                If value.Contains("#") = False Then Return
+            Next
+        Else
+            Return
+        End If
+    End Sub
+
+End Class
