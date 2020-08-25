43a89cbcb9e952b1845cb1627c336532238249ea
Amin Badin <amin@yo-lang.org>
2020-08-25 01:06:48 -0700

    New Method :: assi_str(...) , assigment string value

---
+++ compiler/ilgen/ilopt.vb
@@ -0,0 +1,20 @@
+﻿Public Class ilopt
+
+    Dim _ilmethod As ilformat._ilmethodcollection
+    Public Sub New(ilmethod As ilformat._ilmethodcollection)
+        Me._ilmethod = ilmethod
+    End Sub
+
+    Public Function assi_str(varname As String, clinecodestruc As xmlunpkd.linecodestruc) As ilformat._ilmethodcollection
+        Select Case clinecodestruc.tokenid
+            Case tokenhared.token.TYPE_DU_STR
+                cil.load_string(_ilmethod.codes, clinecodestruc.value)
+            Case tokenhared.token.TYPE_CO_STR
+                cil.load_string(_ilmethod.codes, clinecodestruc.value)
+        End Select
+
+        cil.set_stack_local(_ilmethod.codes, varname)
+
+        Return _ilmethod
+    End Function
+End Class
