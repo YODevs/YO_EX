932af3445e7eb9d5d9b8bdd70fd42acf9285284f
Amin Badin <amin@yo-lang.org>
2020-08-25 22:20:12 -0700

    New Method :: ldc_i_checker(...) , check use ldc.i4 or ldc.i8

---
+++ compiler/yogeb/servinterface.vb
@@ -0,0 +1,12 @@
+﻿Public Class servinterface
+    Friend Shared Sub ldc_i_checker(ByRef codes As ArrayList, value As Object, Optional convtoi8 As Boolean = False)
+        If value.ToString.Length > 10 AndAlso CDec(value) > Int32.MaxValue Or CDec(value) < Int32.MinValue Then
+            cil.push_int64_onto_stack(codes, CDec(value))
+        Else
+            cil.push_int32_onto_stack(codes, CDec(value))
+            If convtoi8 Then
+                cil.conv_to_int64(codes)
+            End If
+        End If
+    End Sub
+End Class
