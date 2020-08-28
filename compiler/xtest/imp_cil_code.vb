b4a1dbfcf7e8d978c2b3c81fd082f80933e28a99
Amin Badin <amin@yo-lang.org>
2020-08-28 08:47:02 -0700

    Update imp_cil_code.vb

--- compiler/xtest/imp_cil_code.vb
+++ compiler/xtest/imp_cil_code.vb
@@ -1,7 +1,7 @@
 ﻿Public Class imp_cil_code
 
-    Const codestr As String = "ldloc age
-call void [mscorlib]System.Console::WriteLine(int32)"
+    Const codestr As String = "
+"
     Friend Shared Sub import_test_code(ByRef funcdt As ilformat._ilmethodcollection)
         For Each cline In codestr.Split(vbCr)
             funcdt.codes.Add(cline.TrimStart)
