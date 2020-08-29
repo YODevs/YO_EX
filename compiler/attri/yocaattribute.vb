5c2bc79c5ff7e811c5fd277ad34c2bb4d5d1b8da
Amin Badin <amin@yo-lang.org>
2020-08-28 21:39:12 -0700

    yoca attribute structures

---
+++ compiler/attri/yocaattribute.vb
@@ -0,0 +1,18 @@
+﻿Public Class yocaattribute
+    ''' <summary>
+    ''' Configure options for compiler.
+    ''' </summary>
+    Structure cfg
+        Dim _cilinject As Boolean
+    End Structure
+
+    Structure yoattribute
+        Dim _cfg As cfg
+    End Structure
+
+    Structure resultattribute
+        Dim typeattribute As String
+        Dim fieldattribute As String
+        Dim valueattribute As String
+    End Structure
+End Class
