25950c18630125d33295beddab10056447c50b8c
Amin Badin <amin@yo-lang.org>
2020-08-31 01:57:37 -0700

    Set value by expression

--- compiler/ilgen/ilopt.vb
+++ compiler/ilgen/ilopt.vb
@@ -31,11 +31,12 @@
 
     Public Function assi_int(varname As String, clinecodestruc As xmlunpkd.linecodestruc, datatype As String) As ilformat._ilmethodcollection
         Dim convtoi8 As Boolean = servinterface.is_i8(datatype)
+
         Select Case clinecodestruc.tokenid
             Case tokenhared.token.TYPE_INT
                 servinterface.ldc_i_checker(_ilmethod.codes, clinecodestruc.value, convtoi8)
             Case tokenhared.token.TYPE_FLOAT
-                servinterface.ldc_i_checker(_ilmethod.codes, CInt(clinecodestruc.value), convtoi8)
+                servinterface.ldc_i_checker(_ilmethod.codes, clinecodestruc.value, convtoi8)
             Case tokenhared.token.IDENTIFIER
                 If assignmentcommondatatype.check_locals_init(_ilmethod.name, clinecodestruc.value, _ilmethod.locallinit, datatype) Then
                     cil.load_local_variable(_ilmethod.codes, clinecodestruc.value)
@@ -43,7 +44,9 @@
             'let value : str = NULL
             Case tokenhared.token.NULL
                 cil.push_null_reference(_ilmethod.codes)
-
+            Case tokenhared.token.EXPRESSION
+                Dim expr As New expressiondt(_ilmethod, "i32")
+                _ilmethod = expr.parse_expression_data(clinecodestruc.value, convtoi8)
             Case Else
                 'Set Error 
         End Select
