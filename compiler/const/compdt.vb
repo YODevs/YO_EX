cce819171f2c651f9c4e9bcff17820b2bb508d31
Amin Badin <amin@yo-lang.org>
2020-08-30 22:19:17 -0700

    New Field :: expressionact() , The four basic mathematical operations

--- compiler/const/compdt.vb
+++ compiler/const/compdt.vb
@@ -1,3 +1,4 @@
 ﻿Public Class compdt
     Friend Shared i8cmtypes() As String = {"int64", "uint64", "valuetype [mscorlib]System.Decimal"}
+    Friend Shared expressionact() As String = {"+", "-", "/", "*"}
 End Class
