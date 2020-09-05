03ea917115009ac6b88ca0fb8a3eefcd636b9180
Amin Badin <amin@yo-lang.org>
2020-09-05 01:50:06 -0700

    New Fields :: expressionactopt() , set action expression il code

--- compiler/const/compdt.vb
+++ compiler/const/compdt.vb
@@ -1,4 +1,5 @@
 ﻿Public Class compdt
     Friend Shared i8cmtypes() As String = {"int64", "uint64", "valuetype [mscorlib]System.Decimal"}
     Friend Shared expressionact() As String = {"+", "-", "/", "*"}
+    Friend Shared expressionactopt() As String = {"add", "sub", "div", "mul"}
 End Class
