60670fb86340b706d12be45f26fd5d119e9aeeac
Amin Badin <amin@yo-lang.org>
2020-09-12 23:43:34 -0700

    New Method :: set_local_init() , import a locals init into the methods.

---
+++ compiler/ilgen/illocalsinit.vb
@@ -0,0 +1,9 @@
+﻿Public Class illocalsinit
+
+    Friend Shared Sub set_local_init(ByRef funcdt As ilformat._ilmethodcollection, locinit As ilformat._illocalinit)
+        'Check local init name
+        Dim funcdtlen As Integer = funcdt.locallinit.Length
+        Array.Resize(funcdt.locallinit, funcdtlen + 1)
+        funcdt.locallinit(funcdtlen) = locinit
+    End Sub
+End Class
