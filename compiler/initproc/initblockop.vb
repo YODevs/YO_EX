59f37c50ff72016899f2e06cb1b2fa501b66e010
Amin Badin <amin@yo-lang.org>
2020-09-16 02:58:53 -0700

    Initial blockopallowlist , List of commands that are allowed to block code.

---
+++ compiler/initproc/initblockop.vb
@@ -0,0 +1,12 @@
+﻿Public Class initblockop
+    Friend Shared blockopallowlist As New mapstoredata
+    Friend Shared Sub init_blockop_allow_list()
+        For index = 0 To compdt.blockopallow.Length - 1
+            blockopallowlist.add([Enum].GetName(GetType(tokenhared.token), compdt.blockopallow(index)), compdt.blockopallow(index))
+        Next
+    End Sub
+
+    Friend Shared Function check_blockop_allow(tokenid As Integer) As Boolean
+        Return blockopallowlist.findkey(tokenid).issuccessful
+    End Function
+End Class
