4d5173916c0182f6d64632e2eb5c509ba260a04a
Amin Badin <amin@yo-lang.org>
2020-09-19 05:53:51 -0700

    New Method :: import_test_code2(...) , another method for inject code ilmethod.

--- compiler/xtest/imp_cil_code.vb
+++ compiler/xtest/imp_cil_code.vb
@@ -1,12 +1,28 @@
 ﻿Public Class imp_cil_code
 
     Const codestr As String = ""
+    Const codestr2 As String = "//ldstr ""Hello World!""
+//call void [mscorlib]System.Console::WriteLine(string)
+//ldloc.0
+//call void [mscorlib]System.Console::WriteLine(int32)
+ldloc.1
+call void [mscorlib]System.Console::WriteLine(int32)
+
+//call string [mscorlib]System.Console::ReadLine()
+//pop
+"
+
     Friend Shared Sub import_test_code(ByRef funcdt As ilformat._ilmethodcollection)
         For Each cline In codestr.Split(vbCr)
             funcdt.codes.Add(cline.TrimStart)
         Next
     End Sub
 
+    Friend Shared Sub import_test_code2(ByRef funcdt As ilformat._ilmethodcollection)
+        For Each cline In codestr2.Split(vbCr)
+            funcdt.codes.Add(cline.TrimStart)
+        Next
+    End Sub
     Friend Shared Sub import_test_local_init(ByRef funcdt As ilformat._ilmethodcollection)
         Dim locinit As New ilformat._illocalinit
         locinit.name = "age"
