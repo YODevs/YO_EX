51a904dd2c18877c2ea86c85dd6c48e6566f94f3
Amin Badin <amin@yo-lang.org>
2020-09-22 08:35:06 +0330

    New Method :: get_str_val(...) , get & auth string value of [yoattr.valueattribute]

--- compiler/attri/setinattr.vb
+++ compiler/attri/setinattr.vb
@@ -3,6 +3,8 @@
     Public Shared Sub init(ByRef yoattr As yocaattribute.yoattribute)
         yoattr._cfg._cilinject = False
         yoattr._cfg._optimize_expression = True
+
+        yoattr._app._classname = compdt.yomainclass
     End Sub
 
     Public Shared Function get_bool_val(yoattr As yocaattribute.resultattribute, path As String) As Boolean
@@ -19,4 +21,22 @@
         End If
         Return False
     End Function
+
+    Public Shared Function get_str_val(yoattr As yocaattribute.resultattribute, path As String) As String
+        Dim expression As String = yoattr.valueattribute
+        If expression.StartsWith(conrex.COSTR) AndAlso expression.EndsWith(conrex.COSTR) Then
+            authfunc.rem_fr_and_en(expression)
+            Return expression
+        ElseIf expression.StartsWith(conrex.DUSTR) AndAlso expression.EndsWith(conrex.DUSTR) Then
+            authfunc.rem_fr_and_en(expression)
+            Return expression
+        Else
+            'Set Error
+            dserr.args.Add(expression)
+            dserr.args.Add("str")
+            dserr.new_error(conserr.errortype.EXPLICITCONVERSION, attr.lastlinecinf.line, path, authfunc.get_line_error(path, attr.lastlinecinf, yoattr.valueattribute))
+        End If
+        Return False
+    End Function
+
 End Class
