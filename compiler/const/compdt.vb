5846cf8c117e2efc86d4bf833c97d3d0b4801940
Amin Badin <amin@yo-lang.org>
2020-09-22 08:36:35 +0330

    New Field :: yomainclass , For use in the default class name

--- compiler/const/compdt.vb
+++ compiler/const/compdt.vb
@@ -4,4 +4,5 @@
     Friend Shared expressionactopt() As String = {"add", "sub", "div", "mul"}
     Friend Shared blockopallow() As tokenhared.token = {tokenhared.token.TO}
     Friend Const flagperfix As String = "YO_Flag_"
+    Friend Const yomainclass As String = "YO_Main"
 End Class
