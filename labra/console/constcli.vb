f0038ded967c5becee0c28b94007ba8d6e0754e2
Amin Badin <amin@yo-lang.org>
2020-09-23 10:01:08 +0330

    New Class :: texts in cli

---
+++ labra/console/constcli.vb
@@ -0,0 +1,4 @@
+﻿Public Class constcli
+    Friend Shared maincohelp As String = "Labra [Dependent on YO] Version [%version%]
+Copyright (C) YODevs . All rights reserved.".Replace("%version%", conrex.VER)
+End Class
