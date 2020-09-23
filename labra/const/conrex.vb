b96f4cec7e3e3344ce6ae2d63f63eb719d45f58e
Amin Badin <amin@yo-lang.org>
2020-09-23 10:00:34 +0330

    New Class :: Labra constants

---
+++ labra/const/conrex.vb
@@ -0,0 +1,11 @@
+﻿Public Class conrex
+    Public Const TITLE As String = "[ Labra ]"
+    Public Shared VER As String = My.Application.Info.Version.ToString
+    Public Shared APPDIR As String = My.Application.Info.DirectoryPath
+    Public Shared ENVCURDIR As String = Environment.CurrentDirectory
+    Public Shared COMMANDLINE As String = Environment.CommandLine
+    Public Const YOFORMAT As String = ".yo"
+    Public Const NULL As String = Nothing
+    Public Const SPACE As Char = " "
+    Public Const DOT As Char = "."
+End Class
