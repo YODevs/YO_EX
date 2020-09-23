d702cf3c8ca8da9d8703371569ea8fa8a353f7c6
Amin Badin <amin@yo-lang.org>
2020-09-23 11:48:51 +0330

    New Method :: goto_cmdln(...)

--- labra/console/main.vb
+++ labra/console/main.vb
@@ -2,8 +2,17 @@
 
     Sub Main(args() As String)
         cli.init_cli(args.Length = 0)
-
-        Threading.Thread.Sleep(1000)
+        If args.Length = 0 Then
+            goto_cmdln(args)
+        Else
+            parseargs.parse_data(args)
+        End If
     End Sub
 
+    Sub goto_cmdln(args() As String)
+        While True
+            interactioncmd.get_interaction(args)
+            parseargs.parse_data(args)
+        End While
+    End Sub
 End Module
