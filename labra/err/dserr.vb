a35d05875512804da1b098257ea1fec408ac86c0
Amin Badin <amin@yo-lang.org>
2020-09-24 15:13:42 +0330

    New Method :: set_error(...)

---
+++ labra/err/dserr.vb
@@ -0,0 +1,10 @@
+﻿Public Class dserr
+    Public Shared Sub set_error(title As String, caption As String)
+        Dim peconsolecolor As Int16 = Console.ForegroundColor
+        Console.ForegroundColor = System.ConsoleColor.DarkRed
+        Console.Write("$Error> " & title & " > ")
+        Console.ForegroundColor = System.ConsoleColor.Gray
+        Console.Write(caption)
+        Console.ForegroundColor = peconsolecolor
+    End Sub
+End Class
