9e169a1d240ce0dfa7096d1864f6c74f3ee6727a
Amin Badin <amin@yo-lang.org>
2020-09-24 15:13:21 +0330

    Invoke init_command_struct , initial commands.

--- labra/console/main.vb
+++ labra/console/main.vb
@@ -1,6 +1,7 @@
 ﻿Module LabraMod
 
     Sub Main(args() As String)
+        cmdlnproc.init_command_struct()
         cli.init_cli(args.Length = 0)
         If args.Length = 0 Then
             goto_cmdln(args)
@@ -10,6 +11,7 @@
     End Sub
 
     Sub goto_cmdln(args() As String)
+        Console.Write(vbCrLf)
         While True
             interactioncmd.get_interaction(args)
             parseargs.parse_data(args)
