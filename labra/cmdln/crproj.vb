0792ce933eadbf2a7e0da50dd21f50ab42f8a11e
Amin Badin <amin@yo-lang.org>
2020-09-26 11:06:28 +0330

    New Method :: get_type_of_projec()

--- labra/cmdln/crproj.vb
+++ labra/cmdln/crproj.vb
@@ -1,11 +1,24 @@
 ﻿Public Class crproj
+
+    Public Structure _projstruct
+        Dim assemblyname As String
+        Dim typeproject As String
+    End Structure
+
     Dim yoda As YODA.YODA_Format
+    Dim proj As _projstruct
+    Dim typesitem As ArrayList
     Public Sub New()
         yoda = New YODA.YODA_Format
+        proj = New _projstruct
+        typesitem = New ArrayList
+        typesitem.Add("Console [.exe]")
+        typesitem.Add("Library [.dll]")
     End Sub
 
     Public Sub init_project()
-        Dim asmname As String = get_assembly_name()
+        proj.assemblyname = get_assembly_name()
+        proj.typeproject = get_type_of_project()
     End Sub
 
     Private Function get_assembly_name() As String
@@ -13,9 +26,9 @@
         Dim asmname As String = String.Empty
         While checkit
             If asmname = String.Empty Then
-                Console.Write("Assembly name [ex:myapp]: ")
+                Console.Write("# Assembly name [ex:myapp]: ")
             Else
-                Console.Write(vbLf & "Assembly name [ex:myapp]: ")
+                Console.Write(vbLf & "# Assembly name [ex:myapp]: ")
             End If
             asmname = Console.ReadLine.Trim
             check_assembly_name(checkit, asmname)
@@ -46,4 +59,12 @@
             continueloop = False
         End If
     End Sub
+
+    Private Function get_type_of_project() As String
+        Console.Write(vbLf & "# Select project type:")
+        Dim menudata As String = yoda.WriteYODA(typesitem)
+        Dim tproj As String = YOOrderList.YOList.ShowMenu(menudata)
+        tproj = tproj.Remove(tproj.IndexOf("[")).Trim.ToLower
+        Return tproj
+    End Function
 End Class
