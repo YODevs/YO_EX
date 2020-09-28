175f6fa019839a3d2a9f154f2e3eedbb8c8562d3
Amin Badin <amin@yo-lang.org>
2020-09-28 10:54:35 +0330

    New Action :: get_interaction(...) , Receive commands from the user

---
+++ compiler/cli/interactioncmd.vb
@@ -0,0 +1,17 @@
+﻿Imports System.Text.RegularExpressions
+
+Public Class interactioncmd
+
+    Friend Shared Sub get_interaction(ByRef args() As String)
+        Console.Write(vbCrLf)
+        write_prefix_ch()
+        args = Regex.Split(Console.ReadLine(), conrex.SPACE)
+    End Sub
+
+    Friend Shared Sub write_prefix_ch()
+        Dim peconsolecolor As Int16 = Console.ForegroundColor
+        Console.ForegroundColor = System.ConsoleColor.DarkCyan
+        Console.Write(vbCr & "$YO CLI> ")
+        Console.ForegroundColor = peconsolecolor
+    End Sub
+End Class
