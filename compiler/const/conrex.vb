2ad143d6ab198e0bd9c53ad266ee57aad107aea5
Amin Badin <amin@yo-lang.org>
2020-09-29 12:56:29 +0330

    New Constant :: ENVCUDIR

--- compiler/const/conrex.vb
+++ compiler/const/conrex.vb
@@ -11,6 +11,7 @@
     Public Const TITLE As String = "[ YO Lang ]"
     Public Shared VER As String = My.Application.Info.Version.ToString
     Public Shared APPDIR As String = My.Application.Info.DirectoryPath
+    Public Shared ENVCURDIR As String = Environment.CurrentDirectory
     Public Const YOFORMAT As String = ".yo"
     Public Const NULL As String = Nothing
     Public Const SPACE As Char = " "
