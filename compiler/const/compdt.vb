dcc10a0c508ee56d005b5697596c620354181abc
Amin Badin <amin@yo-lang.org>
2020-10-03 10:47:40 +0330

    New Constant :: DISPLAYILASMOUTPUT , DISPLAYTOKENWLEX

--- compiler/const/compdt.vb
+++ compiler/const/compdt.vb
@@ -3,6 +3,8 @@
     Friend Shared expressionact() As String = {"+", "-", "/", "*"}
     Friend Shared expressionactopt() As String = {"add", "sub", "div", "mul"}
     Friend Shared blockopallow() As tokenhared.token = {tokenhared.token.TO}
-    Friend Const flagperfix As String = "YO_Flag_"
-    Friend Const yomainclass As String = "YO_Main"
+    Friend Const FLAGPERFIX As String = "YO_Flag_"
+    Friend Const YOMAINCLASS As String = "YO_Main"
+    Friend Const DISPLAYILASMOUTPUT As Boolean = False
+    Friend Const DISPLAYTOKENWLEX As Boolean = False
 End Class
