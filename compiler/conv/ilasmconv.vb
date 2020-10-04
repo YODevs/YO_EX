5b412f5057023e3681d924f64aa48154df93b1f9
Amin Badin <amin@yo-lang.org>
2020-10-04 22:41:54 +0330

    New Method :: get_standard_error(...) , remove_il_data(...)

--- compiler/conv/ilasmconv.vb
+++ compiler/conv/ilasmconv.vb
@@ -4,8 +4,10 @@ Imports System.Text
 Public Class ilasmconv
     Private ilasmoutputdata As StringBuilder
     Private ilasmparameter As ilasmparam
-    Public Sub New(inputparam As ilasmparam)
+    Private path As String
+    Public Sub New(inputparam As ilasmparam, path As String)
         ilasmparameter = inputparam
+        Me.path = path
         init()
     End Sub
 
@@ -27,6 +29,7 @@ Public Class ilasmconv
         ilasmproc.Start()
         ilasmproc.BeginOutputReadLine()
         ilasmproc.WaitForExit()
+
         If compdt.DISPLAYILASMOUTPUT Then Console.WriteLine(ilasmoutputdata.ToString())
         coutputdata.write_file_data("conv_info.txt", ilasmoutputdata.ToString())
 
@@ -45,9 +48,7 @@ Public Class ilasmconv
             Console.ForegroundColor = ConsoleColor.DarkRed
             Console.WriteLine(vbTab & "***** FAILURE *****")
             Console.ForegroundColor = ciden
-            Dim iloutput As String = ilasmoutputdata.ToString().Trim
-            iloutput = iloutput.Remove(0, iloutput.IndexOf(vbLf)).Trim
-            iloutput = iloutput.Remove(iloutput.IndexOf("***** FAILURE *****")).Trim
+            Dim iloutput As String = get_standard_error(ilasmproc.StandardError)
             Console.WriteLine(iloutput)
         End If
     End Sub
@@ -59,5 +60,24 @@ Public Class ilasmconv
     Private Sub init()
         ilasmparameter.add_param("/NOLOGO")
         ilasmparameter.add_param("/OPTIMIZE")
+    End Sub
+
+    Private Function get_standard_error(ptrstderr As StreamReader) As String
+        Dim errtext As String = String.Empty
+        While True
+            Dim rte As String = ptrstderr.ReadToEnd()
+            If rte = String.Empty Then
+                Exit While
+            Else
+                errtext &= rte & vbLf
+            End If
+        End While
+        remove_il_data(errtext)
+        Return errtext.Trim
+    End Function
+
+    Private Sub remove_il_data(ByRef errtext As String)
+        Dim capt As String = path.Remove(path.LastIndexOf(conrex.DOT) + 1)
+        errtext = errtext.Replace(capt, conrex.NULL)
     End Sub
 End Class
