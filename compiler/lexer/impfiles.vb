f29ca8a8b9296ed9118beb7e7b0f383b388c3eaa
Amin Badin <amin@yo-lang.org>
2020-10-06 09:10:42 +0330

    New Action :: Replace 'get_colc_yo_files(...)' method & Legacy method 'get_yo_files'

--- compiler/lexer/impfiles.vb
+++ compiler/lexer/impfiles.vb
@@ -5,7 +5,7 @@ Public Class impfiles
         If Directory.Exists(dir) Then
             Dim files As New ArrayList
             Dim ilgen As ilgencode
-            If get_yo_files(dir, files) Then
+            If get_colc_yo_files(dir, files, True) Then
                 Dim tknfmtclass(files.Count - 1) As tknformat._class
                 procresult.set_state("lex")
                 For index = 0 To files.Count - 1
@@ -23,6 +23,13 @@ Public Class impfiles
         End If
     End Sub
 
+
+    ''' <summary>
+    ''' Simple get file [ just in master route ]
+    ''' </summary>
+    ''' <param name="dir"></param>
+    ''' <param name="files"></param>
+    ''' <returns></returns>
     Friend Shared Function get_yo_files(dir As String, ByRef files As ArrayList) As Boolean
 
         If Directory.GetFiles(dir).Count = 0 Then
@@ -46,4 +53,30 @@ Public Class impfiles
         End If
         Return True
     End Function
+
+    Friend Shared Function get_colc_yo_files(dir As String, ByRef files As ArrayList, Optional head As Boolean = False) As Boolean
+        Dim file As String = Nothing
+        For index = 0 To Directory.GetFiles(dir).Count - 1
+            file = Directory.GetFiles(dir)(index)
+            If file.ToLower.EndsWith(conrex.YOFORMAT) Then
+                files.Add(file)
+            End If
+        Next
+
+        Dim route As String = Nothing
+        For index = 0 To Directory.GetDirectories(dir).Count - 1
+            route = Directory.GetDirectories(dir)(index)
+            get_colc_yo_files(route, files)
+        Next
+
+        If head Then
+            If files.Count = 0 Then
+                dserr.new_error(conserr.errortype.YOFILENOTFOUND, -1, Nothing, "path => " & dir)
+                Return False
+            End If
+            Return True
+        End If
+        Return True
+    End Function
+
 End Class
