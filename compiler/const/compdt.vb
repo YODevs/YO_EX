8097652e6213e403fe8ecb27ce0383e1af5b706f
Amin Badin <amin@yo-lang.org>
2020-10-06 08:49:03 +0330

    New Field :: errcap() : str

--- compiler/const/compdt.vb
+++ compiler/const/compdt.vb
@@ -2,6 +2,7 @@
     Friend Shared i8cmtypes() As String = {"int64", "uint64", "valuetype [mscorlib]System.Decimal"}
     Friend Shared expressionact() As String = {"+", "-", "/", "*"}
     Friend Shared expressionactopt() As String = {"add", "sub", "div", "mul"}
+    Friend Shared errcap() As String = {"Error:", "error code="}
     Friend Shared blockopallow() As tokenhared.token = {tokenhared.token.TO}
     Friend Const FLAGPERFIX As String = "YO_Flag_"
     Friend Const YOMAINCLASS As String = "YO_Main"
