b9292e00893c9258422ed0cacb4808010bd5d6eb
Amin Badin <amin@yo-lang.org>
2020-10-09 12:43:52 +0330

    New Class :: liststoredata , save data to a list

---
+++ compiler/execdata/liststoredata.vb
@@ -0,0 +1,44 @@
+﻿Public Class liststoredata
+    Public Structure dataresult
+        Dim result As String
+        Dim issuccessful As Boolean
+    End Structure
+    Public items As ArrayList
+    Public Sub New()
+        items = New ArrayList
+    End Sub
+
+    Public Sub import_collection(itemlist As ArrayList)
+        items = itemlist
+    End Sub
+
+    Public Sub add(value As String)
+        items.Add(value)
+    End Sub
+
+    Public Function find(value As String) As Boolean
+        Dim endpoint As Integer = items.Count - 1
+        Dim result As New dataresult
+        result.issuccessful = False
+        For index = 0 To endpoint
+            If items(index) = value Then
+                Return True
+            End If
+        Next
+        Return False
+    End Function
+
+    Public Function contains_item(value As String) As dataresult
+        Dim endpoint As Integer = items.Count - 1
+        Dim result As New dataresult
+        result.issuccessful = False
+        For index = 0 To endpoint
+            If items(index).ToString.Contains(value) Then
+                result.result = items(index)
+                result.issuccessful = True
+                Return result
+            End If
+        Next
+        Return result
+    End Function
+End Class
