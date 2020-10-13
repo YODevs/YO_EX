b323662633d5ec0730b76037efd42d1a3c89f905
Amin Badin <amin@yo-lang.org>
2020-10-13 10:28:54 +0330

    New Class :: Infinity Loop (loop{})

---
+++ compiler/yogeb/infloop.vb
@@ -0,0 +1,34 @@
+﻿Public Class infloop
+    Private _ilmethod As ilformat._ilmethodcollection
+    Private endbranchlabel, bodybranchlabel As String
+    Public Sub New(ilmethod As ilformat._ilmethodcollection)
+        Me._ilmethod = ilmethod
+    End Sub
+
+    Public Function set_infinity_loop(clinecodestruc() As xmlunpkd.linecodestruc, ByRef _illocalinit() As ilformat._illocalinit, ByRef localinit As localinitdata) As ilformat._ilmethodcollection
+        syntaxchecker.check_statement(clinecodestruc, syntaxloader.statements.LOOP)
+        endbranchlabel = lngen.get_line_prop("loopexit")
+        bodybranchlabel = lngen.get_line_prop("loopbody")
+
+        set_jmper()
+
+        lngen.set_direct_label(bodybranchlabel, _ilmethod.codes)
+
+        Dim iltrans As New iltranscore(ilbodybulider.path, clinecodestruc(1).value, _illocalinit, localinit)
+        iltrans.gen_transpile_code(_ilmethod, False)
+        _illocalinit = _ilmethod.locallinit
+
+        set_condition()
+        lngen.set_direct_label(endbranchlabel, _ilmethod.codes)
+
+        Return _ilmethod
+    End Function
+
+    Private Sub set_condition()
+        cil.branch_to_target(_ilmethod.codes, bodybranchlabel)
+    End Sub
+
+    Private Sub set_jmper()
+        stjmper.set_new_jmper(tokenhared.token.LOOP, endbranchlabel, bodybranchlabel)
+    End Sub
+End Class
