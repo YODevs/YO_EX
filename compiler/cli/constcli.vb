f65f634f41b120e7b4ddac31fb687d0f53d75872
Amin Badin <amin@yo-lang.org>
2020-10-27 13:27:22 +0330

    New Field :: helpheader

--- compiler/cli/constcli.vb
+++ compiler/cli/constcli.vb
@@ -2,4 +2,5 @@
     Friend Shared maincohelp As String = "YO Lang Compiler Version [%version%]
 Copyright (C) YODevs . All rights reserved.".Replace("%version%", conrex.VER)
 
+    Friend Shared helpheader As String = "--Select one of the menu items below : "
 End Class
