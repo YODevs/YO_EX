908ad676cbf2486e964e85e8c3854073ebf4bf8d
Amin Badin <amin@yo-lang.org>
2020-11-02 14:07:32 +0330

    New Action :: Tolower search in find(...) , findkey(...)

--- compiler/execdata/mapstoredata.vb
+++ compiler/execdata/mapstoredata.vb
@@ -20,31 +20,53 @@
         values.Add(value)
     End Sub
 
-    Public Function find(key As String) As dataresult
+    Public Function find(key As String, Optional tolowersearch As Boolean = False) As dataresult
         Dim endpoint As Integer = keys.Count - 1
         Dim result As New dataresult
         result.issuccessful = False
-        For index = 0 To endpoint
-            If keys.Item(index) = key Then
-                result.result = values(index)
-                result.issuccessful = True
-                Return result
-            End If
-        Next
+        If tolowersearch Then
+            key = key.ToLower
+            For index = 0 To endpoint
+                If keys.Item(index).ToString.ToLower = key Then
+                    result.result = values(index)
+                    result.issuccessful = True
+                    Return result
+                End If
+            Next
+        Else
+            For index = 0 To endpoint
+                If keys.Item(index) = key Then
+                    result.result = values(index)
+                    result.issuccessful = True
+                    Return result
+                End If
+            Next
+        End If
         Return result
     End Function
 
-    Public Function findkey(value As String) As dataresult
+    Public Function findkey(value As String, Optional tolowersearch As Boolean = False) As dataresult
         Dim endpoint As Integer = keys.Count - 1
         Dim result As New dataresult
         result.issuccessful = False
-        For index = 0 To endpoint
-            If values.Item(index) = value Then
-                result.result = keys(index)
-                result.issuccessful = True
-                Return result
-            End If
-        Next
+        If tolowersearch Then
+            value = value.Trim
+            For index = 0 To endpoint
+                If values.Item(index).ToString.ToLower = value Then
+                    result.result = keys(index)
+                    result.issuccessful = True
+                    Return result
+                End If
+            Next
+        Else
+            For index = 0 To endpoint
+                If values.Item(index) = value Then
+                    result.result = keys(index)
+                    result.issuccessful = True
+                    Return result
+                End If
+            Next
+        End If
         Return result
     End Function
 End Class
