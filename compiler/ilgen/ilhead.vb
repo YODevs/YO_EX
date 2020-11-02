9dce5b321bd550e07e275fbed8628ffd672e706b
Amin Badin <amin@yo-lang.org>
2020-11-02 14:05:32 +0330

    Modify Class :: Change parameter type

--- compiler/ilgen/ilhead.vb
+++ compiler/ilgen/ilhead.vb
@@ -1,10 +1,10 @@
 ﻿Public Class ilhead
     Private ilcollection As ilformat.ildata
     Public asmheader(0) As ilformat._ilassemblyextern
-    Public Sub New(ilcol As ilformat.ildata, mdname As String)
+    Public Sub New(ilcol As ilformat.ildata, yoclassdt As tknformat._class)
         ilcollection = ilcol
         asmheader(0) = New ilformat._ilassemblyextern
-        init(mdname)
+        init(yoclassdt.name)
     End Sub
 
     Private Sub init(mdname As String)
