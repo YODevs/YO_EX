a4f7d410ee37359c018ea04b0e72e4117a651b86
Amin Badin <amin@yo-lang.org>
2020-11-04 10:21:24 +0330

    New Method :: Print tokens by xmlunpkd.linecodestruc ,  print_token()

--- compiler/xtest/coutputdata.vb
+++ compiler/xtest/coutputdata.vb
@@ -8,4 +8,10 @@ Public Class coutputdata
     Public Shared Sub write_file_data(filename As String, data As String)
         File.WriteAllText(My.Application.Info.DirectoryPath & "\" & filename, data)
     End Sub
+    Public Shared Sub print_token(clinecodestruc() As xmlunpkd.linecodestruc)
+        Console.WriteLine()
+        For index = 0 To clinecodestruc.Length - 1
+            Console.WriteLine("{0}-{1} :: {2}", clinecodestruc(index).tokenid, clinecodestruc(index).name, clinecodestruc(index).value)
+        Next
+    End Sub
 End Class
