f7954278e101751d33f24afcbe9f8b4f31388f01
Amin Badin <amin@yo-lang.org>
2020-11-04 18:31:35 +0330

    New Class :: Call & invoke simple internal func

---
+++ compiler/funcproc/funcste.vb
@@ -0,0 +1,20 @@
+﻿Public Class funcste
+    Friend Shared attribute As yocaattribute.yoattribute
+    Friend Shared Sub invoke_method(clinecodestruc() As xmlunpkd.linecodestruc, ByRef _ilmethod As ilformat._ilmethodcollection, funcresult As funcvalid._resultfuncvaild)
+        If funcresult.callintern Then
+            Dim classindex As Integer = funcdtproc.get_index_class(attribute._app._classname)
+            If classindex = -1 Then
+                'Class Not Found
+                Return
+            End If
+            Dim methodindex As Integer = funcdtproc.get_index_method(funcresult.clmethod, classindex)
+            If methodindex = -1 Then
+                MsgBox("Method Not Found")
+                'Method Not Found
+            End If
+
+            cil.call_intern_method(_ilmethod.codes, Nothing, attribute._app._classname, funcresult.clmethod, Nothing)
+        End If
+    End Sub
+
+End Class
