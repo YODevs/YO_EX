9c80d155a9984f2731d400a93c75d41900fc7114
Amin Badin <amin@yo-lang.org>
2020-11-06 20:09:09 +0330

    New Field :: Parameter Structure , _ilparameter

--- compiler/ilgen/ilformat.vb
+++ compiler/ilgen/ilformat.vb
@@ -17,13 +17,19 @@
         Dim accessible As _accessiblemethod
         Dim locallinit() As _illocalinit
         Dim returntype As String
-        'Parameters
+        Dim parameter() As _ilparameter
         Dim entrypoint As Boolean
         Dim maxstack As Int16
         Dim codes As ArrayList
         Dim line As ArrayList
     End Structure
-
+    Public Structure _ilparameter
+        Dim name As String
+        Dim datatype As String
+        Dim hasdefaultvalue As Boolean
+        Dim ispointer As Boolean
+        Dim clocalvalue() As xmlunpkd.linecodestruc
+    End Structure
     Public Structure _illocalinit
         Dim name As String
         Dim datatype As String
