f75a2c139b7ba533535cdfca6a440f6105bc9731
Amin Badin <amin@yo-lang.org>
2020-11-13 18:35:56 +0330

    New Method :: Import Parameters , import_parameter(...)

--- compiler/execdata/localinitdata.vb
+++ compiler/execdata/localinitdata.vb
@@ -1,17 +1,28 @@
 ﻿Public Class localinitdata
     Friend datatypelocal As mapstoredata
+    Friend datatypeparameter As mapstoredata
     Public Sub New()
         datatypelocal = New mapstoredata
+        datatypeparameter = New mapstoredata
     End Sub
 
     Public Function check_local_init(name As String) As Boolean
-        name = name.ToLower
         Return datatypelocal.find(name).issuccessful
     End Function
 
     Public Sub add_local_init(name As String, datatype As String)
-        name = name
-        datatype = datatype
         datatypelocal.add(name, datatype)
+    End Sub
+
+    Public Sub add_parameter(name As String, datatype As String)
+        datatypeparameter.add(name, datatype)
+    End Sub
+
+    Public Sub import_parameter(method As tknformat._method)
+        If IsNothing(method.parameters) = False Then
+            For index = 0 To method.parameters.Length - 1
+                add_parameter(method.parameters(index).name, method.parameters(index).ptype)
+            Next
+        End If
     End Sub
 End Class
