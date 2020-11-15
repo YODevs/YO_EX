0d5098be0fabe4076c5436819aa6317774d6691d
Amin Badin <amin@yo-lang.org>
2020-11-15 07:25:25 +0330

    New Method :: Return Statement , ret_to_route(...)

---
+++ compiler/ilgen/ilret.vb
@@ -0,0 +1,11 @@
+﻿Imports YOCA
+
+Public Class ilret
+    Friend Shared Sub ret_to_route(ByRef ilmethod As ilformat._ilmethodcollection, clinecodestruc() As xmlunpkd.linecodestruc)
+        'TODO : Return & Exit func
+        syntaxchecker.check_statement(clinecodestruc, syntaxloader.statements.RET)
+        Dim ldvar As New illdloc(ilmethod)
+        ldvar.load_single_in_stack(ilmethod.returntype, clinecodestruc(1))
+        cil.ret(ilmethod.codes)
+    End Sub
+End Class
