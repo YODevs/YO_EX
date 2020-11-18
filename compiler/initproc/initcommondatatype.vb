505203cbaf0a0fa0977e3d91fc64daf7fba5698c
Amin Badin <amin@yo-lang.org>
2020-11-18 07:34:44 +0330

    New Method :: Initial Pointer Bind , init_ptr_bind()

--- compiler/initproc/initcommondatatype.vb
+++ compiler/initproc/initcommondatatype.vb
@@ -6,4 +6,19 @@
             cdtype.add(conrex.yocommondatatype(index), conrex.msilcommondatatype(index))
         Next
     End Sub
+
+    Friend Shared Sub init_ptr_bind()
+        compdt.ptrinddata = New mapstoredata
+        compdt.ptrinddata.add("int8", "i1")
+        compdt.ptrinddata.add("int16", "i2")
+        compdt.ptrinddata.add("int32", "i4")
+        compdt.ptrinddata.add("int64", "i8")
+        compdt.ptrinddata.add("float32", "r4")
+        compdt.ptrinddata.add("float64", "r8")
+        compdt.ptrinddata.add("uint8", "u1")
+        compdt.ptrinddata.add("uint16", "u2")
+        compdt.ptrinddata.add("uint32", "u4")
+        compdt.ptrinddata.add("uint64", "u8")
+    End Sub
+
 End Class
