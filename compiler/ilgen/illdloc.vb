2fd53053a029e277e7cb933313f392a506a338e3
Amin Badin <amin@yo-lang.org>
2020-11-26 19:50:25 +0330

    New Method :: Load field , ld_field(...)

--- compiler/ilgen/illdloc.vb
+++ compiler/ilgen/illdloc.vb
@@ -110,9 +110,49 @@
         If IsNothing(_ilmethod.parameter) = False Then
             If ld_argument(nvar, _ilmethod, cargcodestruc, datatype) Then Return True
         End If
+        If IsNothing(ilasmgen.classdata.fields) = False Then
+            If ld_field(nvar, _ilmethod, cargcodestruc, datatype) Then Return True
+        End If
 
         dserr.args.Add(nvar)
         dserr.new_error(conserr.errortype.TYPENOTFOUND, cargcodestruc.line, ilbodybulider.path, "Method : " & _ilmethod.name & " - Unknown identifier : " & nvar & vbCrLf & authfunc.get_line_error(ilbodybulider.path, servinterface.get_target_info(cargcodestruc), cargcodestruc.value))
+        Return False
+    End Function
+
+    Friend Shared Function ld_field(nvar As String, ByRef _ilmethod As ilformat._ilmethodcollection, cargcodestruc As xmlunpkd.linecodestruc, datatype As String, Optional ByRef nactorcode As ArrayList = Nothing) As Boolean
+        If IsNothing(ilasmgen.classdata.fields) Then Return False
+        Dim nvartolower As String = nvar.ToLower
+        Dim pnvar As String = String.Empty
+        Dim pdatatype As String = String.Empty
+        For index = 0 To ilasmgen.classdata.fields.Length - 1
+            pnvar = ilasmgen.classdata.fields(index).name
+            If pnvar <> conrex.NULL AndAlso pnvar.ToLower = nvartolower Then
+                pdatatype = ilasmgen.classdata.fields(index).ptype
+                servinterface.is_common_data_type(pdatatype, pdatatype)
+                If pdatatype = datatype Then
+                    Dim classname As String = ilasmgen.classdata.attribute._app._classname
+                    If IsNothing(nactorcode) = False Then
+                        If ilasmgen.classdata.fields(index).modifier = "static" Then
+                            cil.load_static_field(nactorcode, pnvar, pdatatype, classname)
+                        Else
+                            cil.load_field(nactorcode, pnvar, pdatatype, classname)
+                        End If
+                    Else
+                        If ilasmgen.classdata.fields(index).modifier = "static" Then
+                            cil.load_static_field(_ilmethod.codes, pnvar, pdatatype, classname)
+                        Else
+                            cil.load_field(_ilmethod.codes, pnvar, pdatatype, classname)
+                        End If
+                    End If
+                    Return True
+                Else
+                    dserr.args.Add(ilasmgen.classdata.fields(index).ptype)
+                    dserr.args.Add(datatype)
+                    dserr.new_error(conserr.errortype.EXPLICITCONVERSION, cargcodestruc.line, ilbodybulider.path, "Method : " & _ilmethod.name & " - identifier [ Field ] : " & nvar & vbCrLf)
+                    Return False
+                End If
+            End If
+        Next
         Return False
     End Function
 
