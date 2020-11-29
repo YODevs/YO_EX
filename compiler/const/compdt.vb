00c5559a0e9c85af0a2d1f6f6d879ca663212dfb
Amin Badin <amin@yo-lang.org>
2020-11-29 13:06:39 +0330

    New Constant :: YOFORMATTEDSTRBYREGEX

--- compiler/const/compdt.vb
+++ compiler/const/compdt.vb
@@ -18,6 +18,8 @@
     Friend Const RUNCMDDELAY As Integer = 500
     Friend Const WAITILCODE As String = "call string [mscorlib]System.Console::ReadLine()
 pop"
+    Friend Const YOFORMATTEDSTRBYREGEX As String = "#{\w+}"
+
     Friend Const PARAM_IMPASSETS As String = "--import_assets"
     Friend Const PARAM_DEBUG As String = "--debug"
     Friend Const PARAM_DEBUG_IMPL As String = "--debug_impl"
