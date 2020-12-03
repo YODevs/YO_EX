3e046b05ac8716ea46ded653ee7ec57f3863196d
Amin Badin <amin@yo-lang.org>
2020-12-03 12:23:24 +0330

    New Fields :: VB Common Data-Types , plvbcommondatatype()

--- compiler/const/conrex.vb
+++ compiler/const/conrex.vb
@@ -1,5 +1,6 @@
 ﻿Public Class conrex
     Public Shared yocommondatatype() As String = {"bool", "i8", "i16", "i32", "i64", "i128", "u8", "u16", "u32", "u64", "str", "char", "f32", "f64", "obj"}
+    Public Shared plvbcommondatatype() As String = {"Boolean", "Int8", "Int16", "Int32", "Int64", "Decimal", "UInt8", "UInt16", "UInt32", "UInt64", "String", "Char", "Single", "Double", "Object"}
     Public Shared msilcommondatatype() As String = {"bool", "int8", "int16", "int32", "int64", "valuetype [mscorlib]System.Decimal", "uint8", "uint16", "uint32", "uint64", "string", "char", "float32", "float64", "object"}
     Public Shared specificxmlchar() As Char = {">", "<", "'", """", "&"}
     Public Shared spdustrcommand() As String = {"#nl", "#bs", "#tb", "#lf", "#cr", "#qu", "#co"}
