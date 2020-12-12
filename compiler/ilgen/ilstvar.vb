3918c1ade153f4851922e05b33e34a1a69ef7d31
Amin Badin <amin@yo-lang.org>
2020-12-12 08:36:46 +0330

    New Action :: Check & Set stack [ public / private fields]

--- compiler/ilgen/ilstvar.vb
+++ compiler/ilgen/ilstvar.vb
@@ -6,8 +6,23 @@
         If IsNothing(_ilmethod.parameter) = False Then
             If st_argument(nvar, _ilmethod, cargcodestruc, datatype) Then Return True
         End If
-        If IsNothing(ilasmgen.classdata.fields) = False Then
+        If IsNothing(ilasmgen.classdata.fields) = False AndAlso nvar.Contains("::") = False Then
             If st_field(nvar, _ilmethod, cargcodestruc, datatype) Then Return True
+        End If
+        If nvar.Contains("::") Then
+            Dim clinecodestruc As New xmlunpkd.linecodestruc
+            clinecodestruc.value = nvar
+            clinecodestruc.tokenid = tokenhared.token.IDENTIFIER
+            clinecodestruc.line = cargcodestruc.line
+            cargcodestruc.ile = nvar.Length
+            Dim hresult As identvalid._resultidentcvaild = identvalid.get_identifier_valid(clinecodestruc)
+            If hresult.identvalid = True Then
+                If hresult.callintern = True Then
+                    If st_field_global(hresult, _ilmethod, cargcodestruc, datatype) Then Return True
+                Else
+                    'extern fields
+                End If
+            End If
         End If
         dserr.args.Add(nvar)
         dserr.new_error(conserr.errortype.TYPENOTFOUND, cargcodestruc.line, ilbodybulider.path, "Method : " & _ilmethod.name & " - Unknown identifier : " & nvar & vbCrLf & authfunc.get_line_error(ilbodybulider.path, servinterface.get_target_info(cargcodestruc), cargcodestruc.value))
@@ -107,4 +122,49 @@
         Next
         Return False
     End Function
+
+    Friend Shared Function st_field_global(hresult As identvalid._resultidentcvaild, ByRef _ilmethod As ilformat._ilmethodcollection, cargcodestruc As xmlunpkd.linecodestruc, datatype As String, Optional ByRef nactorcode As ArrayList = Nothing) As Boolean
+        Dim nvar As String = hresult.clident
+        Dim fieldindex As Integer = funcdtproc.get_index_field(nvar, hresult.classindex)
+        If fieldindex = -1 Then
+            dserr.args.Add("Identifier '" & hresult.clident & "' was not found in class " & hresult.exclass)
+            dserr.new_error(conserr.errortype.FIELDERROR, cargcodestruc.line, ilbodybulider.path, authfunc.get_line_error(ilbodybulider.path, servinterface.get_target_info(cargcodestruc), cargcodestruc.value))
+        End If
+        Dim hfield As tknformat._pubfield = funcdtproc.get_field_info(hresult.classindex, fieldindex)
+        Dim nvartolower As String = nvar.ToLower
+        Dim pnvar As String = String.Empty
+        Dim pdatatype As String = String.Empty
+
+        pnvar = hfield.name
+        If pnvar <> conrex.NULL AndAlso pnvar.ToLower = nvartolower Then
+            pdatatype = hfield.ptype
+            servinterface.is_common_data_type(pdatatype, pdatatype)
+            If pdatatype = datatype Then
+                Dim classname As String = hresult.exclass
+                If IsNothing(nactorcode) = False Then
+                    If hfield.modifier = "static" Then
+                        cil.set_static_field(nactorcode, pnvar, pdatatype, classname)
+                    Else
+                        cil.set_field(nactorcode, pnvar, pdatatype, classname)
+                    End If
+                Else
+                    If hfield.modifier = "static" Then
+                        cil.set_static_field(_ilmethod.codes, pnvar, pdatatype, classname)
+                    Else
+                        cil.set_field(_ilmethod.codes, pnvar, pdatatype, classname)
+                    End If
+                End If
+                Return True
+            Else
+                dserr.args.Add(hfield.ptype)
+                dserr.args.Add(datatype)
+                dserr.new_error(conserr.errortype.EXPLICITCONVERSION, cargcodestruc.line, ilbodybulider.path, "Method : " & _ilmethod.name & " - identifier [ Field ] : " & nvar & vbCrLf)
+                Return False
+            End If
+            Return True
+        End If
+        Return False
+    End Function
+
+
 End Class
