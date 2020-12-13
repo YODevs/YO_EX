da1eceb8e41602d8a2834b1fd83149c0163dfd65
Amin Badin <amin@yo-lang.org>
2020-12-13 18:34:29 +0330

    New Action :: Set 'return' statement for void funcs

--- compiler/ilgen/ilret.vb
+++ compiler/ilgen/ilret.vb
@@ -2,7 +2,15 @@
 
 Public Class ilret
     Friend Shared Sub ret_to_route(ByRef ilmethod As ilformat._ilmethodcollection, clinecodestruc() As xmlunpkd.linecodestruc)
-        'TODO : Return & Exit func
+        If clinecodestruc.Length = 1 Then
+            If ilmethod.returntype = conrex.NULL Then
+                cil.ret(ilmethod.codes)
+            Else
+                dserr.args.Add(ilmethod.returntype)
+                dserr.new_error(conserr.errortype.RETURNERROR, clinecodestruc(0).line, ilbodybulider.path, authfunc.get_line_error(ilbodybulider.path, servinterface.get_target_info(clinecodestruc(0)), clinecodestruc(0).value))
+            End If
+            Return
+        End If
         syntaxchecker.check_statement(clinecodestruc, syntaxloader.statements.RET)
         Dim ldvar As New illdloc(ilmethod)
         ldvar.load_single_in_stack(ilmethod.returntype, clinecodestruc(1))
