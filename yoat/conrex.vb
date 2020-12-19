2e88b76b2d689bcedcbcc1293c1112fca45e120c
Amin Badin <amin@yo-lang.org>
2020-12-19 17:44:42 +0330

    New Action :: Change directory path

--- yoat/conrex.vb
+++ yoat/conrex.vb
@@ -1,4 +1,4 @@
 ﻿Public Class conrex
-    Friend Shared APPDIR As String = Environment.CurrentDirectory
+    Friend Shared APPDIR As String = My.Application.Info.DirectoryPath
     Friend Const TITLE As String = "YO Automated Tests"
 End Class
