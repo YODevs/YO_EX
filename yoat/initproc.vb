99081e5fe6038c25e1d24487c4d3df85d4244bf3
Amin Badin <amin@yo-lang.org>
2020-12-19 17:45:35 +0330

    New Action :: Check 'ctest' path

--- yoat/initproc.vb
+++ yoat/initproc.vb
@@ -5,13 +5,20 @@ Public Class initproc
     Friend Shared Sub init()
         cli.init_cli(False)
         If check_path() Then
-
+            Dim dirlist() As String = Directory.GetDirectories(conrex.APPDIR & "\ctest")
+            Dim yocapr As New yocaproc(dirlist)
+            yocapr.check()
         End If
     End Sub
 
     Private Shared Function check_path()
-        If File.Exists(conrex.APPDIR & "/YOCA.exe") = False Then
+        If File.Exists(conrex.APPDIR & "\YOCA.exe") = False Then
             Console.Write("YO compiler(YOCA.exe) not found.")
+            Return False
+        End If
+
+        If Directory.Exists(conrex.APPDIR & "\ctest") = False Then
+            Console.Write(conrex.APPDIR & "\ctest" & " not found.")
             Return False
         End If
         Return True
