3bc221eaad28a1d8286668d3facc3822e7982d42
Amin Badin <amin@yo-lang.org>
2020-12-19 08:28:07 +0330

    New Action :: Check compiler path

---
+++ yoat/initproc.vb
@@ -0,0 +1,19 @@
+﻿Imports System.IO
+
+Public Class initproc
+
+    Friend Shared Sub init()
+        cli.init_cli(False)
+        If check_path() Then
+
+        End If
+    End Sub
+
+    Private Shared Function check_path()
+        If File.Exists(conrex.APPDIR & "/YOCA.exe") = False Then
+            Console.Write("YO compiler(YOCA.exe) not found.")
+            Return False
+        End If
+        Return True
+    End Function
+End Class
