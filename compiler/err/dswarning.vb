31a497c44f2a9843caf4c69da5cc6993fc08259b
Amin Badin <amin@yo-lang.org>
2020-12-31 22:23:09 +0330

    Modify Class :: Increase iwar index

--- compiler/err/dswarning.vb
+++ compiler/err/dswarning.vb
@@ -1,11 +1,11 @@
 ﻿Public Class dswar
     Private Shared iwar As Integer = 1
     Friend Shared Sub new_warning(title As String, description As String)
-
         Console.ForegroundColor = ConsoleColor.DarkYellow
         Console.Write(vbCr & iwar & "-")
         Console.Write(title)
         Console.ResetColor()
         Console.WriteLine(" => " & description)
+        iwar += 1
     End Sub
 End Class
