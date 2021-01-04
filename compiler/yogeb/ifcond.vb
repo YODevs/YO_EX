25084556d9912da8264f6a351ba083c1f1a3bb36
Amin Badin <amin@yo-lang.org>
2021-01-04 18:01:03 +0330

    New Class :: If-statement , ifcond

---
+++ compiler/yogeb/ifcond.vb
@@ -0,0 +1,13 @@
+﻿Public Class ifcond
+    Friend Shared leavebrachlabel As New ArrayList
+    Private _ilmethod As ilformat._ilmethodcollection
+    Public Sub New(ilmethod As ilformat._ilmethodcollection)
+        Me._ilmethod = ilmethod
+    End Sub
+
+    Public Function set_if_statement(clinecodestruc() As xmlunpkd.linecodestruc, ByRef _illocalinit() As ilformat._illocalinit, ByRef localinit As localinitdata) As ilformat._ilmethodcollection
+        Dim condlinecodestruc() As xmlunpkd.linecodestruc = condproc.get_condition(clinecodestruc, 1)
+        Dim ifenblock As xmlunpkd.linecodestruc = condproc.get_block_body(clinecodestruc)
+        Return _ilmethod
+    End Function
+End Class
