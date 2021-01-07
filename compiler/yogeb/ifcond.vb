bac12398a2fa561eb97dfe6b5eff45d47767f203
Amin Badin <amin@yo-lang.org>
2021-01-07 20:39:09 +0330

    New Action :: If Statement

--- compiler/yogeb/ifcond.vb
+++ compiler/yogeb/ifcond.vb
@@ -8,6 +8,24 @@
     Public Function set_if_statement(clinecodestruc() As xmlunpkd.linecodestruc, ByRef _illocalinit() As ilformat._illocalinit, ByRef localinit As localinitdata) As ilformat._ilmethodcollection
         Dim condlinecodestruc() As xmlunpkd.linecodestruc = condproc.get_condition(clinecodestruc, 1)
         Dim ifenblock As xmlunpkd.linecodestruc = condproc.get_block_body(clinecodestruc)
+        Dim nbranch As New condproc.branchtargetinfo
+        nbranch.truebranch = lngen.get_line_prop("st_if")
+        nbranch.falsebranch = lngen.get_line_prop("en_if")
+        Dim cdproc As New condproc(nbranch)
+        cdproc.set_condition(_ilmethod, condlinecodestruc)
+        set_if_body(ifenblock, nbranch, _illocalinit, localinit)
         Return _ilmethod
     End Function
+
+    Private Sub set_if_body(ifenblock As xmlunpkd.linecodestruc, nbranch As condproc.branchtargetinfo, ByRef illocalinit() As ilformat._illocalinit, ByRef localinit As localinitdata)
+        '  stjmper.set_new_jmper(tokenhared.token.if, nbranch.falsebranch, Nothing )
+        lngen.set_direct_label(nbranch.truebranch, _ilmethod.codes)
+        Dim iltrans As New iltranscore(ilbodybulider.path, ifenblock.value, illocalinit, localinit)
+        iltrans.gen_transpile_code(_ilmethod, False)
+        illocalinit = _ilmethod.locallinit
+        '   stjmper.reset_jmper(tokenhared.token.IF )
+        lngen.set_direct_label(nbranch.falsebranch, _ilmethod.codes)
+    End Sub
+
+
 End Class
