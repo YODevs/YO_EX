79150b1feb936c669bc37d153b04b0c8a5e60fde
Amin Badin <amin@yo-lang.org>
2021-01-15 04:45:27 +0330

    New Class :: For loop class

---
+++ compiler/yogeb/forloop.vb
@@ -0,0 +1,14 @@
+﻿Public Class forloop
+
+    Private _ilmethod As ilformat._ilmethodcollection
+    Private getbrcond As String = String.Empty
+    Public Sub New(ilmethod As ilformat._ilmethodcollection)
+        Me._ilmethod = ilmethod
+    End Sub
+
+    Public Function set_for_st(clinecodestruc() As xmlunpkd.linecodestruc, ByRef illocalinit() As ilformat._illocalinit, ByRef localinit As localinitdata) As ilformat._ilmethodcollection
+
+        Return _ilmethod
+    End Function
+
+End Class
