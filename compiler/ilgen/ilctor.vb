e45c81acaac23d739a4a09b71915ef64e0491a48
Amin Badin <amin@yo-lang.org>
2021-01-30 19:28:38 +0330

    New Class :: Allocate an uninitialized object or value type and call ctor

---
+++ compiler/ilgen/ilctor.vb
@@ -0,0 +1,16 @@
+﻿Public Class ilctor
+    Private _ilmethod As ilformat._ilmethodcollection
+    Public Sub New(ilmethod As ilformat._ilmethodcollection)
+        Me._ilmethod = ilmethod
+    End Sub
+
+    Public Function set_new_ctor(index As Integer, clinecodestruc() As xmlunpkd.linecodestruc, ByRef _illocalinit() As ilformat._illocalinit, ByRef localinit As localinitdata) As ilformat._ilmethodcollection
+        Dim classindex, namespaceindex As Integer
+        Dim classname As String = _illocalinit(index).datatype
+        If libserv.get_extern_index_class(classname, namespaceindex, classindex) = -1 Then
+            dserr.args.Add("Class " & classname & " not found.")
+            dserr.new_error(conserr.errortype.METHODERROR, clinecodestruc(0).line, ilbodybulider.path, authfunc.get_line_error(ilbodybulider.path, servinterface.get_target_info(clinecodestruc(0)), classname))
+        End If
+        Return _ilmethod
+    End Function
+End Class
