97c78ea47fe62dc5336189e6d2e6d365e5010173
Amin Badin <amin@yo-lang.org>
2021-02-16 19:31:52 +0330

    New Action :: Display Compilation time + execution of projects

--- yoat/initproc.vb
+++ yoat/initproc.vb
@@ -7,10 +7,17 @@ Public Class initproc
         If check_path() Then
             Dim dirlist() As String = Directory.GetDirectories(conrex.APPDIR & "\ctest")
             Dim yocapr As New yocaproc(dirlist)
+            Dim tcompilespan As New TimeSpan()
+            Dim dtbefore As Date = Date.Now
             yocapr.check()
+            tcompilespan = Date.Now.Subtract(dtbefore)
+            print_compile_time(tcompilespan)
         End If
     End Sub
 
+    Private Shared Sub print_compile_time(tcompilespan As TimeSpan)
+        Console.Write("Compilation time + execution of projects: {0}:{1}:{2}.{3}", tcompilespan.Hours, tcompilespan.Minutes, tcompilespan.Seconds, tcompilespan.Milliseconds)
+    End Sub
     Private Shared Function check_path()
         If File.Exists(conrex.APPDIR & "\YOCA.exe") = False Then
             Console.Write("YO compiler(YOCA.exe) not found.")
