c362f17d027b85034de0a397b9599e103327b7a7
Amin Badin <amin@yo-lang.org>
2021-02-27 18:21:48 +0330

    New Method :: Check clinecodestruc is type casting

---
+++ compiler/yogeb/convtc.vb
@@ -0,0 +1,16 @@
+﻿Public Class convtc
+    Friend Shared setconvmethod As Boolean = False
+    Friend Shared Sub is_type_casting(clinecodestruc As xmlunpkd.linecodestruc(), ByRef index As Integer)
+        If clinecodestruc.Length <= index Then
+            Return
+        End If
+        If clinecodestruc(index).tokenid = tokenhared.token.EXPLTYPECAST Then
+            If clinecodestruc.Length > index + 1 Then
+                index += 1
+                setconvmethod = True
+            Else
+                dserr.new_error(conserr.errortype.ASSIGNCONVERT, clinecodestruc(index).line, ilbodybulider.path, "After determining the data type, expect for Identifier / Method, etc." & vbCrLf & authfunc.get_line_error(ilbodybulider.path, servinterface.get_target_info(clinecodestruc(index)), clinecodestruc(index).value))
+            End If
+        End If
+    End Sub
+End Class
