cee9231003c9ec2246887bb7f7a57728a6a76250
Amin Badin <amin@yo-lang.org>
2021-02-27 19:24:28 +0330

    New Field :: Convert methods array

--- compiler/const/conrex.vb
+++ compiler/const/conrex.vb
@@ -2,6 +2,7 @@
     Public Shared yocommondatatype() As String = {"bool", "i8", "i16", "i32", "i64", "i128", "u8", "u16", "u32", "u64", "str", "char", "f32", "f64", "obj"}
     Public Shared plvbcommondatatype() As String = {"Boolean", "Int8", "Int16", "Int32", "Int64", "Decimal", "UInt8", "UInt16", "UInt32", "UInt64", "String", "Char", "Single", "Double", "Object"}
     Public Shared msilcommondatatype() As String = {"bool", "int8", "int16", "int32", "int64", "valuetype [mscorlib]System.Decimal", "uint8", "uint16", "uint32", "uint64", "string", "char", "float32", "float64", "object"}
+    Public Shared convmethod() As String = {"ToBoolean", "ToSByte", "ToInt16", "ToInt32", "ToInt64", "ToDecimal", "ToByte", "ToUInt16", "ToUInt32", "ToUInt64", "ToString", "ToChar", "ToSingle", "ToDouble", ""}
     Public Shared specificxmlchar() As Char = {">", "<", "'", """", "&"}
     Public Shared spdustrcommand() As String = {"#nl", "#bs", "#tb", "#lf", "#cr", "#qu", "#co"}
     Public Shared spdustrreact() As String = {"\r\n", Chr(8), Chr(9), "\n", "\r", "\""", "\'"}
