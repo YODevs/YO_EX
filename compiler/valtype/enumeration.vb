7ace0f613391fbb42664309c078d46e7dafba0e0
Amin Badin <amin@yo-lang.org>
2021-03-08 17:55:18 +0330

    New Action :: Preparing to implement Value-Types

---
+++ compiler/valtype/enumeration.vb
@@ -0,0 +1,3 @@
+﻿Public Class enumeration
+
+End Class
---
+++ compiler/valtype/valtp.vb
@@ -0,0 +1,5 @@
+﻿Public Class valtp
+    Friend Shared Function check_value_type(ByRef valtpinfo As ilformat._valtypeinfo, ByRef datatype As String) As Boolean
+
+    End Function
+End Class
