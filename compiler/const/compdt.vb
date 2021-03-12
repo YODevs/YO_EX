abd54c6594f323b64e0e68563e28a6ecbb42b120
Amin Badin <amin@yo-lang.org>
2021-03-12 17:41:00 +0330

    Documentation :: [Persian] compdt

--- compiler/const/compdt.vb
+++ compiler/const/compdt.vb
@@ -1,4 +1,12 @@
-﻿Public Class compdt
+﻿''' <summary>
+''' <en>
+'''
+''' </en>
+''' <fa>
+''' ثوابت و ارایه هایی که حین عملیات کامپایل در واحدهای آماده سازی پارامتر ها یا کد جنریت ها استفاده می شوند
+''' </fa>
+''' </summary>
+Public Class compdt
     Friend Shared ILASMPATH As String = My.Application.Info.DirectoryPath & "\ilasm.exe"
     Friend Shared cilintegertypes() As String = {"int8", "int16", "int32", "int64", "uint8", "uint16", "uint32", "uint64"}
     Friend Shared yointegertypes() As String = {"i8", "i16", "i32", "i64", "u8", "u16", "u32", "u64"}
