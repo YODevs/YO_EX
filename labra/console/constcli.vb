9bb97566a3b0bf5f1715c6946e9f7e7efec8fdf7
Amin Badin <amin@yo-lang.org>
2021-03-15 18:11:11 +0330

    New Constant :: introtext

--- labra/console/constcli.vb
+++ labra/console/constcli.vb
@@ -2,4 +2,5 @@
     Friend Shared maincohelp As String = "Labra [Dependent on YO] Version [%version%]
 Copyright (C) YODevs . All rights reserved.
 For more information, type the command 'Help'.".Replace("%version%", conrex.VER)
+    Friend Const introtext As String = "--Select one of the menu items below : "
 End Class
