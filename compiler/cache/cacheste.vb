f76995ba6fffd5ed10cb749ae2db55e19897d2b5
Amin Badin <amin@yo-lang.org>
2021-03-24 23:39:01 +0430

    New Class :: Initialize cache system & create cache structure

---
+++ compiler/cache/cacheste.vb
@@ -0,0 +1,15 @@
+﻿Imports System.IO
+
+Public Class cacheste
+    Friend Shared Sub init_cache_system()
+        If Directory.Exists(conrex.CACHEDIR) = False Then
+            create_cache_structure()
+        End If
+    End Sub
+
+    Friend Shared Sub create_cache_structure()
+        Directory.CreateDirectory(conrex.CACHEDIR)
+        File.WriteAllText(conrex.CACHEDIR & "\.ver", conrex.VER)
+        Directory.CreateDirectory(conrex.CACHEDIR & "\fastbuild")
+    End Sub
+End Class
