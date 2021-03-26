3bf9797f36ac838d74aa0105855ccc544305137b
Amin Badin <amin@yo-lang.org>
2021-03-26 19:11:02 +0430

    New Action :: Check compiler version & compare with cache version

--- compiler/cache/cacheste.vb
+++ compiler/cache/cacheste.vb
@@ -4,6 +4,16 @@ Public Class cacheste
     Friend Shared Sub init_cache_system()
         If Directory.Exists(conrex.CACHEDIR) = False Then
             create_cache_structure()
+        Else
+            If File.Exists(conrex.CACHEDIR & "\.ver") Then
+                If File.ReadAllText(conrex.CACHEDIR & "\.ver").ToString <> conrex.VER Then
+                    Directory.Delete(conrex.CACHEDIR, True)
+                    create_cache_structure()
+                End If
+            Else
+                Directory.Delete(conrex.CACHEDIR, True)
+                create_cache_structure()
+            End If
         End If
     End Sub
 
