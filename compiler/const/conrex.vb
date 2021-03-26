b59030a335084e1f9098cc0a80706f5f052a9c6e
Amin Badin <amin@yo-lang.org>
2021-03-26 19:22:13 +0430

    New Constant :: YODAFORMAT

--- compiler/const/conrex.vb
+++ compiler/const/conrex.vb
@@ -27,6 +27,7 @@ Public Class conrex
     Public Const YOFORMAT As String = ".yo"
     Public Const DNLIBFORMAT As String = ".dll"
     Public Const LABRAFORMAT As String = ".labra"
+    Public Const YODAFORMAT As String = ".yoda"
     Public Const NULL As String = Nothing
     Public Const SPACE As Char = " "
     Public Const DOT As Char = "."
