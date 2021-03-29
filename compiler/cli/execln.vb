bbfd2499f4a6af9bb1b6abdd4238a8e0f5641c4e
Amin Badin <amin@yo-lang.org>
2021-03-29 19:46:25 +0430

    New Action :: Get '--dev' parameter

--- compiler/cli/execln.vb
+++ compiler/cli/execln.vb
@@ -136,6 +136,7 @@ You can type 'Help' to view commands.")
             compdt.MUTEPROCESS = argstorelist.find(compdt.PARAM_MUTE_PROCESS, True)
         End If
         procresult.set_state("init")
+        compdt.DEVMOD = argstorelist.find(compdt.PARAM_DEV, True)
         compdt.NOCACHE = argstorelist.find(compdt.PARAM_NOCACHE, True)
         compdt.DISPLAYTOKENWLEX = argstorelist.find(compdt.PARAM_DISPLAYTOKENLEX, True)
         compdt.DISPLAYSTACKTRACE = argstorelist.find(compdt.PARAM_DISPLAYSTACKTRACE, True)
