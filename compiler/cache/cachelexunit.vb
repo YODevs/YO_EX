1d95d11c0c6971d7e077d6011610904119d6e954
Amin Badin <amin@yo-lang.org>
2021-04-01 19:35:12 +0430

    New Method :: Deserialize Method , deserialize_lex(...) & save_include_file()

--- compiler/cache/cachelexunit.vb
+++ compiler/cache/cachelexunit.vb
@@ -1,16 +1,33 @@
-﻿Imports YOCA
+﻿Imports System.IO
+Imports YOCA
 
 Public Class cachelexunit
     Friend Shared Function check_lexer_cache(ByRef tknfmtclass As tknformat._class, path As String) As Boolean
         tknfmtclass.location = path
         cachegtr.check_cache_repo(tknfmtclass)
         If tknfmtclass.cacheinf.active Then
+            deserialize_lex(tknfmtclass)
             Return True
         Else
             Return False
         End If
     End Function
+    Friend Shared Sub deserialize_lex(ByRef tknfmtclass As tknformat._class)
+        If tknfmtclass.cacheinf.active = False Then Return
+        If cachemkr.cacheprojectdir = conrex.NULL Then cachemkr.create_cache_route()
+        Dim filepath As String = cachemkr.cacheprojectdir & servinterface.get_hash(tknfmtclass.location) & conrex.LEXCACHEFORMAT
+        Dim nxmlserializer As New Xml.Serialization.XmlSerializer(tknfmtclass.GetType)
+        Dim streamreader As New IO.StreamReader(filepath)
+        tknfmtclass = nxmlserializer.Deserialize(streamreader)
+    End Sub
 
+    Friend Shared Sub load_includes()
+        Dim cacheprojectdir As String = conrex.CACHEDIR & "\fastbuild\" & servinterface.get_hash(conrex.ENVCURDIR) & "\"
+        If Directory.Exists(cacheprojectdir) AndAlso File.Exists(cacheprojectdir & "includes.yoda") Then
+            Dim yoda As New YODA.YODA_Format
+            incfile.incspath = yoda.ReadYODA(File.ReadAllText(cacheprojectdir & "includes.yoda"))
+        End If
+    End Sub
 
     Private frclass() As tknformat._class
     Public Sub New(tknfmtclass() As tknformat._class)
@@ -19,15 +36,24 @@ Public Class cachelexunit
     Public Sub lex_to_serialization()
         If cachemkr.cacheprojectdir = conrex.NULL Then cachemkr.create_cache_route()
         For index = 0 To frclass.Length - 1
-            If IsNothing(frclass(index).methods) = False Then
-                For ielem = 0 To frclass(index).methods.Length - 1
-                    frclass(index).methods(ielem).bodyxmlfmt = conrex.NULL
-                Next
+            If frclass(index).cacheinf.active = False Then
+                If IsNothing(frclass(index).methods) = False Then
+                    For ielem = 0 To frclass(index).methods.Length - 1
+                        frclass(index).methods(ielem).bodyxmlfmt = conrex.NULL
+                    Next
+                End If
+                Dim filepath As String = cachemkr.cacheprojectdir & servinterface.get_hash(frclass(index).location) & conrex.LEXCACHEFORMAT
+                Dim nxmlserializer As New Xml.Serialization.XmlSerializer(frclass(index).GetType)
+                Dim nstreamwriter As New IO.StreamWriter(filepath)
+                nxmlserializer.Serialize(nstreamwriter, frclass(index))
             End If
-            Dim filepath As String = cachemkr.cacheprojectdir & servinterface.get_hash(frclass(index).location) & conrex.LEXCACHEFORMAT
-            Dim nxmlserializer As New Xml.Serialization.XmlSerializer(frclass(index).GetType)
-            Dim nstreamwriter As New IO.StreamWriter(filepath)
-            nxmlserializer.Serialize(nstreamwriter, frclass(index))
         Next
+        save_include_file()
+    End Sub
+
+    Private Sub save_include_file()
+        Dim includes As New YODA.YODA_Format
+        Dim getincludeyodafmt As String = includes.WriteYODA(incfile.incspath)
+        File.WriteAllText(cachemkr.cacheprojectdir & "includes.yoda", getincludeyodafmt)
     End Sub
 End Class
