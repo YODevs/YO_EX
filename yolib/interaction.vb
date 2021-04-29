779232005d37450da87f12e318cb1e129b9f6071
Amin Badin <amin@yo-lang.org>
2021-04-29 15:11:15 +0430

    New Class :: Interaction

---
+++ yolib/interaction.vb
@@ -0,0 +1,14 @@
+﻿Public Class interaction
+    Public Shared Function inputbox() As String
+        Return inputbox(My.Application.Info.Title, Nothing, Nothing)
+    End Function
+
+    Public Shared Function inputbox(title As String, description As String) As String
+        Return inputbox(title, description, Nothing)
+    End Function
+
+    Public Shared Function inputbox(title As String, description As String, defaultresult As String) As String
+        Dim result As String = Microsoft.VisualBasic.Interaction.InputBox(description, title, defaultresult)
+        Return result
+    End Function
+End Class
