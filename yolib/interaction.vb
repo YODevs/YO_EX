b85097e2998d0705b4f59d044fddcdcf087c0707
Amin Badin <amin@yo-lang.org>
2021-04-29 16:21:50 +0430

    New Method :: Message Box Api , messagebox(...)

--- yolib/interaction.vb
+++ yolib/interaction.vb
@@ -11,4 +11,16 @@
         Dim result As String = Microsoft.VisualBasic.Interaction.InputBox(description, title, defaultresult)
         Return result
     End Function
+
+    Public Shared Function messagebox(description As String) As String
+        Return messagebox(My.Application.Info.Title, description, 0)
+    End Function
+
+    Public Shared Function messagebox(title As String, description As String) As String
+        Return messagebox(title, description, 0)
+    End Function
+
+    Public Shared Function messagebox(title As String, description As String, dialogstyle As Integer) As String
+        Return MsgBox(description, dialogstyle, title).ToString()
+    End Function
 End Class
