6915d383ac29e1de9227c1028f7324b74a523f88
Amin Badin <amin@yo-lang.org>
2021-04-30 14:47:26 +0430

    New Class :: Fieldst

---
+++ compiler/prop/fieldste.vb
@@ -0,0 +1,8 @@
+﻿Imports System.Reflection
+Imports YOCA
+
+Public Class fieldste
+    Friend Shared Sub inv_external_field(clinecodestruc() As xmlunpkd.linecodestruc, ByRef ilmethod As ilformat._ilmethodcollection, fieldresult As identvalid._resultidentcvaild, inline As Integer, optval As tokenhared.token, namespaceindex As Integer, classindex As Integer, retfieldinfo As FieldInfo)
+
+    End Sub
+End Class
