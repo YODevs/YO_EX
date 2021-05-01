f5a4fc30c049038b6631c57003528c7c1384e6cc
Amin Badin <amin@yo-lang.org>
2021-05-01 19:17:25 +0430

    New Field :: MSCORLIB

--- compiler/const/conrex.vb
+++ compiler/const/conrex.vb
@@ -33,6 +33,7 @@ Public Class conrex
     Public Const YODAFORMAT As String = ".yoda"
     Public Const LEXCACHEFORMAT As String = ".yoobj"
     Public Const BOX As String = "box"
+    Public Const MSCORLIB As String = "mscorlib"
     Public Const [OBJECT] As String = "object"
     Public Const [STRING] As String = "string"
     Public Const NULL As String = Nothing
