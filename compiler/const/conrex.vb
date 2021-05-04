a61830165c2a45c25a024f09d12b615824d2f2e0
Amin Badin <amin@yo-lang.org>
2021-05-04 19:03:16 +0430

    New Field :: AMP

--- compiler/const/conrex.vb
+++ compiler/const/conrex.vb
@@ -9,7 +9,7 @@
 Public Class conrex
     Public Shared yocommondatatype() As String = {"bool", "i8", "i16", "i32", "i64", "i128", "u8", "u16", "u32", "u64", "str", "char", "f32", "f64", "obj"}
     Public Shared plvbcommondatatype() As String = {"Boolean", "Int8", "Int16", "Int32", "Int64", "Decimal", "UInt8", "UInt16", "UInt32", "UInt64", "String", "Char", "Single", "Double", "Object"}
-    Public Shared msilcommondatatype() As String = {"bool", "int8", "int16", "int32", "int64", "valuetype [mscorlib]System.Decimal", "uint8", "uint16", "uint32", "uint64", "string", "char", "float32", "float64", "object"}
+    Public Shared msilcommondatatype() As String = {"bool", "int8", "int16", "int32", "int64", "decimal", "uint8", "uint16", "uint32", "uint64", "string", "char", "float32", "float64", "object"}
     Public Shared convmethod() As String = {"ToBoolean", "ToSByte", "ToInt16", "ToInt32", "ToInt64", "ToDecimal", "ToByte", "ToUInt16", "ToUInt32", "ToUInt64", "ToString", "ToChar", "ToSingle", "ToDouble", ""}
     Public Shared specificxmlchar() As Char = {">", "<", "'", """", "&"}
     Public Shared spdustrcommand() As String = {"#nl", "#bs", "#tb", "#lf", "#cr", "#qu", "#co"}
@@ -54,8 +54,9 @@ Public Class conrex
     Public Const LTLEF As Char = ">"
     Public Const CMA As Char = ","
     Public Const DLR As Char = "$"
+    Public Const AMP As Char = "&"
     Public Const CLN As Char = ":"
-    Public Const DBCLN As Char = ":"
+    Public Const DBCLN As String = "::"
     Public Const EQU As Char = "="
     Public Const QES As Char = "?"
 
