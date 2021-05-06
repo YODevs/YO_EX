8a4d8ae0c0665abd6ebc6645ea2b51cd5a813193
Amin Badin <amin@yo-lang.org>
2021-05-06 16:30:34 +0430

    Bug Fixed :: Error when using CIL keywords #107

--- compiler/jmp/jmp.vb
+++ compiler/jmp/jmp.vb
@@ -9,6 +9,7 @@
     Public Shared Sub set_label(clinecodestruc() As xmlunpkd.linecodestruc, ByRef _ilmethod As ilformat._ilmethodcollection)
         syntaxchecker.check_statement(clinecodestruc, syntaxloader.statements.LABELJMP)
         Dim lblcode As String = clinecodestruc(0).value.Remove(0, 1)
+        set_label_customization(lblcode)
         If lbllist.find(lblcode) Then
             dserr.new_error(conserr.errortype.JMPERROR, clinecodestruc(0).line, path, clinecodestruc(0).value & " - This label is already defined in this scope." & vbCrLf & authfunc.get_line_error(path, servinterface.get_target_info(clinecodestruc(0)), clinecodestruc(0).value))
         End If
@@ -19,7 +20,11 @@
     Public Shared Sub jmp_statement(clinecodestruc() As xmlunpkd.linecodestruc, ByRef _ilmethod As ilformat._ilmethodcollection)
         syntaxchecker.check_statement(clinecodestruc, syntaxloader.statements.JMP)
         Dim lblcode As String = clinecodestruc(1).value.Remove(0, 1)
+        set_label_customization(lblcode)
         cil.branch_to_target(_ilmethod.codes, lblcode)
     End Sub
 
+    Private Shared Sub set_label_customization(ByRef lblcode As String)
+        lblcode = conrex.COSTR & lblcode & conrex.COSTR
+    End Sub
 End Class
