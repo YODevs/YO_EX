c985261b1e6f9ad8e90b506d515db15f063943d2
Amin Badin <amin@yo-lang.org>
2021-05-15 13:47:09 +0430

    New Class :: Environment class

---
+++ yolib/environment.vb
@@ -0,0 +1,27 @@
+﻿Public Class [environment]
+    Public ReadOnly Property username() As String
+        Get
+            Return System.Environment.UserName
+        End Get
+    End Property
+    Public ReadOnly Property userdomainname() As String
+        Get
+            Return System.Environment.UserDomainName
+        End Get
+    End Property
+    Public ReadOnly Property stacktrace() As String
+        Get
+            Return System.Environment.StackTrace
+        End Get
+    End Property
+    Public ReadOnly Property osversion() As String
+        Get
+            Return System.Environment.OSVersion.ToString
+        End Get
+    End Property
+    Public ReadOnly Property machinename() As String
+        Get
+            Return System.Environment.MachineName
+        End Get
+    End Property
+End Class
