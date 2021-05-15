92a08125dbbb20238629881c533f0d87b2efb62e
Amin Badin <amin@yo-lang.org>
2021-05-15 14:25:42 +0430

    New Properties :: CurrentDirectory ,  SystemDirectory , ...

--- yolib/environment.vb
+++ yolib/environment.vb
@@ -24,4 +24,37 @@
             Return System.Environment.MachineName
         End Get
     End Property
+    Public ReadOnly Property is64bitprocess() As String
+        Get
+            Return System.Environment.Is64BitProcess
+        End Get
+    End Property
+    Public ReadOnly Property is64bitos() As String
+        Get
+            Return System.Environment.Is64BitOperatingSystem
+        End Get
+    End Property
+    Public ReadOnly Property commandline() As String
+        Get
+            Return System.Environment.CommandLine
+        End Get
+    End Property
+
+    Public ReadOnly Property crdir() As String
+        Get
+            Return System.Environment.CurrentDirectory
+        End Get
+    End Property
+
+    Public ReadOnly Property appdir() As String
+        Get
+            Return My.Application.Info.DirectoryPath
+        End Get
+    End Property
+
+    Public ReadOnly Property sysdir() As String
+        Get
+            Return System.Environment.SystemDirectory
+        End Get
+    End Property
 End Class
