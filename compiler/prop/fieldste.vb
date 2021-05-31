8f9dce16687a9dc3290c5fda7b8e994180c92516
Amin Badin <amin@yo-lang.org>
2021-05-31 22:23:45 +0430

    New Method :: Set assignment for internal fields

--- compiler/prop/fieldste.vb
+++ compiler/prop/fieldste.vb
@@ -33,6 +33,49 @@ Public Class fieldste
         End If
     End Sub
 
+    Friend Shared Sub inv_internal_field(clinecodestruc() As xmlunpkd.linecodestruc, ByRef ilmethod As ilformat._ilmethodcollection, fieldresult As identvalid._resultidentcvaild, inline As Integer, optval As tokenhared.token)
+        Dim classindex As Integer
+        Dim isvirtualmethod As Boolean = False
+        Dim fieldname As String = fieldresult.clident
+        classindex = funcdtproc.get_index_class(ilmethod, fieldresult.exclass, isvirtualmethod)
+        If classindex = -1 Then
+            dserr.args.Add("Class '" & fieldresult.exclass & "' not found.")
+            dserr.new_error(conserr.errortype.FIELDERROR, clinecodestruc(0).line, ilbodybulider.path, authfunc.get_line_error(ilbodybulider.path, servinterface.get_target_info(clinecodestruc(0)), clinecodestruc(0).value))
+            Return
+        End If
+        Dim ldloc As New illdloc(ilmethod)
+        Dim setconvmethod As Boolean = convtc.setconvmethod
+        Dim ntypecast As String = convtc.ntypecast
+        Dim fieldindex As Integer = funcdtproc.get_index_field(fieldresult.clident, classindex)
+        If fieldindex = -1 Then
+            dserr.args.Add("Field '" & fieldresult.clident & "' not found.")
+            dserr.new_error(conserr.errortype.FIELDERROR, clinecodestruc(0).line, ilbodybulider.path, authfunc.get_line_error(ilbodybulider.path, servinterface.get_target_info(clinecodestruc(0)), fieldresult.clident))
+            Return
+        End If
+        Dim nfield As tknformat._pubfield = funcdtproc.get_field_info(classindex, fieldindex)
+        Dim gdatatype As String = nfield.ptype
+        If isvirtualmethod Then
+            Dim gidentifier As xmlunpkd.linecodestruc = clinecodestruc(0)
+            If gidentifier.value.Contains(conrex.DBCLN) Then
+                gidentifier.value = gidentifier.value.Remove(gidentifier.value.IndexOf(conrex.DBCLN))
+                gidentifier.ile = gidentifier.value.Length
+            End If
+            ldloc.load_single_in_stack(gdatatype, gidentifier)
+
+            ldloc.load_single_in_stack(gdatatype, clinecodestruc(inline))
+            cil.set_field(ilmethod.codes, fieldname, gdatatype, fieldresult.exclass)
+        Else
+            ldloc.load_single_in_stack(gdatatype, clinecodestruc(inline))
+            cil.set_static_field(ilmethod.codes, fieldname, gdatatype, fieldresult.exclass)
+        End If
+
+
+        convtc.setconvmethod = setconvmethod
+        convtc.ntypecast = ntypecast
+        If convtc.setconvmethod Then convtc.set_type_cast(ilmethod, gdatatype, fieldname, clinecodestruc(inline))
+
+    End Sub
+
     Friend Shared Sub get_external_field(clinecodestruc() As xmlunpkd.linecodestruc, _ilmethod As ilformat._ilmethodcollection, fieldresult As identvalid._resultidentcvaild, inline As Integer, namespaceindex As Integer, classindex As Integer, retfieldinfo As FieldInfo, isvirtualmethod As Boolean)
         Dim ldloc As New illdloc(_ilmethod)
         Dim setconvmethod As Boolean = convtc.setconvmethod
@@ -68,4 +111,5 @@ Public Class fieldste
         convtc.ntypecast = ntypecast
         If convtc.setconvmethod Then convtc.set_type_cast(_ilmethod, gdatatype, fieldname, clinecodestruc(inline))
     End Sub
+
 End Class
