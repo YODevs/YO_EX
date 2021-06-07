369e45a61dc284e1d31367d6646cb302b3ae0ec5
Amin Badin <amin@yo-lang.org>
2021-06-07 20:11:34 +0430

    Bug Fixed :: Could not load file or assembly 'YOLIB' #125

--- compiler/lexer/impfiles.vb
+++ compiler/lexer/impfiles.vb
@@ -33,6 +33,7 @@ Public Class impfiles
         End If
     End Sub
 
+    Private Shared includelexlist As New ArrayList
     Private Shared Sub import_include_file(ByRef tknfmtclass As tknformat._class())
         cachelexunit.load_includes(tknfmtclass)
         If incfile.incspath.Count = 0 Then Return
@@ -40,12 +41,41 @@ Public Class impfiles
         Array.Resize(tknfmtclass, tknfmtclass.Length + incfile.incspath.Count)
         For index = 0 To incfile.incspath.Count - 1
             If cachelexunit.check_lexer_cache(tknfmtclass(bfindex + index), incfile.incspath(index).ToString) = False Then
+                includelexlist.Add(incfile.incspath(index).ToString)
                 Dim lex As New lexer(incfile.incspath(index).ToString)
                 lex.lexme(tknfmtclass(bfindex + index))
             End If
             servinterface.check_class_vaild(tknfmtclass(bfindex + index).attribute, tknfmtclass(bfindex + index).location)
             funcdtproc.import_method(tknfmtclass(bfindex + index))
         Next
+        If includelexlist.Count > 0 Then import_nested_include_file(tknfmtclass)
+    End Sub
+
+    Private Shared Sub import_nested_include_file(ByRef tknfmtclass As tknformat._class())
+        Dim incscount As Integer = incfile.incspath.Count - 1
+        For index = 0 To incscount
+            Dim lexfile As Boolean = True
+            For ci2 = 0 To includelexlist.Count - 1
+                If incfile.incspath(index).ToString = includelexlist(ci2).ToString Then
+                    lexfile = False
+                    Continue For
+                End If
+            Next
+            If lexfile Then
+                Dim bfindex As Integer = tknfmtclass.Length
+                Array.Resize(tknfmtclass, bfindex + 1)
+                If cachelexunit.check_lexer_cache(tknfmtclass(bfindex), incfile.incspath(index).ToString) = False Then
+                    Dim lex As New lexer(incfile.incspath(index).ToString)
+                    lex.lexme(tknfmtclass(bfindex))
+                    includelexlist.Add(incfile.incspath(index).ToString)
+                End If
+                servinterface.check_class_vaild(tknfmtclass(bfindex).attribute, tknfmtclass(bfindex).location)
+                funcdtproc.import_method(tknfmtclass(bfindex))
+            End If
+        Next
+        If incscount <> incfile.incspath.Count - 1 Then
+            import_nested_include_file(tknfmtclass)
+        End If
     End Sub
     ''' <summary>
     ''' Simple get file [ just in master route ]
