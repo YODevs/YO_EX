7b6f1d9d6cdb5aeb1896856aaba376f24cb56635
Amin Badin <amin@yo-lang.org>
2021-06-11 12:45:53 +0430

    New Method :: Get external types

--- compiler/typs/yotypecreator.vb
+++ compiler/typs/yotypecreator.vb
@@ -5,10 +5,39 @@
 
         Else
             Dim classresult As funcvalid._resultfuncvaild = get_class_valid_rtype(ilmethod, clinecodestruc, indclass)
+            If classresult.callintern Then
+
+            Else
+                get_external_type(ilmethod, tpinf, clinecodestruc, classresult, indclass)
+            End If
         End If
         Return tpinf
     End Function
 
+    Friend Shared Sub get_external_type(ilmethod As ilformat._ilmethodcollection, ByRef tpinf As ilformat._typeinfo, clinecodestruc As xmlunpkd.linecodestruc(), classresult As funcvalid._resultfuncvaild, indclass As Integer)
+        Dim classindex, namespaceindex As Integer
+        Dim reclassname As String = String.Empty
+        Dim isvirtualmethod As Boolean = False
+        Dim classname As String = classresult.exclass
+        If libserv.get_extern_index_class(ilmethod, classresult.exclass, namespaceindex, classindex, isvirtualmethod, reclassname) = -1 Then
+            dserr.args.Add("Class '" & classname & "' not found.")
+            dserr.new_error(conserr.errortype.METHODERROR, clinecodestruc(indclass).line, ilbodybulider.path, authfunc.get_line_error(ilbodybulider.path, servinterface.get_target_info(clinecodestruc(indclass)), clinecodestruc(indclass).value))
+            Return
+        End If
+        classresult.asmextern = libserv.get_extern_assembly(namespaceindex)
+        tpinf = get_extern_class_type(namespaceindex, classindex)
+    End Sub
+
+    Friend Shared Function get_extern_class_type(namespaceindex As Integer, classindex As Integer) As ilformat._typeinfo
+        Dim tpinf As New ilformat._typeinfo
+        Dim tp As Type = libserv.get_extern_class_type(namespaceindex, classindex)
+        tpinf.externlib = tp.Assembly.GetName().Name
+        tpinf.fullname = tp.ToString
+        tpinf.name = tp.Name
+        tpinf.namespace = tp.Namespace
+        tpinf.isprimitive = False
+        Return tpinf
+    End Function
     Private Shared Function get_class_valid_rtype(ilmethod As ilformat._ilmethodcollection, clinecodestruc As xmlunpkd.linecodestruc(), indclass As Integer) As funcvalid._resultfuncvaild
         Dim glinecodestruc() As xmlunpkd.linecodestruc = servinterface.trim_line_code_struc(clinecodestruc, indclass)
         glinecodestruc(0).value &= "::.ctor"
