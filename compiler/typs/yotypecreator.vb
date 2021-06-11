6ae4ed98820567b8592bf15a9e9fd38675fb432e
Amin Badin <amin@yo-lang.org>
2021-06-11 12:12:15 +0430

    New Action :: Get class validator [internal / external class]

--- compiler/typs/yotypecreator.vb
+++ compiler/typs/yotypecreator.vb
@@ -1,5 +1,17 @@
 ﻿Public Class yotypecreator
-    Public Shared Function get_type_info(typestr As String) As ilformat._typeinfo
+    Public Shared Function get_type_info(ilmethod As ilformat._ilmethodcollection, clinecodestruc As xmlunpkd.linecodestruc(), indclass As Integer, typestr As String) As ilformat._typeinfo
+        Dim tpinf As New ilformat._typeinfo
+        If servinterface.is_cil_common_data_type(typestr) OrElse servinterface.is_common_data_type(typestr, Nothing) Then
 
+        Else
+            Dim classresult As funcvalid._resultfuncvaild = get_class_valid_rtype(ilmethod, clinecodestruc, indclass)
+        End If
+        Return tpinf
+    End Function
+
+    Private Shared Function get_class_valid_rtype(ilmethod As ilformat._ilmethodcollection, clinecodestruc As xmlunpkd.linecodestruc(), indclass As Integer) As funcvalid._resultfuncvaild
+        Dim glinecodestruc() As xmlunpkd.linecodestruc = servinterface.trim_line_code_struc(clinecodestruc, indclass)
+        glinecodestruc(0).value &= "::.ctor"
+        Return funcvalid.get_func_valid(ilmethod, glinecodestruc)
     End Function
 End Class
