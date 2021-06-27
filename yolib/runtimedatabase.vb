daaf63fba25f56a7e2de811f903ae8417f2628d4
Amin Badin <amin@yo-lang.org>
2021-06-27 07:15:21 +0430

    New Class :: Run-Time Database

---
+++ yolib/runtimedatabase.vb
@@ -0,0 +1,15 @@
+﻿Public Class rds
+    Private columns, dt As ArrayList
+    Public Sub New()
+        columns = New ArrayList
+        dt = New ArrayList
+    End Sub
+
+    Public Sub set_columns(items As String)
+        Dim yoda As New yoda
+        columns = yoda.ReadYODA(items)
+        If IsNothing(columns) Then
+            Throw New Exception("The columns are still empty.")
+        End If
+    End Sub
+End Class
