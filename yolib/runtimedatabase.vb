607f4160ff589eea37be0dcca410a3ae8dd828df
Amin Badin <amin@yo-lang.org>
2021-06-27 19:19:32 +0430

    New Property :: Count of column

--- yolib/runtimedatabase.vb
+++ yolib/runtimedatabase.vb
@@ -1,14 +1,22 @@
 ﻿Public Class rds
-    Private columns, dt As ArrayList
+    Private columns, dt()() As ArrayList
     Public Sub New()
         columns = New ArrayList
-        dt = New ArrayList
     End Sub
 
+    Public ReadOnly Property columncount() As Integer
+        Get
+            Return columns.Count
+        End Get
+    End Property
     Public Sub set_columns(items As String)
+        If columns.Count > 0 Then
+            Throw New Exception("Columns have already been added to the data store.")
+            Return
+        End If
         Dim yoda As New yoda
         columns = yoda.ReadYODA(items)
-        If IsNothing(columns) Then
+        If IsNothing(columns) OrElse columns.Count = 0 Then
             Throw New Exception("The columns are still empty.")
         End If
     End Sub
