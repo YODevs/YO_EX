c5da81666acf7b7809e5671dd7722711e7ee43cf
Amin Badin <amin@yo-lang.org>
2021-06-27 19:58:39 +0430

    New Method :: Get Row

--- yolib/runtimedatabase.vb
+++ yolib/runtimedatabase.vb
@@ -1,5 +1,5 @@
 ﻿Public Class rds
-    Private columns, dt()() As ArrayList
+    Private columns, dt() As ArrayList
     Public Sub New()
         columns = New ArrayList
     End Sub
@@ -7,6 +7,14 @@
     Public ReadOnly Property columncount() As Integer
         Get
             Return columns.Count
+        End Get
+    End Property
+    Public ReadOnly Property rowcount() As Integer
+        Get
+            If IsNothing(dt) Then
+                Return 0
+            End If
+            Return dt.Count
         End Get
     End Property
     Public Sub set_columns(items As String)
@@ -20,4 +28,49 @@
             Throw New Exception("The columns are still empty.")
         End If
     End Sub
+
+    Public Function insert(items As String) As Integer
+        Dim rowlist As ArrayList
+        Dim yoda As New yoda
+        rowlist = yoda.ReadYODA(items)
+        If rowlist.Count <> columns.Count Then
+            Throw New Exception("Items are not equal to the number of columns[" & columns.Count & "].")
+            Return -1
+        End If
+        Dim getlastitemindex As Integer = rowcount
+        Array.Resize(dt, getlastitemindex + 1)
+        dt(getlastitemindex) = rowlist
+        Return getlastitemindex + 1
+    End Function
+
+    Public Function get_row_map(index As Integer) As map
+        Dim yodastr As String = get_row(index)
+        Dim mp As New map
+        Dim yoda As New yoda
+        Dim rowlist As ArrayList = yoda.ReadYODA(yodastr)
+        For index = 0 To rowlist.Count - 1
+            mp.add(columns(index).ToString, rowlist(index).ToString)
+        Next
+        Return mp
+    End Function
+
+    Public Function get_row_list(index As Integer) As list
+        Dim yodastr As String = get_row(index)
+        Dim ls As New list
+        ls.import(yodastr)
+        Return ls
+    End Function
+    Public Function get_row(index As Integer) As String
+        If IsNothing(dt) Then
+            Throw New Exception("Datastore is empty.")
+            Return Nothing
+        End If
+        If index >= 0 AndAlso index < dt.Length Then
+            Dim yoda As New yoda
+            Return yoda.WriteYODA(dt(index))
+        Else
+            Throw New Exception("Index is not allowed, review it.")
+            Return Nothing
+        End If
+    End Function
 End Class
