a37efd3f1e3124b44e4e84dc27a0bd1bfad2b0f8
Amin Badin <amin@yo-lang.org>
2021-06-29 20:52:56 +0430

    New Method :: Check Value types

--- compiler/valtype/valtp.vb
+++ compiler/valtype/valtp.vb
@@ -1,5 +1,51 @@
-﻿Public Class valtp
-    Friend Shared Function check_value_type(ByRef valtpinfo As ilformat._valtypeinfo, ByRef datatype As String) As Boolean
+﻿Imports YOCA
 
+Public Class valtp
+    Friend Shared Function check_value_type(ilmethod As ilformat._ilmethodcollection, ByRef illocalinit As ilformat._illocalinit, classname As String) As Boolean
+        Dim enumname As String = String.Empty
+        Dim classindex As Integer = -1
+        Dim internalclass As Boolean = False
+        If classname.Contains(conrex.DOT) Then
+            enumname = classname.Remove(0, classname.LastIndexOf(conrex.DOT) + 1)
+            classname = classname.Remove(classname.LastIndexOf(conrex.DOT))
+        Else
+            enumname = classname
+            classname = ilasmgen.classdata.name
+        End If
+        classindex = libserv.get_extern_index_class(ilmethod, classname, Nothing, Nothing, Nothing, Nothing)
+        If classindex = -1 Then
+            classindex = funcdtproc.get_index_class(ilmethod, classname)
+            If classindex = -1 Then
+                Return False
+            End If
+            internalclass = True
+        End If
+
+        If internalclass Then
+            Dim enumindex As Integer = funcdtproc.get_index_enum(enumname, classindex)
+            If enumindex = -1 Then
+                Return False
+            Else
+                illocalinit.isvaluetypes = True
+                illocalinit.typeinf.valtpinf.structuretype = ilformat._valuetypestructure.ENUM
+                illocalinit.typeinf.valtpinf.objectname = enumname
+                illocalinit.typeinf.valtpinf.classname = classname
+                illocalinit.typeinf.isenum = True
+                illocalinit.typeinf.isclass = True
+                illocalinit.typeinf.isinternalclass = True
+                illocalinit.typeinf.isprimitive = False
+                illocalinit.typeinf.fullname = classname & conrex.DOT & enumname
+                illocalinit.typeinf.asminfo = classname & conrex.DOT & enumname
+                If classname.Contains(conrex.DOT) Then
+                    illocalinit.typeinf.name = classname.Remove(0, classname.IndexOf(conrex.DOT) + 1)
+                    illocalinit.typeinf.namespace = classname.Remove(classname.LastIndexOf(conrex.DOT) + 1)
+                Else
+                    illocalinit.typeinf.name = classname
+                End If
+                illocalinit.datatype = illocalinit.typeinf.asminfo
+                Return True
+            End If
+        End If
+        Return True
     End Function
 End Class
