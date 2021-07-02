9ab26f599510d25a89a62244795054ef9381f384
Amin Badin <amin@yo-lang.org>
2021-07-02 16:03:34 +0430

    New Method :: Load & set external enums , `load_extern_enum(...)

--- compiler/valtype/enumeration.vb
+++ compiler/valtype/enumeration.vb
@@ -18,13 +18,17 @@ Public Class enumeration
 
         Dim classindex As Integer = funcdtproc.get_index_class(_ilmethod, classname, False)
         If classindex = -1 Then
-
-            set_execption_class_unknown(classname, cargcodestruc)
+            Dim namespaceindex As Integer = -1
+            If libserv.get_extern_index_class(_ilmethod, classname, namespaceindex, classindex) = 1 Then
+                load_extern_enum(_ilmethod, classname, enumname, idenresult, cargcodestruc, classindex, namespaceindex)
+                Return True
+            End If
+            set_exception_class_unknown(classname, cargcodestruc)
             Return False
         Else
             Dim enumindex As Integer = funcdtproc.get_index_enum(enumname, classindex)
             If enumindex = -1 Then
-                set_expection_enum_unknown(enumname, cargcodestruc)
+                set_exception_enum_unknown(enumname, cargcodestruc)
                 Return False
             End If
             Dim enumtp As tknformat._enum = funcdtproc.get_enum_info(classindex, enumindex)
@@ -34,12 +38,31 @@ Public Class enumeration
         Return False
     End Function
 
-    Private Shared Sub set_execption_class_unknown(classname As String, cargcodestruc As xmlunpkd.linecodestruc)
+    Private Shared Sub load_extern_enum(ByRef ilmethod As ilformat._ilmethodcollection, classname As String, enumname As String, idenresult As identvalid._resultidentcvaild, cargcodestruc As xmlunpkd.linecodestruc, classindex As Integer, namespaceindex As Integer)
+        Dim enumindex As Integer = libserv.get_index_enum(enumname, namespaceindex, classindex)
+        If enumindex = -1 Then
+            set_exception_enum_unknown(enumname, cargcodestruc)
+            Return
+        End If
+        Dim enumtp As Type = libserv.get_nested_type(enumindex, namespaceindex, classindex)
+        Dim enumfield As String = idenresult.clident.ToLower
+        For index = 0 To enumtp.GetEnumValues.Length - 1
+            If enumtp.GetEnumValues(index).ToString.ToLower = enumfield Then
+                cil.push_int32_onto_stack(ilmethod.codes, enumtp.GetEnumValues(index))
+                Return
+            End If
+        Next
+        dserr.args.Add(idenresult.clident)
+        dserr.args.Add(idenresult.exclass)
+        dserr.new_error(conserr.errortype.ENUMMEMBERERROR, cargcodestruc.line, ilbodybulider.path, authfunc.get_line_error(ilbodybulider.path, servinterface.get_target_info(cargcodestruc), cargcodestruc.value))
+    End Sub
+
+    Private Shared Sub set_exception_class_unknown(classname As String, cargcodestruc As xmlunpkd.linecodestruc)
         dserr.args.Add("Class '" & classname & "' not found.")
         dserr.new_error(conserr.errortype.METHODERROR, cargcodestruc.line, ilbodybulider.path, authfunc.get_line_error(ilbodybulider.path, servinterface.get_target_info(cargcodestruc), cargcodestruc.value))
     End Sub
 
-    Private Shared Sub set_expection_enum_unknown(enumname As String, cargcodestruc As xmlunpkd.linecodestruc)
+    Private Shared Sub set_exception_enum_unknown(enumname As String, cargcodestruc As xmlunpkd.linecodestruc)
         dserr.args.Add(enumname)
         dserr.new_error(conserr.errortype.TYPENOTFOUND, cargcodestruc.line, ilbodybulider.path, authfunc.get_line_error(ilbodybulider.path, servinterface.get_target_info(cargcodestruc), cargcodestruc.value))
     End Sub
