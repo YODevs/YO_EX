b20413948ccc0d6bd2c347a2b6b45fdbf2a150dc
Amin Badin <amin@yo-lang.org>
2021-07-09 16:17:05 +0430

    Bug Fixed :: Detect & delete cache of deleted files #129 ,#142

--- compiler/cache/cachelexunit.vb
+++ compiler/cache/cachelexunit.vb
@@ -2,6 +2,10 @@
 Imports YOCA
 
 Public Class cachelexunit
+    Enum cachecheckstate
+        INCLUDEFILE
+        PROJECTFILE
+    End Enum
     Friend Shared Function check_lexer_cache(ByRef tknfmtclass As tknformat._class, path As String) As Boolean
         If compdt.DEVMOD = True Then Return False
         If compdt.NOCACHE = True Then Return False
@@ -30,15 +34,31 @@ Public Class cachelexunit
         If Directory.Exists(cacheprojectdir) AndAlso File.Exists(cacheprojectdir & "includes.yoda") Then
             Dim yoda As New YODA.YODA_Format
             incfile.incspath = yoda.ReadYODA(File.ReadAllText(cacheprojectdir & "includes.yoda"))
+            check_exist_source(incfile.incspath, cachecheckstate.INCLUDEFILE)
         End If
     End Sub
     Friend Shared Sub load_includes(tknfmtclass As tknformat._class())
         Dim cacheprojectdir As String = conrex.CACHEDIR & "\fastbuild\" & servinterface.get_hash(conrex.ENVCURDIR) & "\"
         If Directory.Exists(cacheprojectdir) Then
             For index = 0 To tknfmtclass.Length - 1
+                check_exist_source(tknfmtclass(index).includelist, cachecheckstate.INCLUDEFILE)
                 For i2 = 0 To tknfmtclass(index).includelist.Count - 1
                     incfile.add_new_path(tknfmtclass(index).includelist(i2).ToString)
                 Next
+            Next
+        End If
+    End Sub
+
+    Friend Shared Sub check_exist_source(files As ArrayList, checkstate As cachecheckstate)
+        If checkstate = cachecheckstate.INCLUDEFILE Then
+            If files.Count = 0 Then Return
+            For index = 0 To files.Count - 1
+                If File.Exists(files(index).ToString) = False Then
+                    Dim cacheprojectdir As String = conrex.CACHEDIR & "\fastbuild\" & servinterface.get_hash(conrex.ENVCURDIR) & "\"
+                    File.WriteAllText(cacheprojectdir & ".delete", conrex.NULL)
+                    dserr.args.Add(files(index).ToString)
+                    dserr.new_error(conserr.errortype.ACCESSINGFILEERROR, -1, Nothing, "This error occurs when the compiler cannot access the project files.", "Use the compile command again: yoca build Or yoca run")
+                End If
             Next
         End If
     End Sub
