bc44b5686ee61fabb07ff5ad66d08bc2ab6f3d02
Amin Badin <amin@yo-lang.org>
2021-07-25 14:48:31 +0430

    New Class :: Matrix class

---
+++ yolib/matrix.vb
@@ -0,0 +1,22 @@
+﻿Public Class matrix
+
+    Private dt() As ArrayList
+    Private gcolumnsize, growsize As Integer
+    Public ReadOnly Property columnsize() As Integer
+        Get
+            Return gcolumnsize
+        End Get
+    End Property
+    Public ReadOnly Property rowsize() As Integer
+        Get
+            Return rowsize
+        End Get
+    End Property
+    Public Sub New(ncol As Integer, nrow As Integer)
+        If ncol < 1 OrElse nrow < 1 Then
+            Throw New Exception("Number of columns or rows must be more than zero.")
+        End If
+        gcolumnsize = ncol
+        growsize = nrow
+    End Sub
+End Class
