b9480c4eff0a9d4c10b753cc5a5cb75e36802458
Amin Badin <amin@yo-lang.org>
2021-07-27 18:18:08 +0430

    New Method :: Scalar Matrix , `set_scalar_matrix(...)`

--- yolib/matrix.vb
+++ yolib/matrix.vb
@@ -64,6 +64,28 @@
         Next
         gisempty = False
     End Sub
+    Private Sub priv_set_scalar_matrix(val As Object)
+        If gcolumnsize <> growsize Then Throw New Exception("Unit matrix should be a square matrix (for example, 3 * 3 or 2 * 2).")
+        clear_matrix()
+        Dim rowunit As Integer = 0
+        For index = 0 To gcolumnsize - 1
+            For i2 = 0 To growsize - 1
+                If rowunit = i2 Then
+                    dt(i2).Add(val)
+                Else
+                    dt(i2).Add(0)
+                End If
+            Next
+            rowunit += 1
+        Next
+        gisempty = False
+    End Sub
+    Public Sub set_scalar_matrix(val As Integer)
+        priv_set_scalar_matrix(val)
+    End Sub
+    Public Sub set_scalar_matrix(val As Double)
+        priv_set_scalar_matrix(val)
+    End Sub
     Public Function get_matrix() As String
         If gisempty Then Throw New Exception("The matrix is empty, you must first fill in the matrix values.")
         Dim result As String
