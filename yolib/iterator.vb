548ccb933ea45c80b6d297861cc01ab335069db0
Amin Badin <amin@yo-lang.org>
2021-09-01 19:23:25 +0430

    New Class :: `Iterator`

---
+++ yolib/iterator.vb
@@ -0,0 +1,10 @@
+﻿Public Class iterator
+    Private clist As list
+    Private count, index As Integer
+    Public Sub New(clonelist As list)
+        count = clonelist.count
+        index = 0
+        If IsNothing(clonelist) OrElse count = 0 Then Throw New Exception("The list is empty.")
+        clist = clonelist
+    End Sub
+End Class
