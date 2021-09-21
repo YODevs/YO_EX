1b64b9328ca56444b1af730f33e8bc894683b3aa
Amin Badin <amin@yo-lang.org>
2021-09-21 18:32:14 +0430

    New Action :: Set `index` property & Make changes to how it works

--- yolib/iterator.vb
+++ yolib/iterator.vb
@@ -1,21 +1,36 @@
 ﻿Public Class iterator
     Private clist As list
-    Private count, index As Integer
+    Private count, privindex As Integer
+
+    Public Property index() As Integer
+        Get
+            Return privindex
+        End Get
+        Set(ByVal value As Integer)
+            If value >= 0 AndAlso count > privindex Then
+                privindex = value
+            Else
+                Throw New Exception(value & " - The index is invalid.")
+            End If
+        End Set
+    End Property
+
     Public Sub New(clonelist As list)
         count = clonelist.count
-        index = 0
+        privindex = 0
         If IsNothing(clonelist) OrElse count = 0 Then Throw New Exception("The list is empty.")
         clist = clonelist
     End Sub
 
     Public Function has_next() As Boolean
-        Return (count > index + 1)
+        Return (count > privindex)
     End Function
 
     Public Function [next]() As String
         If has_next() Then
-            index += 1
-            Return clist.get(index)
+            Dim result As String = clist.get(privindex)
+            privindex += 1
+            Return result
         End If
         Throw New Exception("There is no item, with 'has_next()' function you can check the iterator values.")
     End Function
