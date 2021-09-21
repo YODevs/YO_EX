6d8131f6bab628e6f69bee2c81eca9c8e9d59d72
Amin Badin <amin@yo-lang.org>
2021-09-21 18:04:56 +0430

    New Method :: `next()` & `has_next()`

--- yolib/iterator.vb
+++ yolib/iterator.vb
@@ -7,4 +7,17 @@
         If IsNothing(clonelist) OrElse count = 0 Then Throw New Exception("The list is empty.")
         clist = clonelist
     End Sub
+
+    Public Function has_next() As Boolean
+        Return (count > index + 1)
+    End Function
+
+    Public Function [next]() As String
+        If has_next() Then
+            index += 1
+            Return clist.get(index)
+        End If
+        Throw New Exception("There is no item, with 'has_next()' function you can check the iterator values.")
+    End Function
+
 End Class
