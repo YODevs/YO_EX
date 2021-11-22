56ad2bf633efcbbedebca357a72a2098745e8ee1
AminCoder <aminlord96@gmail.com>
2021-11-22 02:46:39 -0800

    New Field :: PROGRAMFILEDIR

--- labra/const/conrex.vb
+++ labra/const/conrex.vb
@@ -4,6 +4,7 @@
     Public Shared APPDIR As String = My.Application.Info.DirectoryPath
     Public Shared ENVCURDIR As String = Environment.CurrentDirectory
     Public Shared COMMANDLINE As String = Environment.CommandLine
+    Public Shared PROGRAMFILEDIR As String = Environment.GetFolderPath(Environment.SpecialFolder.ProgramFiles)
     Public Const YOFORMAT As String = ".yo"
     Public Const NULL As String = Nothing
     Public Const SPACE As Char = " "
