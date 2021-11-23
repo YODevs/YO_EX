87d61cad5e9d3acdb902ab7c84391b52c4d9596f
AminCoder <ruzeslarrey@gmail.com>
2021-11-23 10:03:43 -0800

    New Class :: Initialize .NetCore Process

---
+++ compiler/netcore/netcoreinitproc.vb
@@ -0,0 +1,13 @@
+﻿Imports System.IO
+
+Public Class netcoreinitproc
+    Friend Shared Sub init()
+        If Directory.Exists(conrex.DOTNETCACHEDIR) = False Then
+            Directory.CreateDirectory(conrex.DOTNETCACHEDIR)
+        End If
+        If Directory.Exists(conrex.DOTNETCACHEDIR & "\" & compdt.TARGETFRAMEWORK) = False Then
+            Directory.CreateDirectory(conrex.DOTNETCACHEDIR & "\" & compdt.TARGETFRAMEWORK)
+            'Action ...
+        End If
+    End Sub
+End Class
