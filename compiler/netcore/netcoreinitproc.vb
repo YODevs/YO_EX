439ec6278f987aa65f8bd5d0b91e07f2bfe11bba
AminCoder <ruzeslarrey@gmail.com>
2021-11-26 12:26:43 +0330

    New Method :: Start running `BridgeCore` & Create Caches

--- compiler/netcore/netcoreinitproc.vb
+++ compiler/netcore/netcoreinitproc.vb
@@ -7,7 +7,35 @@ Public Class netcoreinitproc
         End If
         If Directory.Exists(conrex.DOTNETCACHEDIR & "\" & compdt.TARGETFRAMEWORK) = False Then
             Directory.CreateDirectory(conrex.DOTNETCACHEDIR & "\" & compdt.TARGETFRAMEWORK)
-            'Action ...
         End If
+        start_process()
+    End Sub
+
+    Private Shared Sub start_process()
+        Dim targetdir As String = conrex.DOTNETCACHEDIR & "\" & compdt.TARGETFRAMEWORK
+        Dim bridgecoreproc As New System.Diagnostics.Process()
+        Try
+            With bridgecoreproc.StartInfo
+                .FileName = compdt.BRIDGECORE
+                .Arguments = compdt.TARGETFRAMEWORK & Space(1) & targetdir.Replace(conrex.SPACE, "@#SP#")
+                .RedirectStandardOutput = True
+                .RedirectStandardError = True
+                .RedirectStandardInput = True
+                .UseShellExecute = False
+                .WindowStyle = ProcessWindowStyle.Normal
+                .CreateNoWindow = False
+            End With
+
+            bridgecoreproc.Start()
+            bridgecoreproc.WaitForExit()
+            Dim resultbrigecore As String = conrex.NULL
+            For Each stdoutput In bridgecoreproc.StandardOutput.ReadToEnd().Split(vbCrLf)
+                resultbrigecore &= stdoutput
+            Next
+            '...Action
+        Catch ex As Exception
+            dserr.args.Add(ex.Message)
+            dserr.new_error(conserr.errortype.DOTNETERROR, -1, Nothing, Nothing)
+        End Try
     End Sub
 End Class
