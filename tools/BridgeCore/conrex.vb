3086c48b25826c6cbfa83004aedf96de71387bb9
AminCoder <ruzeslarrey@gmail.com>
2021-11-27 23:41:01 +0330

    New Fields :: `TARGETMAJORVER` , `DOT` , `CMA` , ...

--- tools/BridgeCore/conrex.vb
+++ tools/BridgeCore/conrex.vb
@@ -1,5 +1,11 @@
 ﻿Public Class conrex
     Friend Shared TARGETFRAMEWORK As String
     Friend Shared TARGETCACHEDIR As String
+    Friend Shared TARGETMAJORVER As String
     Friend Const SPACE As String = " "
+    Public Const DNLIBFORMAT As String = ".dll"
+    Public Const DOT As Char = "."
+    Public Const NULL As String = Nothing
+    Public Const CMA As Char = ","
+    Friend Shared DOTNETCOREROOT As String = Environment.GetFolderPath(Environment.SpecialFolder.ProgramFiles) & "\dotnet\shared\Microsoft.NETCore.App\"
 End Class
