5c6e4b0e973c9236ee2e3d8fdd2cc170c3c9d47f
AminCoder <ruzeslarrey@gmail.com>
2021-11-27 23:42:19 +0330

    New Method :: Get target framework root

--- tools/BridgeCore/mainproc.vb
+++ tools/BridgeCore/mainproc.vb
@@ -1,10 +1,26 @@
 Imports System
+Imports System.IO
 
 Module mainproc
     Sub Main(args As String())
         If args.Length < 2 Then Return
         conrex.TARGETFRAMEWORK = args(0)
         conrex.TARGETCACHEDIR = args(1).Replace("@#SP#", conrex.SPACE)
-        Console.WriteLine(conrex.TARGETCACHEDIR)
+        conrex.TARGETMAJORVER = conrex.TARGETFRAMEWORK.Remove(conrex.TARGETFRAMEWORK.IndexOf("."))
+        libreg.init_libraries(get_targetdir())
+        Environment.Exit(0)
     End Sub
+    Function get_targetdir() As String
+        If Directory.Exists(conrex.DOTNETCOREROOT) = False Then
+            Console.Write(".Net Core not found.")
+            Environment.Exit(-1)
+        End If
+        For Each sdir In Directory.GetDirectories(conrex.DOTNETCOREROOT)
+            If sdir.StartsWith(conrex.DOTNETCOREROOT & conrex.TARGETMAJORVER & conrex.DOT) Then
+                Return sdir
+            End If
+        Next
+        Console.Write("Version " & conrex.TARGETMAJORVER & " of .NET Core is not installed")
+        Environment.Exit(-1)
+    End Function
 End Module
