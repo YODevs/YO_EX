377395f2416587e8b276f48fae22fac0cc26e5ba
AminCoder <53114901+YODevs@users.noreply.github.com>
2022-01-08 00:10:33 -0800

    New Class :: Structures of assembly type

---
+++ tools/BridgeCore/struct.vb
@@ -0,0 +1,24 @@
+﻿Public Class struct
+
+    Public Structure _method
+        Dim name As String
+        Dim returntype As Type
+        Dim parameter() As _parameterinfo
+        Dim isstatic, isvirtual, ispublic As Boolean
+    End Structure
+    Public Structure _parameterinfo
+        Dim __type As _type
+        Dim name As String
+        Dim isout As Boolean
+        Dim position As Integer
+    End Structure
+    Public Structure _type
+        Dim name, _namespace, fullname, assemblyqualifiedname As String
+        Dim isarray, isenum, isvaluetype, isclass As Boolean
+        Dim __assembly As _assembly
+    End Structure
+
+    Public Structure _assembly
+        Dim location, fullname, tostring As String
+    End Structure
+End Class
