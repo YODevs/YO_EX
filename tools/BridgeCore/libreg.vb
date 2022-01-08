66c77a00bfb8e90e70709e479d19cf5bb3be05d4
AminCoder <53114901+YODevs@users.noreply.github.com>
2022-01-08 00:11:13 -0800

    New Action :: Code for test

--- tools/BridgeCore/libreg.vb
+++ tools/BridgeCore/libreg.vb
@@ -5,11 +5,30 @@ Public Class libreg
     Public Shared files As New ArrayList
     Friend Shared assemblymap As mapstoredata
     Friend Shared externtypes As mapstoredata
-    Friend Shared types()() As Type
-    Public Shared Sub init_libraries(path As String)
+    Public Shared s As Type
+    Public Shared types()() As MethodInfo
+
+    Public Shared _externinf() As _extern_info
+    Public Structure _extern_info
+        Dim name As String
+        Dim asmname As String
+        Dim fullname As String
+        Dim msnamespace As String
+        Dim location As String
+        '  Dim asmmethods() As MethodInfo
+        Dim asmmethod() As _method
+    End Structure
+    Public Structure _method
+        Dim methodname As String
+        Dim methodparameter As String
+    End Structure
+    Public Shared Sub init_lib(path As String)
         assemblymap = New mapstoredata
         externtypes = New mapstoredata
         get_dll_list(path, files, True)
+        If files.Count = 0 Then
+            Environment.Exit(-2)
+        End If
         get_dll_info()
     End Sub
 
@@ -23,10 +42,46 @@ Public Class libreg
 
             End Try
         Next
+        Console.WriteLine("Start")
+        type_serializer()
+        Console.WriteLine("End")
         Environment.Exit(0)
     End Sub
 
-    Private Shared Sub extract_dt_info_to_map(asm As Assembly)
+    Public Shared Sub type_serializer()
+        Dim filepath As String = conrex.TARGETCACHEDIR
+        Dim nxmlserializer As New Xml.Serialization.XmlSerializer(GetType(_extern_info()))
+        Dim nstreamwriter As New IO.StreamWriter(filepath & "\types.xml")
+        nxmlserializer.Serialize(nstreamwriter, _externinf)
+    End Sub
+
+    Public Shared Sub extract_dt_info_to_map(asm As Assembly)
+        'assemblymap.add(asm.GetName.Name, asm.Location)
+        Static Dim indexarray As Int16 = 0
+        For Each asmtype In asm.GetTypes()
+            Array.Resize(_externinf, indexarray + 1)
+            _externinf(indexarray) = New _extern_info
+            If asmtype.Namespace <> conrex.NULL Then
+                _externinf(indexarray).name = asmtype.Namespace & conrex.DOT
+            End If
+            _externinf(indexarray).name &= asmtype.Name
+            _externinf(indexarray).asmname = asm.GetName.Name
+            _externinf(indexarray).fullname = asmtype.FullName
+            _externinf(indexarray).msnamespace = asmtype.Namespace
+            _externinf(indexarray).location = asm.Location
+            Dim i As Integer = 0
+            For Each c In asmtype.GetMethods
+                Array.Resize(_externinf(indexarray).asmmethod, i + 1)
+                _externinf(indexarray).asmmethod(i) = New _method
+                _externinf(indexarray).asmmethod(i).methodname = c.Name
+                _externinf(indexarray).asmmethod(i).methodparameter = c.ReturnType.Name
+                i += 1
+            Next
+            '_externinf(indexarray).asmmethods = asmtype.GetMethods
+            indexarray += 1
+        Next
+    End Sub
+    Private Sub extract_dt_info_to_map2(asm As Assembly)
         assemblymap.add(asm.GetName.Name, asm.Location)
         Static Dim indexarray As Int16 = 0
         Array.Resize(types, indexarray + 1)
@@ -39,7 +94,7 @@ Public Class libreg
             End If
             nsname &= asmtype.Name
             externtypes.add(nsname, indexarray & conrex.CMA & index)
-            types(indexarray)(index) = asmtype
+            '   types(indexarray)(index) = asmtype.GetMethods
             index += 1
         Next
         indexarray += 1
--- tools/BridgeCore/mainproc.vb
+++ tools/BridgeCore/mainproc.vb
@@ -1,5 +1,6 @@
 Imports System
 Imports System.IO
+Imports ExtendedXmlSerializer
 
 Module mainproc
     Sub Main(args As String())
@@ -7,7 +8,8 @@ Module mainproc
         conrex.TARGETFRAMEWORK = args(0)
         conrex.TARGETCACHEDIR = args(1).Replace("@#SP#", conrex.SPACE)
         conrex.TARGETMAJORVER = conrex.TARGETFRAMEWORK.Remove(conrex.TARGETFRAMEWORK.IndexOf("."))
-        libreg.init_libraries(get_targetdir())
+        libreg.init_lib(get_targetdir())
+        Console.ReadLine()
         Environment.Exit(0)
     End Sub
     Function get_targetdir() As String
@@ -21,6 +23,14 @@ Module mainproc
             End If
         Next
         Console.Write("Version " & conrex.TARGETMAJORVER & " of .NET Core is not installed")
-        Environment.Exit(-1)
+        Environment.Exit(-2)
     End Function
+
+    Public Sub WriteXML()
+        Dim writer As New System.Xml.Serialization.XmlSerializer(GetType(libreg))
+        Dim file As New System.IO.StreamWriter(
+          "C:\Users\AminCoder\AppData\Local\YOLang\.NetCoreCache\6.0.100\types.xml")
+        ' writer.Serialize(file, CObj(libreg))
+        file.Close()
+    End Sub
 End Module
