fd87b42ac8ef9aaa4db42606e1c9ab013465c18d
AminCoder <53114901+YODevs@users.noreply.github.com>
2022-01-10 03:08:45 -0800

    New Action :: Set Overload to `add_point(...)` & `new_series(...)`

--- yolib/chart.vb
+++ yolib/chart.vb
@@ -1,7 +1,17 @@
-﻿Public Class chart
+﻿Imports System.Windows.Forms.DataVisualization.Charting
+
+Public Class chart
     Dim chartform As chartui
+    Dim charttypes As map
     Public Sub New()
         chartform = New chartui
+        charttypes = New map
+        Dim ccharttype As SeriesChartType
+        Dim values As Array = [Enum].GetValues(ccharttype.GetType)
+        Dim keys As String() = [Enum].GetNames(ccharttype.GetType)
+        For index = 0 To values.Length - 1
+            charttypes.add(keys(index).ToLower, values(index))
+        Next
     End Sub
     Public Property formtitle() As String
         Get
@@ -11,12 +21,26 @@
             chartform.Text = value
         End Set
     End Property
-
     Public Sub new_series(name As String)
+        new_series(name, "Area")
+    End Sub
+    Public Sub new_series(name As String, charttypename As String)
         chartform.chart.Series.Add(name)
+        Dim localcharttype As String = charttypes.get(charttypename.ToLower)
+        If localcharttype <> String.Empty Then
+            chartform.chart.Series(name).ChartType = Convert.ToInt32(localcharttype)
+        Else
+            Throw New Exception(charttypename & " - The selected chart type could not be found! You can use charts like [Pie,Line,Bar,Area,Bubble,Point,Radar,...].")
+        End If
     End Sub
 
     Public Sub add_point(name As String, xvalue As Integer, yvalue As Integer)
+        chartform.chart.Series(name).Points.AddXY(xvalue, yvalue)
+    End Sub
+    Public Sub add_point(name As String, xvalue As Double, yvalue As Double)
+        chartform.chart.Series(name).Points.AddXY(xvalue, yvalue)
+    End Sub
+    Public Sub add_point(name As String, xvalue As Single, yvalue As Single)
         chartform.chart.Series(name).Points.AddXY(xvalue, yvalue)
     End Sub
 
