f8f8a5d40e04e7f12fde63d1ca367235a20e565e
AminCoder <53114901+YODevs@users.noreply.github.com>
2022-01-11 09:01:08 -0800

    New Action :: Menubar added to `chartui`

--- yolib/chartui.Designer.vb
+++ yolib/chartui.Designer.vb
@@ -25,7 +25,15 @@ Partial Class chartui
         Dim ChartArea1 As System.Windows.Forms.DataVisualization.Charting.ChartArea = New System.Windows.Forms.DataVisualization.Charting.ChartArea()
         Dim Legend1 As System.Windows.Forms.DataVisualization.Charting.Legend = New System.Windows.Forms.DataVisualization.Charting.Legend()
         Me.chart = New System.Windows.Forms.DataVisualization.Charting.Chart()
+        Me.MenuStrip1 = New System.Windows.Forms.MenuStrip()
+        Me.FileToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.SaveChartToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.ToolStripSeparator1 = New System.Windows.Forms.ToolStripSeparator()
+        Me.ExitToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.EditToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.EditViewPointsToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
         CType(Me.chart, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.MenuStrip1.SuspendLayout()
         Me.SuspendLayout()
         '
         'chart
@@ -37,11 +45,57 @@ Partial Class chartui
         Me.chart.Dock = System.Windows.Forms.DockStyle.Fill
         Legend1.Name = "Legend1"
         Me.chart.Legends.Add(Legend1)
-        Me.chart.Location = New System.Drawing.Point(0, 0)
+        Me.chart.Location = New System.Drawing.Point(0, 24)
         Me.chart.Name = "chart"
-        Me.chart.Size = New System.Drawing.Size(644, 421)
+        Me.chart.Size = New System.Drawing.Size(644, 397)
         Me.chart.TabIndex = 0
         Me.chart.Text = "My Chart"
+        '
+        'MenuStrip1
+        '
+        Me.MenuStrip1.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.FileToolStripMenuItem, Me.EditToolStripMenuItem})
+        Me.MenuStrip1.Location = New System.Drawing.Point(0, 0)
+        Me.MenuStrip1.Name = "MenuStrip1"
+        Me.MenuStrip1.Size = New System.Drawing.Size(644, 24)
+        Me.MenuStrip1.TabIndex = 1
+        Me.MenuStrip1.Text = "MenuStrip1"
+        '
+        'FileToolStripMenuItem
+        '
+        Me.FileToolStripMenuItem.DropDownItems.AddRange(New System.Windows.Forms.ToolStripItem() {Me.SaveChartToolStripMenuItem, Me.ToolStripSeparator1, Me.ExitToolStripMenuItem})
+        Me.FileToolStripMenuItem.Name = "FileToolStripMenuItem"
+        Me.FileToolStripMenuItem.Size = New System.Drawing.Size(37, 20)
+        Me.FileToolStripMenuItem.Text = "&File"
+        '
+        'SaveChartToolStripMenuItem
+        '
+        Me.SaveChartToolStripMenuItem.Name = "SaveChartToolStripMenuItem"
+        Me.SaveChartToolStripMenuItem.Size = New System.Drawing.Size(128, 22)
+        Me.SaveChartToolStripMenuItem.Text = "Save chart"
+        '
+        'ToolStripSeparator1
+        '
+        Me.ToolStripSeparator1.Name = "ToolStripSeparator1"
+        Me.ToolStripSeparator1.Size = New System.Drawing.Size(125, 6)
+        '
+        'ExitToolStripMenuItem
+        '
+        Me.ExitToolStripMenuItem.Name = "ExitToolStripMenuItem"
+        Me.ExitToolStripMenuItem.Size = New System.Drawing.Size(128, 22)
+        Me.ExitToolStripMenuItem.Text = "Exit"
+        '
+        'EditToolStripMenuItem
+        '
+        Me.EditToolStripMenuItem.DropDownItems.AddRange(New System.Windows.Forms.ToolStripItem() {Me.EditViewPointsToolStripMenuItem})
+        Me.EditToolStripMenuItem.Name = "EditToolStripMenuItem"
+        Me.EditToolStripMenuItem.Size = New System.Drawing.Size(39, 20)
+        Me.EditToolStripMenuItem.Text = "&Edit"
+        '
+        'EditViewPointsToolStripMenuItem
+        '
+        Me.EditViewPointsToolStripMenuItem.Name = "EditViewPointsToolStripMenuItem"
+        Me.EditViewPointsToolStripMenuItem.Size = New System.Drawing.Size(160, 22)
+        Me.EditViewPointsToolStripMenuItem.Text = "Edit/View points"
         '
         'chartui
         '
@@ -50,15 +104,26 @@ Partial Class chartui
         Me.BackColor = System.Drawing.SystemColors.ButtonFace
         Me.ClientSize = New System.Drawing.Size(644, 421)
         Me.Controls.Add(Me.chart)
+        Me.Controls.Add(Me.MenuStrip1)
         Me.Font = New System.Drawing.Font("Consolas", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.MainMenuStrip = Me.MenuStrip1
         Me.Margin = New System.Windows.Forms.Padding(4, 3, 4, 3)
         Me.Name = "chartui"
         Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
         Me.Text = "chartui"
         CType(Me.chart, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.MenuStrip1.ResumeLayout(False)
+        Me.MenuStrip1.PerformLayout()
         Me.ResumeLayout(False)
+        Me.PerformLayout()
 
     End Sub
-
-    Friend WithEvents chart As Windows.Forms.DataVisualization.Charting.Chart
+    Friend WithEvents MenuStrip1 As Windows.Forms.MenuStrip
+    Friend WithEvents FileToolStripMenuItem As Windows.Forms.ToolStripMenuItem
+    Friend WithEvents SaveChartToolStripMenuItem As Windows.Forms.ToolStripMenuItem
+    Friend WithEvents ToolStripSeparator1 As Windows.Forms.ToolStripSeparator
+    Friend WithEvents ExitToolStripMenuItem As Windows.Forms.ToolStripMenuItem
+    Friend WithEvents EditToolStripMenuItem As Windows.Forms.ToolStripMenuItem
+    Friend WithEvents EditViewPointsToolStripMenuItem As Windows.Forms.ToolStripMenuItem
+    Public WithEvents chart As Windows.Forms.DataVisualization.Charting.Chart
 End Class
--- yolib/chartui.vb
+++ yolib/chartui.vb
@@ -1,3 +1,6 @@
 ﻿Public Class chartui
-
+    Private Sub EditViewPointsToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles EditViewPointsToolStripMenuItem.Click
+        Dim formpoint As New editpoint
+        formpoint.ShowDialog()
+    End Sub
 End Class
