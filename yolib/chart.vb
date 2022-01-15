52932515e23247455c9fe8fd2618caff50b71099
AminCoder <53114901+YODevs@users.noreply.github.com>
2022-01-15 06:14:55 -0800

    New Method :: Set default setting to `series` & `points`

--- yolib/chart.vb
+++ yolib/chart.vb
@@ -51,33 +51,47 @@ Public Class chart
 
     Public Sub new_series(name As String)
         new_series(name, "Area")
+        set_default_setting_series(name)
     End Sub
     Public Sub new_series(name As String, charttypename As String)
         chartform.chart.Series.Add(name)
         Dim localcharttype As String = charttypes.get(charttypename.ToLower)
         If localcharttype <> String.Empty Then
             chartform.chart.Series(name).ChartType = Convert.ToInt32(localcharttype)
+            set_default_setting_series(name)
         Else
             Throw New Exception(charttypename & " - The selected chart type could not be found! You can use charts like [Pie,Line,Bar,Area,Bubble,Point,Radar,...].")
         End If
     End Sub
 
     Public Sub add_point(seriesname As String, xvalue As Integer, yvalue As Integer)
-        chartform.chart.Series(seriesname).Points.AddXY(xvalue, yvalue)
+        Dim pointid As Integer = chartform.chart.Series(seriesname).Points.AddXY(xvalue, yvalue)
+        set_default_setting_point(seriesname, pointid, xvalue, yvalue)
     End Sub
     Public Sub add_point(seriesname As String, xvalue As Double, yvalue As Double)
-        chartform.chart.Series(seriesname).Points.AddXY(xvalue, yvalue)
+        Dim pointid As Integer = chartform.chart.Series(seriesname).Points.AddXY(xvalue, yvalue)
+        set_default_setting_point(seriesname, pointid, xvalue, yvalue)
     End Sub
     Public Sub add_point(seriesname As String, xvalue As Single, yvalue As Single)
-        chartform.chart.Series(seriesname).Points.AddXY(xvalue, yvalue)
+        Dim pointid As Integer = chartform.chart.Series(seriesname).Points.AddXY(xvalue, yvalue)
+        set_default_setting_point(seriesname, pointid, xvalue, yvalue)
     End Sub
+
+    Private Sub set_default_setting_point(seriesname As String, pointid As Integer, xvalue As Object, yvalue As Object)
+        chartform.chart.Series(seriesname).Points(pointid).MarkerStyle = MarkerStyle.Circle
+        chartform.chart.Series(seriesname).Points(pointid).ToolTip = String.Format("{3}{1}X:{0}{1}Y:{2}", xvalue, vbCrLf, yvalue, seriesname)
+    End Sub
+
+    Private Sub set_default_setting_series(seriesname As String)
+        chartform.chart.Series(seriesname).BorderWidth = 2
+    End Sub
+
     Public Sub set_asix_label(seriesname As String, index As Integer, value As String)
         chartform.chart.Series(seriesname).Points(index).AxisLabel = value
     End Sub
     Public Sub set_asix_label(index As Integer, value As String)
         chartform.chart.Series(0).Points(index).AxisLabel = value
     End Sub
-
     Public Sub show()
         chartform.ShowDialog()
     End Sub
