019a4f4c8a7ff10dee3f809c41a22305f8c9e46d
AminCoder <53114901+YODevs@users.noreply.github.com>
2022-02-14 17:23:15 +0330

    New Action :: Getting `assembly` of .NetCore libraries

--- tools/BridgeCore/libreg.vb
+++ tools/BridgeCore/libreg.vb
@@ -1,13 +1,14 @@
 ﻿Imports System.IO
 Imports System.Reflection
+Imports System.Text
 
 Public Class libreg
     Public Shared files As New ArrayList
     Friend Shared assemblymap As mapstoredata
     Friend Shared externtypes As mapstoredata
+    Friend Shared asmtypemapstore As mapstoredata
     Public Shared s As Type
     Public Shared types()() As MethodInfo
-
     Public Shared _externinf() As _extern_info
     Public Structure _extern_info
         Dim name As String
@@ -15,7 +16,6 @@ Public Class libreg
         Dim fullname As String
         Dim msnamespace As String
         Dim location As String
-        '  Dim asmmethods() As MethodInfo
         Dim asmmethod() As _method
     End Structure
     Public Structure _method
@@ -25,6 +25,7 @@ Public Class libreg
     Public Shared Sub init_lib(path As String)
         assemblymap = New mapstoredata
         externtypes = New mapstoredata
+        asmtypemapstore = New mapstoredata
         get_dll_list(path, files, True)
         If files.Count = 0 Then
             Environment.Exit(-2)
@@ -33,71 +34,22 @@ Public Class libreg
     End Sub
 
     Friend Shared Sub get_dll_info()
-        Dim asm As [Assembly]
+        extract_dt_info_to_map(Assembly.Load("System.Private.CoreLib"))
         For index = 0 To files.Count - 1
             Try
-                asm = [Assembly].LoadFrom(files(index).ToString)
-                extract_dt_info_to_map(asm)
+                Console.WriteLine(files(index).ToString)
+                extract_dt_info_to_map(Assembly.LoadFrom(files(index).ToString))
             Catch ex As Exception
-
             End Try
         Next
-        Console.WriteLine("Start")
-        type_serializer()
-        Console.WriteLine("End")
-        Environment.Exit(0)
-    End Sub
-
-    Public Shared Sub type_serializer()
-        Dim filepath As String = conrex.TARGETCACHEDIR
-        Dim nxmlserializer As New Xml.Serialization.XmlSerializer(GetType(_extern_info()))
-        Dim nstreamwriter As New IO.StreamWriter(filepath & "\types.xml")
-        nxmlserializer.Serialize(nstreamwriter, _externinf)
+        Dim yoda As New yoda
+        File.WriteAllText(conrex.TARGETCACHEDIR & "\types.yoda", yoda.WriteYODA_Map(asmtypemapstore.keys, asmtypemapstore.values, True))
     End Sub
 
     Public Shared Sub extract_dt_info_to_map(asm As Assembly)
-        'assemblymap.add(asm.GetName.Name, asm.Location)
-        Static Dim indexarray As Int16 = 0
-        For Each asmtype In asm.GetTypes()
-            Array.Resize(_externinf, indexarray + 1)
-            _externinf(indexarray) = New _extern_info
-            If asmtype.Namespace <> conrex.NULL Then
-                _externinf(indexarray).name = asmtype.Namespace & conrex.DOT
-            End If
-            _externinf(indexarray).name &= asmtype.Name
-            _externinf(indexarray).asmname = asm.GetName.Name
-            _externinf(indexarray).fullname = asmtype.FullName
-            _externinf(indexarray).msnamespace = asmtype.Namespace
-            _externinf(indexarray).location = asm.Location
-            Dim i As Integer = 0
-            For Each c In asmtype.GetMethods
-                Array.Resize(_externinf(indexarray).asmmethod, i + 1)
-                _externinf(indexarray).asmmethod(i) = New _method
-                _externinf(indexarray).asmmethod(i).methodname = c.Name
-                _externinf(indexarray).asmmethod(i).methodparameter = c.ReturnType.Name
-                i += 1
-            Next
-            '_externinf(indexarray).asmmethods = asmtype.GetMethods
-            indexarray += 1
+        For Each asmtype In asm.GetTypes
+            asmtypemapstore.add(asmtype.FullName, asmtype.Assembly.GetName.Name)
         Next
-    End Sub
-    Private Sub extract_dt_info_to_map2(asm As Assembly)
-        assemblymap.add(asm.GetName.Name, asm.Location)
-        Static Dim indexarray As Int16 = 0
-        Array.Resize(types, indexarray + 1)
-        Dim index As Integer = 0
-        For Each asmtype In asm.GetTypes()
-            Array.Resize(types(indexarray), index + 1)
-            Dim nsname As String = String.Empty
-            If asmtype.Namespace <> conrex.NULL Then
-                nsname = asmtype.Namespace & conrex.DOT
-            End If
-            nsname &= asmtype.Name
-            externtypes.add(nsname, indexarray & conrex.CMA & index)
-            '   types(indexarray)(index) = asmtype.GetMethods
-            index += 1
-        Next
-        indexarray += 1
     End Sub
 
     Friend Shared Function get_dll_list(dir As String, ByRef files As ArrayList, Optional head As Boolean = False) As Boolean
