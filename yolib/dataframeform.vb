b4b41babd5a5a00eaab1d2de0b0b75d9b6b181cc
AminCoder <53114901+YODevs@users.noreply.github.com>
2022-03-17 19:09:50 +0330

    New Action :: Set-in information of datagrade in footer

--- yolib/dataframeform.vb
+++ yolib/dataframeform.vb
@@ -1,3 +1,11 @@
 ﻿Public Class dataframeform
+    Public typeofstruct As String
+    Private Sub infotimer_Tick(sender As Object, e As EventArgs) Handles infotimer.Tick
+        dtginfotext.Text = String.Format("{0} Cols , {1} Rows ", dtg.Columns.Count, dtg.Rows.Count)
+        dtgstructtext.Text = String.Format("Type of structure : {0}", typeofstruct)
+    End Sub
 
+    Private Sub dataframeform_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        infotimer_Tick(Nothing, Nothing)
+    End Sub
 End Class
