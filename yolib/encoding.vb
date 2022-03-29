ae978d0834492c42fd44c0a95ff897b5855ad70e
AminCoder <53114901+YODevs@users.noreply.github.com>
2022-03-29 11:49:09 +0430

    New Class :: `Encoding`

---
+++ yolib/encoding.vb
@@ -0,0 +1,30 @@
+﻿Imports System.IO
+
+Public Class encoding
+
+    Public Shared Function utf8_get_bytes(value As String) As Byte()
+        Return System.Text.Encoding.UTF8.GetBytes(value)
+    End Function
+    Public Shared Function utf8_get_string(value As Byte()) As String
+        Return System.Text.Encoding.UTF8.GetString(value)
+    End Function
+    Public Shared Function utf32_get_bytes(value As String) As Byte()
+        Return System.Text.Encoding.UTF32.GetBytes(value)
+    End Function
+    Public Shared Function utf32_get_string(value As Byte()) As String
+        Return System.Text.Encoding.UTF32.GetString(value)
+    End Function
+    Public Shared Function ascii_get_bytes(value As String) As Byte()
+        Return System.Text.Encoding.ASCII.GetBytes(value)
+    End Function
+    Public Shared Function ascii_get_string(value As Byte()) As String
+        Return System.Text.Encoding.ASCII.GetString(value)
+    End Function
+
+    Public Shared Function unicode_get_bytes(value As String) As Byte()
+        Return System.Text.Encoding.Unicode.GetBytes(value)
+    End Function
+    Public Shared Function unicode_get_string(value As Byte()) As String
+        Return System.Text.Encoding.Unicode.GetString(value)
+    End Function
+End Class
