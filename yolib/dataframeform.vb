882df75169c1d4b3511020b87f7476488567b1fb
AminCoder <53114901+YODevs@users.noreply.github.com>
2022-03-30 11:28:07 +0430

    New Action :: Export dataframe data to `csv`

--- yolib/dataframeform.vb
+++ yolib/dataframeform.vb
@@ -1,4 +1,6 @@
-﻿Public Class dataframeform
+﻿Imports System.Text
+
+Public Class dataframeform
     Public typeofstruct As String
     Private Sub infotimer_Tick(sender As Object, e As EventArgs) Handles infotimer.Tick
         dtginfotext.Text = String.Format("{0} Cols , {1} Rows ", dtg.Columns.Count, dtg.Rows.Count)
@@ -14,5 +16,36 @@
         If dialogresult = Windows.Forms.DialogResult.OK OrElse dialogresult = Windows.Forms.DialogResult.Yes Then
             dtg.DefaultCellStyle.Font = fontselector.Font
         End If
+    End Sub
+
+    Private Sub exportdatalabel_Click(sender As Object, e As EventArgs) Handles exportdatalabel.Click
+        Dim sb As New StringBuilder
+        Dim columnscount As Integer = dtg.Columns.Count - 1
+        Dim rowscount As Integer = dtg.Rows.Count - 1
+
+        For index = 0 To dtg.Columns.Count - 1
+            sb.Append(dtg.Columns(index).HeaderText)
+            If index < dtg.Columns.Count - 1 Then
+                sb.Append(",")
+            End If
+        Next
+        sb.AppendLine()
+        For index = 0 To rowscount
+            For i2 = 0 To columnscount
+                Dim getitem As String = dtg.Item(i2, index).Value
+                If getitem.Contains(",") Then
+                    getitem = String.Format("""{0}""", getitem)
+                End If
+                sb.Append(getitem)
+                If i2 <> columnscount Then
+                    sb.Append(",")
+                End If
+            Next
+            sb.AppendLine()
+        Next
+
+        IO.File.WriteAllText(environment.appdir & "\output.csv", sb.ToString)
+        MsgBox("Csv output generated successfully.
+Storage location:" & environment.appdir & "\output.csv", MsgBoxStyle.Information)
     End Sub
 End Class
