aef1bc9a24fee4f166edbb329c31e538f8b93f13
AminCoder <53114901+YODevs@users.noreply.github.com>
2022-04-06 11:49:37 +0430

    New Action :: Added project default files

---
+++ yolib/yodefopt/AssemblyInfo.vb
@@ -0,0 +1,35 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' General Information about an assembly is controlled through the following
+' set of attributes. Change these attribute values to modify the information
+' associated with an assembly.
+
+' Review the values of the assembly attributes
+
+<Assembly: AssemblyTitle("YOLIB")>
+<Assembly: AssemblyDescription("By YODev")>
+<Assembly: AssemblyCompany("YO Lang")>
+<Assembly: AssemblyProduct("YOLIB")>
+<Assembly: AssemblyCopyright("Copyright ©  2021 YOLang")>
+<Assembly: AssemblyTrademark("YODev")>
+
+<Assembly: ComVisible(False)>
+
+'The following GUID is for the ID of the typelib if this project is exposed to COM
+<Assembly: Guid("66769d83-728a-4597-811d-fa9694cec26b")>
+
+' Version information for an assembly consists of the following four values:
+'
+'      Major Version
+'      Minor Version
+'      Build Number
+'      Revision
+'
+' You can specify all the values or you can default the Build and Revision Numbers
+' by using the '*' as shown below:
+' <Assembly: AssemblyVersion("1.0.*")>
+
+<Assembly: AssemblyVersion("1.0.0.0")>
+<Assembly: AssemblyFileVersion("1.0.0.0")>
