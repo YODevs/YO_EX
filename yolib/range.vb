b31ad65cecf3d6ef6cfa5757aa3f9933a931d0bb
AminCoder <53114901+YODevs@users.noreply.github.com>
2022-04-12 17:06:31 +0430

    New Method :: Get Expression of range

--- yolib/range.vb
+++ yolib/range.vb
@@ -27,6 +27,10 @@
         Return items
     End Function
 
+    Public Shared Function get_range(startpoint As Integer, endpoint As Integer, [step] As Integer, ignorelastpoint As Boolean, expression As String) As list
+        Return get_range(Convert.ToDouble(startpoint), Convert.ToDouble(endpoint), Convert.ToDouble([step]), ignorelastpoint, expression)
+    End Function
+
     Public Shared Function get_range(startpoint As Double, endpoint As Double) As list
         Return get_range(startpoint, endpoint, 1.0, True)
     End Function
@@ -60,4 +64,40 @@
         End If
         Return items
     End Function
+
+    Public Shared Function get_range(startpoint As Double, endpoint As Double, [step] As Double, ignorelastpoint As Boolean, expression As String) As list
+        Dim items As New YOLIB.list
+        Dim dt As New DataTable
+        Dim critem As Double = startpoint
+        Dim bfitem As Double = 0.0
+        Dim zresp As Double = 0.0
+        If endpoint > startpoint Then
+            If [step] <= 0 Then Throw New Exception("The value of the step parameter is not allowed for this range; Intervals greater than than 0 should be used.")
+            bfitem = critem - [step]
+            While critem <= endpoint
+                zresp = dt.Compute(expression.ToLower.Replace("x", critem).Replace("y", bfitem).Replace("z", zresp), String.Empty)
+                items.add(zresp)
+                bfitem = critem
+                critem += [step]
+            End While
+        ElseIf endpoint < startpoint Then
+            If [step] >= 0 Then Throw New Exception("The value of the step parameter is not allowed for this range; A range of less than 0 should be used.")
+            bfitem = critem + [step]
+            While critem >= endpoint
+                zresp = dt.Compute(expression.ToLower.Replace("x", critem).Replace("y", bfitem).Replace("z", zresp), String.Empty)
+                bfitem = critem
+                critem += [step]
+            End While
+        End If
+        If startpoint = endpoint Then
+            bfitem = critem - [step]
+            items.add(dt.Compute(expression.ToLower.Replace("x", critem).Replace("y", bfitem).Replace("z", zresp), String.Empty))
+        Else
+            If ignorelastpoint = True AndAlso items.count > 1 Then
+                items.remove_at(items.count - 1)
+            End If
+        End If
+        Return items
+    End Function
+
 End Class
