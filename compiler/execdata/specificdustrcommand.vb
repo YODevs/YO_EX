6775ad369909f1ce4834d84b79d0eaf99b4ac963
AminCoder <53114901+YODevs@users.noreply.github.com>
2022-04-30 16:10:21 +0430

    New Method :: Replacement of symbols in strings

--- compiler/execdata/specificdustrcommand.vb
+++ compiler/execdata/specificdustrcommand.vb
@@ -2,16 +2,20 @@
 
 Public Class specificdustrcommand
     Public Shared spdustr As mapstoredata
+    Friend Shared symbollist As YOLIB.map
     Friend Shared Sub init()
         spdustr = New mapstoredata
         For index = 0 To conrex.spdustrcommand.Length - 1
             spdustr.add(conrex.spdustrcommand(index), conrex.spdustrreact(index))
         Next
+        symbollist = New YOLIB.map
+        symbollist.load(conrex.APPDIR & "\iniopt\sym\symbols.yoda")
     End Sub
 
     Friend Shared Sub get_specific_dustr_command(ByRef value As String)
         rem_specific_cil_char(value)
         set_asc_dustr_command(value)
+        set_symbol_dustr_command(value)
         If value.Contains("#") Then
             For index = 0 To conrex.spdustrcommand.Length - 1
                 Dim resultspdustr As mapstoredata.dataresult = spdustr.find(conrex.spdustrcommand(index))
@@ -27,7 +31,18 @@ Public Class specificdustrcommand
             Return
         End If
     End Sub
-
+    Private Shared Sub set_symbol_dustr_command(ByRef value As String)
+        Dim regexresult As MatchCollection = Regex.Matches(value, compdt.SYMBOLREGEXFMTLIT)
+        For index = 0 To regexresult.Count - 1
+            Dim result As String = regexresult(index).Value
+            result = result.Remove(0, 3)
+            result = result.Remove(result.Length - 1)
+            Dim newvalue As String = symbollist.get(result)
+            If result <> conrex.NULL Then
+                value = value.Replace(regexresult(index).Value, newvalue)
+            End If
+        Next
+    End Sub
     Private Shared Sub set_asc_dustr_command(ByRef value As String)
         Dim regexresult As MatchCollection = Regex.Matches(value, "\#\[\d+\]")
         For index = 0 To regexresult.Count - 1
