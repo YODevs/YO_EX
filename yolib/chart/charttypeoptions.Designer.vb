1e0a265beca4e3f4aba6d560b74570080a73b216
AminCoder <53114901+YODevs@users.noreply.github.com>
2022-05-02 15:16:59 +0430

    New Action :: Change the chart type by form

---
+++ yolib/chart/charttypeoptions.Designer.vb
@@ -0,0 +1,108 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class charttypeoptions
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.seriescombo = New System.Windows.Forms.ComboBox()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.chartcombo = New System.Windows.Forms.ComboBox()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(32, 15)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(45, 13)
+        Me.Label1.TabIndex = 4
+        Me.Label1.Text = "Series : "
+        '
+        'seriescombo
+        '
+        Me.seriescombo.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList
+        Me.seriescombo.FlatStyle = System.Windows.Forms.FlatStyle.Popup
+        Me.seriescombo.FormattingEnabled = True
+        Me.seriescombo.Location = New System.Drawing.Point(83, 12)
+        Me.seriescombo.Name = "seriescombo"
+        Me.seriescombo.Size = New System.Drawing.Size(175, 21)
+        Me.seriescombo.TabIndex = 3
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(32, 51)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(38, 13)
+        Me.Label2.TabIndex = 6
+        Me.Label2.Text = "Chart :"
+        '
+        'chartcombo
+        '
+        Me.chartcombo.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList
+        Me.chartcombo.FlatStyle = System.Windows.Forms.FlatStyle.Popup
+        Me.chartcombo.FormattingEnabled = True
+        Me.chartcombo.Location = New System.Drawing.Point(83, 48)
+        Me.chartcombo.Name = "chartcombo"
+        Me.chartcombo.Size = New System.Drawing.Size(175, 21)
+        Me.chartcombo.TabIndex = 5
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(109, 92)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(75, 23)
+        Me.Button1.TabIndex = 7
+        Me.Button1.Text = "Change"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'charttypeoptions
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.SystemColors.ButtonHighlight
+        Me.ClientSize = New System.Drawing.Size(296, 146)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.chartcombo)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.seriescombo)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow
+        Me.MaximizeBox = False
+        Me.MinimizeBox = False
+        Me.Name = "charttypeoptions"
+        Me.ShowIcon = False
+        Me.ShowInTaskbar = False
+        Me.Text = "Chart Type"
+        Me.TopMost = True
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Label1 As Windows.Forms.Label
+    Friend WithEvents seriescombo As Windows.Forms.ComboBox
+    Friend WithEvents Label2 As Windows.Forms.Label
+    Friend WithEvents chartcombo As Windows.Forms.ComboBox
+    Friend WithEvents Button1 As Windows.Forms.Button
+End Class
---
+++ yolib/chart/charttypeoptions.vb
@@ -0,0 +1,52 @@
+﻿Imports System.Windows.Forms
+Imports System.Windows.Forms.DataVisualization.Charting
+
+Public Class charttypeoptions
+    Dim formid As Integer = -1
+    Dim chart As Windows.Forms.DataVisualization.Charting.Chart
+    Dim charttypes As New map
+    Private Sub charttypeoptions_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        For fsindex = 0 To Application.OpenForms.Count - 1
+            If Application.OpenForms(fsindex).Name = "chartui" Then
+                formid = fsindex
+                Exit For
+            End If
+        Next
+        If formid = -1 Then Me.Close()
+        chart = Application.OpenForms(formid).Controls(0)
+        If chart.Series.Count = -1 Then
+            MsgBox("No series specified for the chart! Define it first.", MsgBoxStyle.Critical, "Series Error")
+            Me.Close()
+        End If
+        For fseriesindex = 0 To chart.Series.Count - 1
+            seriescombo.Items.Add(chart.Series(fseriesindex).Name)
+        Next
+        set_chart_type()
+    End Sub
+
+    Private Sub set_chart_type()
+        Dim ccharttype As SeriesChartType
+        Dim values As Array = [Enum].GetValues(ccharttype.GetType)
+        Dim keys As String() = [Enum].GetNames(ccharttype.GetType)
+        For index = 0 To values.Length - 1
+            charttypes.add(keys(index), values(index))
+            chartcombo.Items.Add(keys(index))
+        Next
+    End Sub
+
+    Private Sub seriescombo_TextChanged(sender As Object, e As EventArgs) Handles seriescombo.TextChanged
+        Try
+            chartcombo.SelectedIndex = charttypes.get(chart.Series(seriescombo.Text).ChartType.ToString())
+        Catch ex As Exception
+            MsgBox(ex.Message, MsgBoxStyle.Critical)
+        End Try
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Try
+            chart.Series(seriescombo.Text).ChartType = charttypes.get(chartcombo.SelectedItem)
+        Catch ex As Exception
+            MsgBox(ex.Message, MsgBoxStyle.Critical)
+        End Try
+    End Sub
+End Class
