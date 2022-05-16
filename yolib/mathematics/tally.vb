91bb9dbbbaaeceb0fd69d5a84694f660888060df
AminCoder <53114901+YODevs@users.noreply.github.com>
2022-05-16 13:04:07 +0430

    New Method :: Get group of an item

--- yolib/mathematics/tally.vb
+++ yolib/mathematics/tally.vb
@@ -5,6 +5,16 @@
         countlist = New ArrayList
     End Sub
 
+    Private privgrouprange As Integer = 5
+    Public Property grouprange() As Integer
+        Get
+            Return privgrouprange
+        End Get
+        Set(ByVal value As Integer)
+            If value <= 0 Then value = 5
+            privgrouprange = value
+        End Set
+    End Property
     Public Sub add(item As String)
         Dim indexref As Integer = 0
         If check_item_repetition(item, indexref) Then
@@ -21,6 +31,14 @@
             Return CInt(countlist(indexref))
         End If
         Return 0
+    End Function
+
+    Public Function get_group(item As String) As Single
+        Dim indexref As Integer = 0
+        If check_item_repetition(item, indexref) Then
+            Return CSng(CInt(countlist(indexref)) / privgrouprange)
+        End If
+        Return 0.0
     End Function
 
     Private Function check_item_repetition(item As String, ByRef indexref As Integer) As Boolean
