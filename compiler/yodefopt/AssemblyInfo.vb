ea66163e8cada7c94f63dc7475bcb3bc76927a0e
AminCoder <53114901+YODevs@users.noreply.github.com>
2022-06-16 11:16:50 +0430

    New Action :: Upload the required compiler files

---
+++ compiler/yodefopt/AssemblyInfo.vb
@@ -0,0 +1,35 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' General Information about an assembly is controlled through the following
+' set of attributes. Change these attribute values to modify the information
+' associated with an assembly.
+
+' Review the values of the assembly attributes
+
+<Assembly: AssemblyTitle("YOCA")>
+<Assembly: AssemblyDescription("YO Compiler API")>
+<Assembly: AssemblyCompany("YODevs")>
+<Assembly: AssemblyProduct("YO Compiler")>
+<Assembly: AssemblyCopyright("Copyright © 2017 - 2022")>
+<Assembly: AssemblyTrademark("YODevs")>
+
+<Assembly: ComVisible(False)>
+
+'The following GUID is for the ID of the typelib if this project is exposed to COM
+<Assembly: Guid("17a46f65-5544-4d66-9dee-a01c4d85b31f")>
+
+' Version information for an assembly consists of the following four values:
+'
+'      Major Version
+'      Minor Version
+'      Build Number
+'      Revision
+'
+' You can specify all the values or you can default the Build and Revision Numbers
+' by using the '*' as shown below:
+' <Assembly: AssemblyVersion("1.0.*")>
+
+<Assembly: AssemblyVersion("0.1.25.0")>
+<Assembly: AssemblyFileVersion("0.1.25.0")>
