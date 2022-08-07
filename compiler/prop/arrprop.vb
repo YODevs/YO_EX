733a6df96a52abf8aad029fb48f84afb3122c23b
AminCoder <53114901+YODevs@users.noreply.github.com>
2022-08-07 17:13:47 +0430

    New Action :: Set datatype of array property

--- compiler/prop/arrprop.vb
+++ compiler/prop/arrprop.vb
@@ -1,4 +1,11 @@
 ﻿Public Class arrprop
+
+    Private Shared cidentdatatype As String = String.Empty
+    Public Shared ReadOnly Property datatype() As String
+        Get
+            Return cidentdatatype
+        End Get
+    End Property
     Friend Shared Function check_array_property(ByRef ilmethod As ilformat._ilmethodcollection, linecodestruc As xmlunpkd.linecodestruc, propresult As identvalid._resultidentcvaild, varname As String) As Boolean
         Dim gtypeinfo As ilformat._typeinfo = servinterface.get_variable_type(ilmethod, varname)
         If gtypeinfo.isarray = False Then Return False
@@ -19,11 +26,13 @@
         illdloc.ld_identifier(varname, ilmethod, varlinecodestruc, Nothing, get_arr_type(gtypeinfo))
         cil.ldlen(ilmethod.codes)
         cil.conv_to_int32(ilmethod.codes)
+        cidentdatatype = "int32"
     End Sub
     Private Shared Sub get_longlength(ByRef ilmethod As ilformat._ilmethodcollection, varlinecodestruc As xmlunpkd.linecodestruc, varname As String, gtypeinfo As ilformat._typeinfo)
         illdloc.ld_identifier(varname, ilmethod, varlinecodestruc, Nothing, get_arr_type(gtypeinfo))
         cil.ldlen(ilmethod.codes)
         cil.conv_to_int64(ilmethod.codes)
+        cidentdatatype = "int64"
     End Sub
 
     Private Shared Function get_arr_type(gtypeinfo As ilformat._typeinfo) As String
@@ -33,4 +42,7 @@
         Return gtypeinfo.fullname
     End Function
 
+    Friend Shared Sub reset_datatype()
+        cidentdatatype = String.Empty
+    End Sub
 End Class
