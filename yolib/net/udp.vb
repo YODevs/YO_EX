505689b2ce6ff8e3a8a762f50bd7264f5dfbae5c
AminCoder <53114901+YODevs@users.noreply.github.com>
2022-08-22 12:06:24 +0430

    New Method :: Listen & Receive datagram , `listen(...)`

--- yolib/net/udp.vb
+++ yolib/net/udp.vb
@@ -5,11 +5,18 @@ Public Class udp
     Dim udp As UdpClient
     Dim ipset As IPAddress
     Dim ip As String, port As Integer
+    Public thgetdatagram As System.Threading.Thread
+    Public ipendpoint As System.Net.IPEndPoint
+    Private datalist As YOLIB.list
+    Private _hasdata As Boolean = False
+    Private _latestdata As String = String.Empty
     Public Sub New(ip As String, port As Integer)
         Me.ip = ip
         Me.port = port
         ipset = IPAddress.Parse(ip)
         udp = New UdpClient
+        ipendpoint = New System.Net.IPEndPoint(System.Net.IPAddress.Any, 0)
+        datalist = New list
     End Sub
 
     Public Sub send(text As String)
@@ -18,4 +25,39 @@ Public Class udp
         udp.Connect(ipset, port)
         udp.Send(datagram, datagram.Length)
     End Sub
+
+    Public Sub listen(port As Integer)
+        udp = New UdpClient(port)
+        thgetdatagram = New System.Threading.Thread(AddressOf get_datagrams_event)
+        thgetdatagram.Start()
+    End Sub
+
+    Private Sub get_datagrams_event()
+        Dim recbytes As [Byte]() = udp.Receive(ipendpoint)
+        Dim getdata As String = System.Text.Encoding.ASCII.GetChars(recbytes)
+        If IsNothing(datalist) Then datalist = New list()
+        datalist.add(getdata)
+        _hasdata = True
+        _latestdata = getdata
+        thgetdatagram = New System.Threading.Thread(AddressOf get_datagrams_event)
+        thgetdatagram.Start()
+    End Sub
+
+    Public ReadOnly Property has_data() As Boolean
+        Get
+            If IsNothing(udp) Then Return False
+            Return _hasdata
+        End Get
+    End Property
+
+    Public ReadOnly Property latest_data() As String
+        Get
+            If _hasdata = False Then
+                Return String.Empty
+            Else
+                _hasdata = False
+                Return _latestdata
+            End If
+        End Get
+    End Property
 End Class
