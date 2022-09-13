4553632bc5f72e05c6045941b4616775d1c6bd36
AminCoder <53114901+YODevs@users.noreply.github.com>
2022-09-13 10:21:23 +0430

    New Method :: `attribute_value_refinement(...)`

---
+++ compiler/attri/methodattr.vb
@@ -0,0 +1,20 @@
+﻿Public Class methodattr
+
+    Public Shared Function attribute_value_refinement(value As String, token As tokenhared.token, linecinf As lexer.targetinf, sourceloc As String) As String
+        Select Case token
+            Case tokenhared.token.TYPE_BOOL
+                Return value.ToLower
+            Case tokenhared.token.TYPE_CO_STR
+                Return value
+            Case tokenhared.token.TYPE_DU_STR
+                Return value
+            Case tokenhared.token.TYPE_FLOAT
+                Return value
+            Case tokenhared.token.TYPE_INT
+                Return value
+            Case Else
+                dserr.args.Add("Attribute value format is not allowed.")
+                dserr.new_error(conserr.errortype.METHODATTRIBUTEERROR, linecinf.line, sourceloc, authfunc.get_line_error(sourceloc, linecinf, value), ".stacksize 8")
+        End Select
+    End Function
+End Class
