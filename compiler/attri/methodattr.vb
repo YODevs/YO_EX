9cf340ea38bc42177d9ace7c4199e6eb1bf87411
AminCoder <53114901+YODevs@users.noreply.github.com>
2022-09-16 11:19:46 +0430

    New Method :: `get_attribute_by_i32(...)`

--- compiler/attri/methodattr.vb
+++ compiler/attri/methodattr.vb
@@ -1,4 +1,6 @@
-﻿Public Class methodattr
+﻿Imports YOLIB
+
+Public Class methodattr
 
     Public Shared Function attribute_value_refinement(value As String, token As tokenhared.token, linecinf As lexer.targetinf, sourceloc As String) As String
         Select Case token
@@ -14,7 +16,24 @@
                 Return value
             Case Else
                 dserr.args.Add("Attribute value format is not allowed.")
-                dserr.new_error(conserr.errortype.METHODATTRIBUTEERROR, linecinf.line, sourceloc, authfunc.get_line_error(sourceloc, linecinf, value), ".stacksize 8")
+                dserr.new_error(conserr.errortype.METHODATTRIBUTEERROR, linecinf.line, sourceloc, authfunc.get_line_error(sourceloc, linecinf, value), ".maxstack 8")
         End Select
     End Function
+
+    Friend Shared Function get_attribute_by_i32(method As ilformat._ilmethodcollection, key As String, defresult As Integer) As Integer
+        Dim result As Object
+        Try
+            result = method.attributes.get(key)
+        Catch ex As Exception
+            Return defresult
+        End Try
+        If IsNumeric(result) = False Then
+            set_warning(method.name, key, defresult, result)
+        End If
+        Return result
+    End Function
+
+    Private Shared Sub set_warning(name As String, key As String, defresult As Integer, result As Object)
+        dswar.set_warning("Worng data-type", String.Format("The attribute '{0}' in the method '{1}' has the wrong value '{2}', so the compiler sets the default value '{3}' for this attribute.", key, name, result, defresult), ilbodybulider.path)
+    End Sub
 End Class
