f0b677ecc21e35e995e023364c7c7793c43b221f
AminCoder <53114901+YODevs@users.noreply.github.com>
2022-09-19 10:09:45 +0430

    New Method :: Method attributes supports bool values

--- compiler/attri/methodattr.vb
+++ compiler/attri/methodattr.vb
@@ -14,6 +14,10 @@ Public Class methodattr
                 Return value
             Case tokenhared.token.TYPE_INT
                 Return value
+            Case tokenhared.token.TRUE
+                Return value
+            Case tokenhared.token.FALSE
+                Return value
             Case Else
                 dserr.args.Add("Attribute value format is not allowed.")
                 dserr.new_error(conserr.errortype.METHODATTRIBUTEERROR, linecinf.line, sourceloc, authfunc.get_line_error(sourceloc, linecinf, value), ".maxstack 8")
@@ -29,11 +33,26 @@ Public Class methodattr
         End Try
         If IsNumeric(result) = False Then
             set_warning(method.name, key, defresult, result)
+            Return defresult
         End If
         Return result
     End Function
 
-    Private Shared Sub set_warning(name As String, key As String, defresult As Integer, result As Object)
+    Private Shared Sub set_warning(name As String, key As String, defresult As Object, result As Object)
         dswar.set_warning("Worng data-type", String.Format("The attribute '{0}' in the method '{1}' has the wrong value '{2}', so the compiler sets the default value '{3}' for this attribute.", key, name, result, defresult), ilbodybulider.path)
     End Sub
+
+    Friend Shared Function get_attribute_by_bool(method As ilformat._ilmethodcollection, key As String, defresult As Boolean) As Boolean
+        Dim result As Object
+        Try
+            result = method.attributes.get(key).ToString.ToLower
+        Catch ex As Exception
+            Return defresult
+        End Try
+        If result <> "true" AndAlso result <> "false" Then
+            set_warning(method.name, key, defresult, result)
+            Return defresult
+        End If
+        Return result
+    End Function
 End Class
