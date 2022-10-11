7387f0e9a1ebf656ad7da85141a60ca1f4bc9d17
AminCoder <53114901+YODevs@users.noreply.github.com>
2022-10-11 10:59:37 +0330

    New Action :: Checking path of `fileopenlist.yoda`

--- ide/init/chreqinit.vb
+++ ide/init/chreqinit.vb
@@ -4,6 +4,7 @@ Public Class chreqinit
     Public Shared Function requires_initialization() As Boolean
         If Directory.Exists(conrex.LOCALAPPDATADIR) = False Then Return True
         If File.Exists(conrex.PROJECTLISTFILE) = False Then Return True
+        If File.Exists(conrex.FILEOPENLIST) = False Then Return True
         Return False
     End Function
 End Class
