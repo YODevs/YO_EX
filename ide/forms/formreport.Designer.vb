8ec19e6974d326be4b54914472ecf2611079f456
AminCoder <53114901+YODevs@users.noreply.github.com>
2022-10-15 12:23:09 +0330

    New Form :: Reporting & Show the building progress

---
+++ ide/forms/formreport.Designer.vb
@@ -0,0 +1,108 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class formreport
+    Inherits Telerik.WinControls.UI.RadForm
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Me.Office2019DarkTheme1 = New Telerik.WinControls.Themes.Office2019DarkTheme()
+        Me.reporttext = New Telerik.WinControls.UI.RadTextBoxControl()
+        Me.reptimer = New System.Windows.Forms.Timer(Me.components)
+        Me.RadButton1 = New Telerik.WinControls.UI.RadButton()
+        Me.progressbar = New Telerik.WinControls.UI.RadProgressBar()
+        CType(Me.reporttext, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.RadButton1, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.progressbar, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'reporttext
+        '
+        Me.reporttext.Font = New System.Drawing.Font("Consolas", 10.1!)
+        Me.reporttext.IsReadOnly = True
+        Me.reporttext.Location = New System.Drawing.Point(12, 48)
+        Me.reporttext.Multiline = True
+        Me.reporttext.Name = "reporttext"
+        Me.reporttext.ShowClearButton = True
+        Me.reporttext.Size = New System.Drawing.Size(707, 322)
+        Me.reporttext.TabIndex = 0
+        Me.reporttext.ThemeName = "Office2019Dark"
+        '
+        'reptimer
+        '
+        Me.reptimer.Enabled = True
+        Me.reptimer.Interval = 10
+        '
+        'RadButton1
+        '
+        Me.RadButton1.Location = New System.Drawing.Point(609, 376)
+        Me.RadButton1.Name = "RadButton1"
+        Me.RadButton1.Size = New System.Drawing.Size(110, 24)
+        Me.RadButton1.TabIndex = 1
+        Me.RadButton1.Text = "Close"
+        Me.RadButton1.ThemeName = "Office2019Dark"
+        '
+        'progressbar
+        '
+        Me.progressbar.Location = New System.Drawing.Point(12, 13)
+        Me.progressbar.Name = "progressbar"
+        Me.progressbar.Size = New System.Drawing.Size(707, 24)
+        Me.progressbar.TabIndex = 2
+        Me.progressbar.Text = "Progress Report"
+        Me.progressbar.ThemeName = "Office2019Dark"
+        CType(Me.progressbar.GetChildAt(0), Telerik.WinControls.UI.RadProgressBarElement).Text = "Progress Report"
+        CType(Me.progressbar.GetChildAt(0).GetChildAt(0), Telerik.WinControls.UI.ProgressIndicatorElement).BackColor = System.Drawing.Color.FromArgb(CType(CType(7, Byte), Integer), CType(CType(211, Byte), Integer), CType(CType(29, Byte), Integer))
+        CType(Me.progressbar.GetChildAt(0).GetChildAt(0), Telerik.WinControls.UI.ProgressIndicatorElement).Visibility = Telerik.WinControls.ElementVisibility.Collapsed
+        CType(Me.progressbar.GetChildAt(0).GetChildAt(1), Telerik.WinControls.UI.UpperProgressIndicatorElement).BackColor = System.Drawing.Color.FromArgb(CType(CType(5, Byte), Integer), CType(CType(172, Byte), Integer), CType(CType(13, Byte), Integer))
+        CType(Me.progressbar.GetChildAt(0).GetChildAt(1), Telerik.WinControls.UI.UpperProgressIndicatorElement).Visibility = Telerik.WinControls.ElementVisibility.Collapsed
+        '
+        'formreport
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(731, 400)
+        Me.Controls.Add(Me.progressbar)
+        Me.Controls.Add(Me.RadButton1)
+        Me.Controls.Add(Me.reporttext)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow
+        Me.Name = "formreport"
+        '
+        '
+        '
+        Me.RootElement.ApplyShapeToControl = True
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent
+        Me.Text = "Progress Report"
+        Me.ThemeName = "Office2019Dark"
+        CType(Me.reporttext, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.RadButton1, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.progressbar, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents Office2019DarkTheme1 As Telerik.WinControls.Themes.Office2019DarkTheme
+    Friend WithEvents reporttext As Telerik.WinControls.UI.RadTextBoxControl
+    Friend WithEvents reptimer As Timer
+    Friend WithEvents RadButton1 As Telerik.WinControls.UI.RadButton
+    Friend WithEvents progressbar As Telerik.WinControls.UI.RadProgressBar
+End Class
+
---
+++ ide/forms/formreport.vb
@@ -0,0 +1,53 @@
+﻿Public Class formreport
+
+
+    Private progresslist As New YOLIB.list
+    Public isfail As Boolean = False
+    Private Sub reptimer_Tick(sender As Object, e As EventArgs) Handles reptimer.Tick
+        For index = 0 To progresslist.count - 1
+            update_progressbar_value(progresslist.get(index))
+            reporttext.AppendText(progresslist.get(index))
+            reporttext.AppendText(vbCrLf)
+        Next
+        progresslist.clear()
+    End Sub
+
+    Private Sub update_progressbar_value(val As String)
+        If val.Trim = String.Empty Then Return
+        If val = "-end" Then
+            If isfail = False Then Return
+            progressbar.Text = "Error !"
+        End If
+        Select Case val.Trim
+            Case "-Initialization & Process Preparation"
+                progressbar.Value1 = 5
+                Return
+            Case "-Lexical Analyzer"
+                progressbar.Value1 = 20
+                Return
+            Case "-Intermediate Code Generation"
+                progressbar.Value1 = 45
+                Return
+            Case "-Assembly Code Generation"
+                progressbar.Value1 = 70
+                Return
+            Case "-The overall result"
+                progressbar.Value1 = 100
+                Return
+            Case "***** Operation completed successfully *****"
+                Me.Close()
+            Case Else
+                If (val.StartsWith("Error Code :")) Then
+                    isfail = True
+                End If
+        End Select
+    End Sub
+
+    Public Sub get_update(val As String)
+        progresslist.add(val)
+    End Sub
+
+    Private Sub RadButton1_Click(sender As Object, e As EventArgs) Handles RadButton1.Click
+        Me.Close()
+    End Sub
+End Class
