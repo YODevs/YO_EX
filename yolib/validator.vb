1e7107a42bb9cbee7f6b87c87abbbfd4c78e3f73
Mahdi Foroughi <dev.mahdi82@gmail.com>
2023-05-19 13:55:15 +0330

    New Method :: Added six new functions

--- yolib/validator.vb
+++ yolib/validator.vb
@@ -36,5 +36,46 @@
         Next
         Return True
     End Function
-
+    Public Shared Function is_numeric(str As String) As Boolean
+        If str.Length = 0 Then Return False
+        For i = 0 To str.Length - 1
+            If Char.IsNumber(str(i)) = False Then Return False
+        Next
+        Return True
+    End Function
+    Public Shared Function is_uppercase(str As String) As Boolean
+        If str.Length = 0 Then Return False
+        Dim loweredstr As String = str.ToLower
+        For i = 0 To str.Length - 1
+            If str(i) = loweredstr(i) And str(i) <> " " Then Return False Else Continue For
+        Next
+        Return True
+    End Function
+    Public Shared Function is_lowercase(str As String) As Boolean
+        If str.Length = 0 Then Return False
+        Dim loweredstr As String = str.ToUpper
+        For i = 0 To str.Length - 1
+            If str(i) = loweredstr(i) And str(i) <> " " Then Return False Else Continue For
+        Next
+        Return True
+    End Function
+    Public Shared Function is_hexadecimal(str As String) As Boolean
+        If str.Length = 0 Then Return False
+        Return System.Text.RegularExpressions.Regex.IsMatch(str, "\A\b[0-9a-fA-F]+\b\Z")
+    End Function
+    Public Shared Function is_email(str As String) As Boolean
+        If str.Length = 0 Then Return False
+        Try
+            Dim address As New System.Net.Mail.MailAddress(str)
+            Return address.Address = str
+        Catch
+            Return False
+        End Try
+    End Function
+    Public Shared Function is_date(str As String) As Boolean
+        If str.Length = 0 Then Return False
+        If Char.IsNumber(str(0)) = False Or Char.IsNumber(str(str.Length - 1)) = False Then Return False
+        Dim result As DateTime
+        Return DateTime.TryParse(str, result)
+    End Function
 End Class
